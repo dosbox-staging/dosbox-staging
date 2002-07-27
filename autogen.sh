7d1ca9bdd4255c1d7957426bddea565639b8add0
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2002-07-27 12:56:33 +0000

    First file woohoo
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@79

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+echo "Generating build information using aclocal, auotheader, automake and autoconf"
+echo "This may take a while ..."
+
+# Touch the timestamps on all the files since CVS messes them up
+directory=`dirname $0`
+touch $directory/configure.in
+
+# Regenerate configuration files
+
+aclocal
+autoheader
+automake --gnits --include-deps --add-missing --copy 
+autoconf
+
+cp $directory/settings.h.cvs $directory/settings.h
+echo "Now you are ready to run ./configure also check settings.h for extra build settings"
