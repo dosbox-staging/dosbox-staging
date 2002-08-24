401169e0b4ef4faff8f6f5416aa46976aa87e7dd
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2002-08-24 20:01:55 +0000

    Added autoheader again
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@212

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-#
-echo "Generating build information using aclocal, automake and autoconf"
+
+echo "Generating build information using aclocal, autoheader, automake and autoconf"
 echo "This may take a while ..."
 
-# Touch the timestamps on all the files since CVS messes them up
-directory=`dirname $0`
-touch $directory/configure.in
-
-# Regenerate configuration files
+# Regenerate configuration files.
 
 aclocal
+autoheader
 automake --gnits --include-deps --add-missing --copy 
 autoconf
 
+#Copy settings.h.cvs to settings.h and that's it,
+
+directory=`dirname $0`
 cp $directory/settings.h.cvs $directory/settings.h
 echo "Now you are ready to run ./configure also check settings.h for extra build settings"
