b950a89a3ba23c67ad8da4a75a5821ba1e307fb1
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2002-10-18 09:20:24 +0000

    Change the PAGE_SIZE #define to be a bit safer
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@344

--- include/mem.h
+++ include/mem.h
@@ -30,7 +30,7 @@ typedef Bit8u (*MEMORY_ReadHandler)(PhysPt pt);
 typedef void (*MEMORY_WriteHandler)(PhysPt pt,Bit8u val);
 
 #define PAGE_KB	16
-#define PAGE_SIZE PAGE_KB*1024
+#define PAGE_SIZE (PAGE_KB*1024)
 #define PAGE_SHIFT 14
 #define PAGE_COUNT(A) (A & ((1 << PAGE_SHIFT)-1) ? 1+(A >> PAGE_SHIFT) : (A >> PAGE_SHIFT) )
 #define MAX_PAGES PAGE_COUNT(C_MEM_MAX_SIZE*1024*1024)
