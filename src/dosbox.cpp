26940f77ad6f4e33ae61d2dcae9eadc34aa78ce7
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2002-12-22 20:42:43 +0000

    Disable GUS startup for now.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@605

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -199,7 +199,7 @@ void DOSBOX_Init(void) {
 	secprop->AddInitFunction(&CMS_Init);
     secprop->Add_bool("cms",false);
 
-	secprop=control->AddSection_prop("gus",&GUS_Init);
+//	secprop=control->AddSection_prop("gus",&GUS_Init);
 	
 	secprop=control->AddSection_prop("disney",&DISNEY_Init);
 
