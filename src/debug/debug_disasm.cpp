d3d19f1039a7dbc412bee5ad6a9b2912d1c7918e
Ulf Wohlers <finsterr@users.sourceforge.net>
2003-02-21 20:36:02 +0000

    Added get operand size for debugger
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@677

--- src/debug/debug_disasm.cpp
+++ src/debug/debug_disasm.cpp
@@ -1111,6 +1111,11 @@ Bitu DasmI386(char* buffer, PhysPt pc, Bitu cur_ip, bool bit32)
 	return getbyte_mac-pc;
 }
 
+int DasmLastOperandSize()
+{
+	return opsize;
+};
+
 
 #endif
 
--- src/debug/debug_inc.h
+++ src/debug/debug_inc.h
@@ -53,3 +53,4 @@ extern DBGBlock dbg;
 
 /* Local Debug Stuff */
 Bitu DasmI386(char* buffer, PhysPt pc, Bitu cur_ip, bool bit32);
+int  DasmLastOperandSize(void);
