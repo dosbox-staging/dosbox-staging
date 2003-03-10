fbfd081260c315729f93b257e9fa953c7994654c
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2003-03-10 15:17:37 +0000

    Removal of settings.h, settings moved in here.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@736

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -2,5 +2,22 @@
 
 #define VERSION "0.57"
 
-#define GCC_ATTRIBUTE(x) /* attribute not supported */
+/* Define to 1 to enable internal debugger, requires libcurses */
+#define C_DEBUG 1
 
+/* Define to 1 to enable screenshots, requires libpng */
+#define C_SSHOTS 1
+
+/* Enable some heavy debugging options */
+#define C_HEAVY_DEBUG 0
+
+/* Enable some big compile-time increasing inlines */
+#define C_EXTRAINLINE 0
+
+/* Enable the FPU module, still only for beta testing */
+#define C_FPU 0
+
+/* Maximum memory address range in megabytes */
+#define C_MEM_MAX_SIZE 12
+
+#define GCC_ATTRIBUTE(x) /* attribute not supported */
