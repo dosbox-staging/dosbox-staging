42cf596c568991a57d204c01a8699a268e4a9183
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2003-08-23 11:00:08 +0000

    Fix correct address of linear frame buffer.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@1201

--- src/hardware/vga_crtc.cpp
+++ src/hardware/vga_crtc.cpp
@@ -422,12 +422,12 @@ void write_p3d5(Bit32u port,Bit8u val) {
 			7	(928) RAS 6-MCLK. If set the random read/write cycle time is 6MCLKs,
 				if clear 7MCLKs
 		*/
-	case 0x59:	/* Linear Address Window Position Low */
-		vga.s3.la_window=(vga.s3.la_window&0xff00) | val;
+	case 0x59:	/* Linear Address Window Position High */
+		vga.s3.la_window=(vga.s3.la_window&0x00ff) | (val << 8);
 		VGA_StartUpdateLFB();
 		break;
-	case 0x5a:	/* Linear Address Window Position High */
-		vga.s3.la_window=(vga.s3.la_window&0x00ff) | (val << 8);
+	case 0x5a:	/* Linear Address Window Position Low */
+		vga.s3.la_window=(vga.s3.la_window&0xff00) | val;
 		VGA_StartUpdateLFB();
 		break;
 	case 0x5D:	/* Extended Horizontal Overflow */
