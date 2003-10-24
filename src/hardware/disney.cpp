23bbc96f4dba2b8a67f9d6a550d17803bbdb92ef
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2003-10-24 20:03:33 +0000

    Change the disney enabling config setting
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@1374

--- src/hardware/disney.cpp
+++ src/hardware/disney.cpp
@@ -101,7 +101,7 @@ static void DISNEY_CallBack(Bit8u * stream,Bit32u len) {
 void DISNEY_Init(Section* sec) {
 	MSG_Add("DISNEY_CONFIGFILE_HELP","Nothing to setup yet!\n");
 	Section_prop * section=static_cast<Section_prop *>(sec);
-	if(!section->Get_bool("enabled")) return;
+	if(!section->Get_bool("disney")) return;
 
 	IO_RegisterWriteHandler(DISNEY_BASE,disney_write,"DISNEY");
 	IO_RegisterWriteHandler(DISNEY_BASE+1,disney_write,"DISNEY");
