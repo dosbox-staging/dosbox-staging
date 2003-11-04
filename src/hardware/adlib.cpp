4057f50057575e2a2d4edee15a42babee419aa8a
Dean Beeler <canadacow@users.sourceforge.net>
2003-11-04 03:40:22 +0000

    Added port hack to Gravis to aid in GUS detection
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@1396

--- src/hardware/adlib.cpp
+++ src/hardware/adlib.cpp
@@ -109,6 +109,10 @@ static Bit8u read_p388(Bit32u port) {
 
 static void write_p388(Bit32u port,Bit8u val) {
 	regsel=val;
+
+	// The following writes this value to ultrasounds equivalent register.
+	// I don't know of any other way to do this
+	IO_Write(0x248,val);
 }
 
 static void write_p389(Bit32u port,Bit8u val) {
