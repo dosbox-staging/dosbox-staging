cb3ee0f0c08e0a49b6dd30b941aed15220c6a42a
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2004-01-28 14:48:20 +0000

    Enable C_OPENGL for visualc
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@1614

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -8,6 +8,9 @@
 /* Define to 1 to enable screenshots, requires libpng */
 #define C_SSHOT 1
 
+/* Define to 1 to use opengl display output support */
+#define C_OPENGL 1
+
 /* Define to 1 to enable internal modem support, requires SDL_net */
 #define C_MODEM 1
 
