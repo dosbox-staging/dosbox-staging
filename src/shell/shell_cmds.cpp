1d11c37e2380017690d41f7fcce632275cb93f1b
Peter Veenstra <qbix79@users.sourceforge.net>
2006-04-21 13:21:09 +0000

    make default choice lowcase so case-sensitive matching shows a more reasanable number.(ykhwong)
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@2611

--- src/shell/shell_cmds.cpp
+++ src/shell/shell_cmds.cpp
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-/* $Id: shell_cmds.cpp,v 1.64 2006-04-21 08:18:51 qbix79 Exp $ */
+/* $Id: shell_cmds.cpp,v 1.65 2006-04-21 13:21:09 qbix79 Exp $ */
 
 #include <string.h>
 #include <ctype.h>
@@ -780,7 +780,7 @@ void DOS_Shell::CMD_LOADHIGH(char *args){
 }
 
 void DOS_Shell::CMD_CHOICE(char * args){
-	static char defchoice[3] = {'Y','N',0};
+	static char defchoice[3] = {'y','n',0};
 	char *rem = NULL, *ptr;
 	bool optN = ScanCMDBool(args,"N");
 	bool optS = ScanCMDBool(args,"S"); //Case-sensitive matching
