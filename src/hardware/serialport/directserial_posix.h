6945f3e81fa73e352bbc0f14cb64d6df8e41b147
Peter Veenstra <qbix79@users.sourceforge.net>
2009-01-26 20:15:58 +0000

    Enable directserial for BSD. (talked it over with the FreeBSD maintainer
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3274

--- src/hardware/serialport/directserial_posix.h
+++ src/hardware/serialport/directserial_posix.h
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-/* $Id: directserial_posix.h,v 1.2 2007-08-26 17:19:46 qbix79 Exp $ */
+/* $Id: directserial_posix.h,v 1.3 2009-01-26 20:15:58 qbix79 Exp $ */
 
 // include guard
 #ifndef DOSBOX_DIRECTSERIAL_POSIX_H
@@ -25,7 +25,7 @@
 #include "dosbox.h"
 
 #if C_DIRECTSERIAL
-#if defined (LINUX) || defined (MACOSX)
+#if defined (LINUX) || defined (MACOSX) || defined (BSD)
 
 
 
