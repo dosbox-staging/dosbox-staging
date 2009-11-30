8c94075781749024c762f159668c2cb03e1fd46f
Peter Veenstra <qbix79@users.sourceforge.net>
2009-11-30 13:48:54 +0000

    Undo 3505.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3506

--- include/dos_inc.h
+++ include/dos_inc.h
@@ -86,7 +86,7 @@ enum { RETURN_EXIT=0,RETURN_CTRLC=1,RETURN_ABORT=2,RETURN_TSR=3};
 #define DOS_SDA_OFS 0
 #define DOS_CDS_SEG 0x108
 #define DOS_FIRST_SHELL 0x118
-#define DOS_MEM_START 0x170		//First Segment that DOS can use
+#define DOS_MEM_START 0x16f		//First Segment that DOS can use
 
 #define DOS_PRIVATE_SEGMENT 0xc800
 #define DOS_PRIVATE_SEGMENT_END 0xd000
