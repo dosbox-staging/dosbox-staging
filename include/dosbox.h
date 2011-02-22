e31e0316aa7f776a4ef73467e6f82db73e90a00f
Peter Veenstra <qbix79@users.sourceforge.net>
2011-02-22 18:02:10 +0000

    Declare E_Exit as noreturn. Makes gcc happy
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3678

--- include/dosbox.h
+++ include/dosbox.h
@@ -23,10 +23,10 @@
 
 #include "config.h"
 
-void E_Exit(const char * message,...) GCC_ATTRIBUTE( __format__(__printf__, 1, 2));
+GCC_ATTRIBUTE(noreturn) void E_Exit(const char * message,...) GCC_ATTRIBUTE( __format__(__printf__, 1, 2));
 
-void MSG_Add(const char*,const char*); //add messages to the internal langaugefile
-const char* MSG_Get(char const *);     //get messages from the internal langaugafile
+void MSG_Add(const char*,const char*); //add messages to the internal languagefile
+const char* MSG_Get(char const *);     //get messages from the internal languagefile
 
 class Section;
 
