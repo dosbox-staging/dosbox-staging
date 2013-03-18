ffd5cf861e7720bb1e4f97fbe0ef650f8acd2160
Peter Veenstra <qbix79@users.sourceforge.net>
2013-03-18 12:34:13 +0000

    Enable interupts after running INT 25 and 26, fixes Eol-ui Moheom installer (ripsaw)
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3822

--- src/dos/dos.cpp
+++ src/dos/dos.cpp
@@ -1157,27 +1157,29 @@ static Bitu DOS_27Handler(void) {
 }
 
 static Bitu DOS_25Handler(void) {
-	if(Drives[reg_al]==0){
-		reg_ax=0x8002;
+	if (Drives[reg_al] == 0){
+		reg_ax = 0x8002;
 		SETFLAGBIT(CF,true);
-	}else{
+	} else {
 		SETFLAGBIT(CF,false);
-		if((reg_cx != 1) ||(reg_dx != 1))
+		if ((reg_cx != 1) ||(reg_dx != 1))
 			LOG(LOG_DOSMISC,LOG_NORMAL)("int 25 called but not as diskdetection drive %X",reg_al);
 
-	   reg_ax=0;
+	   reg_ax = 0;
 	}
+	SETFLAGBIT(IF,true);
     return CBRET_NONE;
 }
 static Bitu DOS_26Handler(void) {
 	LOG(LOG_DOSMISC,LOG_NORMAL)("int 26 called: hope for the best!");
-	if(Drives[reg_al]==0){
-		reg_ax=0x8002;
+	if (Drives[reg_al] == 0){
+		reg_ax = 0x8002;
 		SETFLAGBIT(CF,true);
-	}else{
+	} else {
 		SETFLAGBIT(CF,false);
-		reg_ax=0;
+		reg_ax = 0;
 	}
+	SETFLAGBIT(IF,true);
     return CBRET_NONE;
 }
 
