babf6195f53c744bc3d0be32b5d3b706764eb4fd
Peter Veenstra <qbix79@users.sourceforge.net>
2013-11-04 20:49:58 +0000

    make clang happy. Thanks wjp
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3841

--- src/fpu/fpu_instructions_x86.h
+++ src/fpu/fpu_instructions_x86.h
@@ -1161,12 +1161,12 @@ static void FPU_FLD_F80(PhysPt addr) {
 
 static void FPU_FLD_I16(PhysPt addr,Bitu store_to) {
 	fpu.p_regs[8].m1 = (Bit32u)mem_readw(addr);
-	FPUD_LOAD(fild,WORD,)
+	FPUD_LOAD(fild,WORD,s)
 }
 
 static void FPU_FLD_I16_EA(PhysPt addr) {
 	fpu.p_regs[8].m1 = (Bit32u)mem_readw(addr);
-	FPUD_LOAD_EA(fild,WORD,)
+	FPUD_LOAD_EA(fild,WORD,s)
 }
 
 static void FPU_FLD_I32(PhysPt addr,Bitu store_to) {
@@ -1211,7 +1211,7 @@ static void FPU_FST_F80(PhysPt addr) {
 }
 
 static void FPU_FST_I16(PhysPt addr) {
-	FPUD_STORE(fistp,WORD,)
+	FPUD_STORE(fistp,WORD,s)
 	mem_writew(addr,(Bit16u)fpu.p_regs[8].m1);
 }
 
