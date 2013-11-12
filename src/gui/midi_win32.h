0d65d2956c9f240f3e5dac8d2d6c618f2a514a5c
Peter Veenstra <qbix79@users.sourceforge.net>
2013-11-12 21:39:18 +0000

    Correct the parameter type for 64 bit.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3847

--- src/gui/midi_win32.h
+++ src/gui/midi_win32.h
@@ -47,10 +47,10 @@ public:
 				MIDIOUTCAPS mididev;
 				midiOutGetDevCaps(nummer, &mididev, sizeof(MIDIOUTCAPS));
 				LOG_MSG("MIDI:win32 selected %s",mididev.szPname);
-				res = midiOutOpen(&m_out, nummer, (DWORD)m_event, 0, CALLBACK_EVENT);
+				res = midiOutOpen(&m_out, nummer, (DWORD_PTR)m_event, 0, CALLBACK_EVENT);
 			}
 		} else {
-			res = midiOutOpen(&m_out, MIDI_MAPPER, (DWORD)m_event, 0, CALLBACK_EVENT);
+			res = midiOutOpen(&m_out, MIDI_MAPPER, (DWORD_PTR)m_event, 0, CALLBACK_EVENT);
 		}
 		if (res != MMSYSERR_NOERROR) return false;
 		isOpen=true;
