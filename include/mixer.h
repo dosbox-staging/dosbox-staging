271d8cffa6985b49d6d84c495d72f0b95b63cf2b
Sjoerd van der Berg <harekiet@users.sourceforge.net>
2017-01-04 08:35:10 +0000

    Updated header for mixer changes
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4003

--- include/mixer.h
+++ include/mixer.h
@@ -72,18 +72,27 @@ public:
 	void AddSamples_s16u_nonnative(Bitu len, const Bit16u * data);
 	void AddSamples_m32_nonnative(Bitu len, const Bit32s * data);
 	void AddSamples_s32_nonnative(Bitu len, const Bit32s * data);
-
+	
 	void AddStretched(Bitu len,Bit16s * data);		//Strech block up into needed data
+
 	void FillUp(void);
 	void Enable(bool _yesno);
 	MIXER_Handler handler;
 	float volmain[2];
 	float scale;
 	Bit32s volmul[2];
-	Bitu freq_add,freq_index;
-	Bitu done,needed;
-	Bits last[2];
+	
+	//This gets added the frequency counter each mixer step
+	Bitu freq_add;
+	//When this flows over a new sample needs to be read from the device
+	Bitu freq_counter;
+	//Timing on how many samples have been done and were needed by th emixer
+	Bitu done, needed;
+	//Previous and next samples
+	Bits prevSample[2];
+	Bits nextSample[2];
 	const char * name;
+	bool interpolate;
 	bool enabled;
 	MixerChannel * next;
 };
