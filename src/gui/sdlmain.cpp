48611d614135dd5041d3e0a1220779b3e76068e3
Peter Veenstra <qbix79@users.sourceforge.net>
2018-04-04 10:05:50 +0000

    Do we even use those timers ? Ticks are initted regardless. Please report problems! (helps with cpu usage on Macs)
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4090

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -1998,7 +1998,9 @@ int main(int argc, char* argv[]) {
 	 */
 	putenv(const_cast<char*>("SDL_DISABLE_LOCK_KEYS=1"));
 #endif
-	if ( SDL_Init( SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_TIMER|SDL_INIT_CDROM
+	// Don't init timers, GetTicks seems to work fine and they can use a fair amount of power (Macs again) 
+	// Please report problems with audio and other things.
+	if ( SDL_Init( SDL_INIT_AUDIO|SDL_INIT_VIDEO | /*SDL_INIT_TIMER |*/ SDL_INIT_CDROM
 		|SDL_INIT_NOPARACHUTE
 		) < 0 ) E_Exit("Can't init SDL %s",SDL_GetError());
 	sdl.inited = true;
