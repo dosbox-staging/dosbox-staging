1afaf2892107501e25e6d4ef58c0d863cb5b8b51
ripsaw8080 <ripsaw8080@users.sourceforge.net>
2018-06-12 20:52:08 +0000

    Make Media ID table relative to DPB table. Fixes Hattrick by Ikarion.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4116

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -126,6 +126,7 @@ public:
 					switch (DriveManager::UnmountDrive(i_drive)) {
 					case 0:
 						Drives[i_drive] = 0;
+						mem_writeb(Real2Phys(dos.tables.mediaid)+i_drive*5,0);
 						if(i_drive == DOS_GetDefaultDrive()) 
 							DOS_SetDrive(ZDRIVE_NUM);
 						WriteOut(MSG_Get("PROGRAM_MOUNT_UMOUNT_SUCCESS"),umount[0]);
@@ -394,7 +395,7 @@ public:
 		if (!newdrive) E_Exit("DOS:Can't create drive");
 		Drives[drive-'A']=newdrive;
 		/* Set the correct media byte in the table */
-		mem_writeb(Real2Phys(dos.tables.mediaid)+(drive-'A')*2,newdrive->GetMediaByte());
+		mem_writeb(Real2Phys(dos.tables.mediaid)+(drive-'A')*5,newdrive->GetMediaByte());
 		WriteOut(MSG_Get("PROGRAM_MOUNT_STATUS_2"),drive,newdrive->GetInfo());
 		/* check if volume label is given and don't allow it to updated in the future */
 		if (cmd->FindString("-label",label,true)) newdrive->dirCache.SetLabel(label.c_str(),iscdrom,false);
@@ -1314,7 +1315,7 @@ public:
 				DriveManager::InitializeDrive(drive - 'A');
 
 				// Set the correct media byte in the table 
-				mem_writeb(Real2Phys(dos.tables.mediaid) + (drive - 'A') * 2, mediaid);
+				mem_writeb(Real2Phys(dos.tables.mediaid) + (drive - 'A') * 5, mediaid);
 				
 				/* Command uses dta so set it to our internal dta */
 				RealPt save_dta = dos.dta();
@@ -1392,7 +1393,7 @@ public:
 				DriveManager::InitializeDrive(drive - 'A');
 				
 				// Set the correct media byte in the table 
-				mem_writeb(Real2Phys(dos.tables.mediaid) + (drive - 'A') * 2, mediaid);
+				mem_writeb(Real2Phys(dos.tables.mediaid) + (drive - 'A') * 5, mediaid);
 				
 				// Print status message (success)
 				WriteOut(MSG_Get("MSCDEX_SUCCESS"));
