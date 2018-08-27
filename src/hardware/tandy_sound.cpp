0463a8b9078bf2dadd877408772200dcb1c9696e
Peter Veenstra <qbix79@users.sourceforge.net>
2018-08-27 09:49:07 +0000

    Update tandy sound core to latest (unmerged) mame version. Update tandy sound selection to use the ncr8496 for normal usage and the sn76496 in PCjr mode.
    Correct noise algorithm. Ignore writes to certain registers if highest bit is not set for ncr8496 mode. Don't reset the noise producing register on all writes, only when changing the type of noise (ncr8496). 
    Fixes intro sound of KQ2, waves in KQ2 and sound on the title screen of sentinel.
    Thanks for the help Lord_Nightmare. 
    
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4141

--- src/hardware/tandy_sound.cpp
+++ src/hardware/tandy_sound.cpp
@@ -63,8 +63,11 @@ static struct {
 	} dac;
 } tandy;
 
-static sn76496_device device(machine_config(), 0, 0, SOUND_CLOCK );
+static sn76496_device device_sn76496(machine_config(), 0, 0, SOUND_CLOCK );
+static ncr8496_device device_ncr8496(machine_config(), 0, 0, SOUND_CLOCK);
 
+static sn76496_base_device* activeDevice = &device_ncr8496;
+#define device (*activeDevice)
 
 static void SN76496Write(Bitu /*port*/,Bitu data,Bitu /*iolen*/) {
 	tandy.last_write=PIC_Ticks;
@@ -73,6 +76,8 @@ static void SN76496Write(Bitu /*port*/,Bitu data,Bitu /*iolen*/) {
 		tandy.enabled=true;
 	}
 	device.write(data);
+
+//	LOG_MSG("3voice write %X at time %7.3f",data,PIC_FullIndex());
 }
 
 static void SN76496Update(Bitu length) {
@@ -270,6 +275,10 @@ public:
 		if (SB_Get_Address(sbport, sbirq, sbdma)) {
 			enable_hw_tandy_dac=false;
 		}
+
+		//Select the correct tandy chip implementation
+		if (machine == MCH_PCJR) activeDevice = &device_sn76496;
+		else activeDevice = &device_ncr8496;
 
 		real_writeb(0x40,0xd4,0x00);
 		if (IS_TANDY_ARCH) {
