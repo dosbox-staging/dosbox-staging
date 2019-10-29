af6557ae70bdcaf9b9d8c5a1377d5d7664249f78
Patryk Obara <dreamer.tan@gmail.com>
2019-10-29 20:50:23 +0100

    Update allowed warnings limit

--- scripts/count-bugs.py
+++ scripts/count-bugs.py
@@ -23,7 +23,7 @@ from bs4 import BeautifulSoup
 # then script will return with status 1. Simply change this line if you
 # want to set a different limit.
 #
-MAX_ISSUES = 99
+MAX_ISSUES = 94
 
 def summary_values(summary_table):
     if not summary_table:
