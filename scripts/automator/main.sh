b44d19ff2d883b8c4a48330c7b236ab72285a17b
krcroft <krcroft@users.noreply.github.com>
2019-11-23 19:54:19 -0800

    Refactor the build and list-packages scripts
    Until now the build and package scripts have supported
    several architectures, compilers, build types, package-managers,
    and bit-depth targets.
    
    The code might be maintainable if left as-such, however we have plans
    to continue expanding the number of architectures (ARM, PPC, ... ),
    operating systems (Android, BSDs, ...), and build variations
    amung those.
    
    The scripts (regardless of language) would only grow in complexity
    as more variations are added. Thus, we needed a solution that can
    scale without adding complexity.
    
    To achieve this, the scripts were refactored as follows:
     - all "data" was moved out of code into configuration files
     - A back-end "Automator" engine was written to parse the
       data based on generic variables fed to it by a front-end
       script
     - build.sh and list-packages.sh were re-authored as thin front-end
       scripts that drive the automator
     - Their CLI's were retained so there has been very little change
       needed to the CI invocation lines.  The only changes have been to
       clarify the existing arguments improved based on feedback, ie:
       --build-type release, --build-type debug instead of fast, small

---
+++ scripts/automator/main.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Copyright (c) 2019 Kevin R Croft <krcroft@gmail.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Automator is a generic automation tool that helps separate data
+# from logical code.  It uses one or more "variables" files placed
+# in a single sub-directory, that are sourced in logical succession,
+# such as variables defined in earler files are either added-to or
+# overridden in subsequent files.
+#
+# For a thorough example, see scripts/build.sh and its data files
+# in scripts/automator/build.
+#
+# TODO: This script manages variables using eval meta-programming.
+#       Although the syntax is quite readable under bash 4.x,
+#       we have limitted our syntax to the uglier and heavier bash 3.x
+#       because Apple is still only shipping bash 3.x, even on their
+#       latest operating system (Nov-2019).
+#
+#       Future work might involve switching this to python and using YAML
+#       syntax in the variables files. Python's dictionaries can be
+#       merged or have their values overridden, and key names can be readily
+#       manipulated.
+#
+set -euo pipefail
+
+function underline() {
+	echo ""
+	echo "$1"
+	echo "${1//?/${2:--}}"
+}
+
+function lower() {
+	echo "${1}" | tr '[:upper:]' '[:lower:]'
+}
+
+function upper() {
+	echo "${1}" | tr '[:lower:]' '[:upper:]'
+}
+
+function arg_error() {
+	local PURPLE='\033[0;34m'
+	local BLACK='\033[0;30m'
+	local ON_WHITE='\033[47m'
+	local NO_COLOR='\033[0m'
+	>&2 echo -e "Please specify the ${BLACK}${ON_WHITE}${1}${NO_COLOR} argument with one of: ${PURPLE}${2}${NO_COLOR}"
+	exit 1
+}
+
+function import() { # arguments: category instance
+	category=$(lower "${1}")
+	instance=$(lower "${2}")
+	if [[ -z "$instance" ]]; then
+		instances=( "$(find "${data_dir}" -name "${category}-*" -a ! -name '*defaults' | sed 's/.*-//' | xargs)" )
+		arg_error "--${category}" "${instances[*]}"
+	else for instance in defaults "${instance}"; do
+		varfile="${data_dir}/${category}-${instance}"
+		# shellcheck disable=SC1090,SC1091
+		if [[ -f "$varfile" ]]; then source "$varfile"
+		else >&2 echo "${varfile} does not exist, skipping"; fi; done
+	fi
+}
+
+function construct_environment() {
+	# underline "Environment"
+	for base_var in "${VARIABLES[@]}"; do
+		# First check if we have a build-specific variable
+		# shellcheck disable=SC2154
+		if [[ -n "$(eval 'echo ${'"${base_var}_${selected_type}"'[*]:-}')" ]]; then
+			var_name="${base_var}_${selected_type}"
+		# Otherwise fallback to the default variable
+		else var_name="${base_var}"; fi
+
+		# Aggregate any modifiers, which are stand-alone arguments
+		mod_values=""
+		# shellcheck disable=SC2154
+		for mod_upper in "${modifiers[@]}"; do
+			mod=$(lower "${mod_upper}")
+			# Mods can only add options to existing VARs, so we check for those
+			if [[ -n "$(eval 'echo ${'"${base_var}_${mod}"'[*]:-}')" ]]; then
+				var_with_mod="${base_var}_${mod}"
+				# shellcheck disable=SC2034
+				mod_values=$(eval 'echo ${mod_values} ${'"${var_with_mod}"'[*]}')
+			fi
+		done
+
+		# Expand our variable array (or scalar) and combine it with our aggregated
+		# modification string. Echo takes care of spacing out our variables without
+		# explicit padding, because it either drops empty variables or adds spaces
+		# between them if they exist.
+		# shellcheck disable=SC2034
+		combined=$(eval 'echo ${'"${var_name}"'[*]} ${mod_values}')
+		env_var=$(upper "${base_var}")
+		eval 'export '"${env_var}"'="${combined}"'
+		# echo "${env_var}=\"$(eval echo \$"${env_var}")\""
+	done
+}
+
+function perform_steps() {
+	# underline "Launching"
+	for step in "${STEPS[@]}"; do
+		if type -t "$step" | grep -q function; then "$step"
+		else
+			# eval 'echo ${'"${step}"'[*]}'
+			eval '${'"${step}"'[*]}'
+		fi
+	done
+}
+
+function main() {
+	cd "$(dirname "$0")/automator"
+	if [[ -z "${data_dir:-}" ]]; then data_dir="$(basename "$0" '.sh')"; fi
+	parse_args "$@"
+	construct_environment
+	perform_steps
+}
+
+main "$@"
+
--- scripts/list-build-dependencies.sh
+++ scripts/list-build-dependencies.sh
@@ -1,196 +1,55 @@
 #!/bin/bash
 
-##
-#  Copyright (c) 2019 Kevin R. Croft
-#  SPDX-License-Identifier: GPL-2.0-or-later
-#
-#  This script lists development packages and DOSBox dependencies needed to build DOSBox.
-#  This package names provided are tailor based on the provided package manager,
-#  choice of compiler, and optionally its bit-depth.
-#
-#  See the usage arguments below for details or run it with the -h or --help.
-#
-#  In general, this script adheres to Google's shell scripting style guide
-#  (https://google.github.io/styleguide/shell.xml), however some deviations (such as
-#  tab indents instead of two-spaces) are used to fit DOSBox's in-practice coding style.
-#
+# Copyright (c) 2019 Kevin R Croft <krcroft@gmail.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
 
+# This script prints package dependencies for a given package manmager,
+# compiler (gcc or clang), and bit-depth (32 or 64).
+#
+# If run without arguments, the script asks for required arguments one
+# by one, which includes the package manager (--manager) and --compiler.
+#
+# Optional arguments include the bit-depth, defaults to 64-bit;
+# and the compiler version, which defaults to the package manager's
+# default version.
+#
+# Usage examples:
+#   $ ./list-build-dependencies.sh               # asks for --manager
+#   $ ./list-build-dependencies.sh --manager apt # asks for --compiler
+#   $ ./list-build-dependencies.sh --manager apt --compiler gcc -v 9
+#   $ ./list-build-dependencies.sh --manager zypper --compiler clang
+#   $ ./list-build-dependencies.sh --manager msy2 --compiler gcc -b 32
+#
+# This script makes use of the automator package, see automator/main.sh.
+#
 set -euo pipefail
-IFS=$'\n\t'
 
-function usage() {
-	if [ -n "${1}" ]; then
-		errcho "${1}"
-	fi
-	local script
-	script=$(basename "${0}")
-	echo "Usage: ${script} -p apt|xcode|brew|macports|msys2|vcpkg [-b 32|64] [-c gcc|clang] [-o FILE] [-u #]"
-	echo ""
-	echo "  FLAG                       Description                                          Default"
-	echo "  -----------------------    ---------------------------------------------------  -------"
-	echo "  -b, --bit-depth            Choose either a 64 or 32 bit compiler                [$(print_var "${BITS}")]"
-	echo "  -c, --compiler             Choose either gcc or clang                           [$(print_var "${COMPILER}")]"
-	echo "  -u, --compiler-version <#> Customize the compiler version (if available)        [$(print_var "${COMPILER_VERSION}")]"
-	echo "  -v, --version              Print the version of this script                     [$(print_var "${SCRIPT_VERSION}")]"
-	echo "  -h, --help                 Print this usage text"
-	echo "  -p, --package-manager      Choose one of the following:"
-	echo "                               - apt      (Linux: Debian, Ubuntu, Raspbian)"
-	echo "                               - dnf      (Linux: Fedora, RedHat, CentOS)"
-	echo "                               - pacman   (Linux: Arch, Manjaro)"
-	echo "                               - zypper   (Linux: SuSE, OpenSUSE)"
-	echo "                               - xcode    (OS X: Apple-supported)"
-	echo "                               - brew     (OS X: Homebrew community-supported)"
-	echo "                               - macports (OS X: MacPorts community-supported)"
-	echo "                               - msys2    (Windows: Cygwin-based, community-support)"
-	echo "                               - vcpkg    (Windows: Visual Studio builds)"
-	echo "  -----------------------    ---------------------------------------------------  -------"
-	echo ""
-	echo "Example: ${script} --package-manager apt --compiler clang --compiler-version 8"
-	echo ""
-	echo "Note: the last value will take precendent if duplicate flags are provided."
-	exit 1
-}
-
-function defaults() {
-	BITS="64"
-	COMPILER="gcc"
-	COMPILER_VERSION=""
-	PACKAGE_MANAGER="unset"
-	readonly SCRIPT_VERSION="0.2"
-}
-
-# parse params
 function parse_args() {
-	defaults
+	# Gather the parameters that define this build
+	bits="64"
+	modifiers=("")
+	# shellcheck disable=SC2034
 	while [[ "${#}" -gt 0 ]]; do case ${1} in
-		-b|--bit-depth)        BITS="${2}";             shift;shift;;
-		-c|--compiler)         COMPILER="${2}";         shift;shift;;
-		-p|--package-manager)  PACKAGE_MANAGER="${2}";  shift;shift;;
-		-u|--compiler-version) COMPILER_VERSION="${2}"; shift;shift;;
-		-v|--version)          print_version;           shift;;
-		-h|--help)             usage "Show usage";      shift;;
-		*) usage "Unknown parameter: ${1}";             shift;shift;;
+		-m|--manager)           manager="${2}";                shift 2;;
+		-c|--compiler)          selected_type=$(lower "${2}"); shift 2;;
+		-b|--bit-depth)         bits="${2}";                   shift 2;;
+		-v|--compiler-version)  version="${2}";                shift 2;;
+		-a|--addon)             modifiers+=("${2}");           shift 2;;
+		*) >&2 echo "Unknown parameter: ${1}"; exit 1;         shift 2;;
 	esac; done
 
-	# Check mandatory arguments
-	if [[ "${PACKAGE_MANAGER}" == "unset" ]]; then
-		usage "A choice of package manager must be provided; use '-p <choice>' or '--package-manager <choice>'"
-	fi
+	# Import our settings and report missing values
+	import manager "${manager:-}"
+	if [[ -z "${selected_type:-}" ]]; then arg_error "--compiler" "${TYPES[*]}"; fi
+
+	# If a version was provided then construct a postfix string given the manager's
+	# potential unique delimeter (ie: gcc@9 for brew, gcc-9 for apt).
+	# shellcheck disable=SC2034,SC2154
+	postfix=$([[ -n "${version:-}" ]] && echo "${delim}${version}" || echo "")
+	import "${selected_type:-}" "${manager:-}"
 }
+# shellcheck disable=SC2034
+data_dir="packages"
 
-function errcho() {
-	local CLEAR='\033[0m'
-	local RED='\033[0;91m'
-	>&2 echo ""
-	>&2 echo -e " ${RED}👉  ${*}${CLEAR}" "\\n"
-}
-
-function print_var() {
-	if [[ -z "${1}" ]]; then
-		echo "unset"
-	else
-		echo "${1}"
-	fi
-}
-
-function list_packages() {
-	VERSION_DELIM=""
-	case "$1" in
-
-		apt)
-			# Apt separates GCC into the gcc and g++ pacakges, the latter which depends on the prior.
-			# Therefore, we provide g++ in-place of gcc.
-			VERSION_DELIM="-"
-			if [[ "${COMPILER}" == "gcc" ]]; then
-				COMPILER="g++"
-			fi
-			PACKAGES=(libtool build-essential autoconf-archive libsdl1.2-dev libsdl-net1.2-dev libopusfile-dev)
-			;;
-
-		dnf)
-			VERSION_DELIM="-"
-			PACKAGES=(libtool autoconf-archive SDL SDL_net-devel opusfile-devel)
-			;;
-
-		pacman)
-			# Arch offers 32-bit versions of SDL (but not others)
-			PACKAGES=(libtool autoconf-archive sdl_net opusfile)
-			if [[ "${BITS}" == 32 ]]; then
-				PACKAGES+=(lib32-sdl)
-			else
-				PACKAGES+=(sdl)
-			fi
-			;;
-
-		zypper)
-			# OpenSUSE offers 32-bit versions of SDL and SDL_net (but not others)
-			PACKAGES=(devel_basis libtool autoconf-archive opusfile)
-			if [[ "${BITS}" == 32 ]]; then
-				PACKAGES+=(libSDL-devel-32bit libSDL_net-devel-32bit)
-			else
-				PACKAGES+=(SDL SDL_net)
-			fi
-			;;
-
-		xcode)
-			# If the user doesn't want to use Homebrew or MacPorts, then they are limited to
-			# Apple's Clang plus the provided command line development tools provided by Xcode.
-			COMPILER=""
-			echo "Execute the following:"
-			echo "   xcode-select --install    # to install command line development tools"
-			echo "   sudo xcodebuild -license  # to accept Apple's license agreement"
-			echo ""
-			echo "Now download, build, and install the following manually to avoid using Homebrew or MacPorts:"
-			echo " - coreutils autogen autoconf automake pkg-config libpng sdl sdl_net opusfile"
-			;;
-
-		brew)
-			# If the user wants Clang, we knock it out because it's provided provided out-of-the-box
-			VERSION_DELIM="@"
-			if [[ "${COMPILER}" == "clang" ]]; then
-				COMPILER=""
-			fi
-			PACKAGES=(coreutils autogen autoconf autoconf-archive automake pkg-config libpng sdl sdl_net opusfile)
-			;;
-
-		macports)
-			PACKAGES=(coreutils autogen automake autoconf autoconf-archive pkgconfig libpng libsdl libsdl_net opusfile)
-			;;
-
-		msys2)
-			# MSYS2 only supports the current latest releases of Clang and GCC, so we disable version customization
-			COMPILER_VERSION=""
-			local pkg_type
-			pkg_type="x86_64"
-			if [[ "${BITS}" == 32 ]]; then
-				pkg_type="i686"
-			fi
-			PACKAGES=(autogen autoconf autoconf-archive base-devel automake-wrapper)
-			for pkg in pkg-config libtool libpng zlib SDL SDL_net opusfile; do
-				PACKAGES+=("mingw-w64-${pkg_type}-${pkg}")
-			done
-			COMPILER="mingw-w64-${pkg_type}-${COMPILER}"
-			;;
-
-		vcpkg)
-			# VCPKG doesn't provide Clang or GCC, so we knock out the compiler and just give packages
-			COMPILER=""
-			PACKAGES=(libpng sdl1 sdl1-net opusfile)
-			;;
-		*)
-			usage "Unknown package manager ${1}"
-			;;
-	esac
-
-	if [[ -n "${COMPILER_VERSION}" && -n "${COMPILER}" ]]; then
-		COMPILER+="${VERSION_DELIM}${COMPILER_VERSION}"
-	fi
-	echo "${COMPILER} ${PACKAGES[*]}"
-}
-
-function main() {
-	parse_args "$@"
-	list_packages "${PACKAGE_MANAGER}"
-}
-
-main "$@"
+# shellcheck source=scripts/automator/main.sh
+source "$(dirname "$0")/automator/main.sh"
