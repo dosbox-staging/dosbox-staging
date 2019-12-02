58a4ff067d18c30413303bf7786009ec282754b6
Patryk Obara <dreamer.tan@gmail.com>
2019-12-02 14:56:23 +0100

    Update fake SDL headers for MSVC

--- src/platform/visualc/sdl_thread.h
+++ src/platform/visualc/sdl_thread.h
@@ -1,4 +1,4 @@
 // This file exists only for MSVC builds.
 // It's needed because vcpkg does not set SDL subdirectory in includes list
 // for projects that do not use cmake.
-#include <SDL/SDL_thread.h>
+#include <SDL2/SDL_thread.h>
