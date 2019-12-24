5963be833612366c0290bed2d44b9908ece825be
Patryk Obara <dreamer.tan@gmail.com>
2019-12-24 06:56:25 +0100

    Surround WIN32_LEAN_AND_MEAN with ifndef
    Otherwise it redefines macro definition in SDL_opengl.h.

--- include/cross.h
+++ include/cross.h
@@ -76,10 +76,10 @@ public:
 	static bool IsPathAbsolute(std::string const& in);
 };
 
-
 #if defined (WIN32)
-
-#define WIN32_LEAN_AND_MEAN        // Exclude rarely-used stuff from 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 typedef struct dir_struct {
