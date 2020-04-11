fc4bbf696963096ea7ee50202a3438e689b44097
krcroft <krcroft@users.noreply.github.com>
2020-04-11 13:47:41 -0700

    Fix alignment when saving the branch value

--- src/cpu/core_dyn_x86/risc_x86.h
+++ src/cpu/core_dyn_x86/risc_x86.h
@@ -983,8 +983,8 @@ static Bit8u * gen_create_branch_long(BranchTypes type) {
 	return (cache.pos-4);
 }
 
-static void gen_fill_branch_long(Bit8u * data,Bit8u * from=cache.pos) {
-	*(Bit32u*)data=(from-data-4);
+static void gen_fill_branch_long(uint8_t *data, uint8_t *from = cache.pos) {
+	host_writed(data, from - data - sizeof(uint32_t));
 }
 
 static Bit8u * gen_create_jump(Bit8u * to=0) {
