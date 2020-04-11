34d46ecd7f8e35a2345c29ee657d6b780b11a046
krcroft <krcroft@users.noreply.github.com>
2020-04-11 13:43:06 -0700

    Fix alignment when incrementing values in the cache

--- src/cpu/core_dynrec/decoder_basic.h
+++ src/cpu/core_dynrec/decoder_basic.h
@@ -294,9 +294,14 @@ static void INLINE decode_increase_wmapmask(Bitu size) {
 	// update mask entries
 	switch (size) {
 		case 1 : activecb->cache.wmapmask[mapidx]+=0x01; break;
-		case 2 : (*(Bit16u*)&activecb->cache.wmapmask[mapidx])+=0x0101; break;
-		case 4 : (*(Bit32u*)&activecb->cache.wmapmask[mapidx])+=0x01010101; break;
-	}
+	        case 2:
+		        host_addw(activecb->cache.wmapmask + mapidx, 0x0101);
+		        break;
+
+	        case 4:
+		        host_addd(activecb->cache.wmapmask + mapidx, 0x01010101);
+		        break;
+	        }
 }
 
 // fetch a byte, val points to the code location if possible,
