323a728478dedb5b7461bc0657cbb5521738903d
kcgen <1557255+kcgen@users.noreply.github.com>
2020-06-07 19:08:38 -0700

    Inform on unexpected control characters in batch files
    This commit informs the user when an illegal character is
    encounter in a batch file.  It does not change the actual
    parsing or handling of the illegal character, which is to
    skip the character.
    
    Because some control characters are invisible when TYPE'd
    on the command line or viewed in an editor, this messaging
    will alert users that something is (very likely) amiss in
    their batch file and could be the cause for unexpected
    behavior or parsing.

--- src/shell/shell_batch.cpp
+++ src/shell/shell_batch.cpp
@@ -78,7 +78,8 @@ emptyline:
 				//So we continue reading till EOL/EOF
 				if (((cmd_write - temp) + 1) < (CMD_MAXLINE - 1))
 					*cmd_write++ = c;
-			}
+			} else if (c != 10 && c != 13)
+				shell->WriteOut(MSG_Get("SHELL_ILLEGAL_CONTROL_CHARACTER"), c, c);
 		}
 	} while (c!='\n' && n);
 	*cmd_write=0;
@@ -181,7 +182,8 @@ again:
 			if (c>31) {
 				if (((cmd_write - cmd_buffer) + 1) < (CMD_MAXLINE - 1))
 					*cmd_write++ = c;
-			}
+			} else if (c != 10 && c != 13)
+				shell->WriteOut(MSG_Get("SHELL_ILLEGAL_CONTROL_CHARACTER"), c, c);
 		}
 	} while (c!='\n' && n);
 	*cmd_write++ = 0;
