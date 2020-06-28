62ebc306d853ba4b3335dfa45137f54ac5073859
Wengier <wengierwu@yahoo.com>
2020-06-27 21:08:36 -0400

    more

--- src/dos/dos_misc.cpp
+++ src/dos/dos_misc.cpp
@@ -188,7 +188,7 @@ static bool DOS_MultiplexFunctions(void) {
 		psp_mcb.GetFileName(psp_name);
 		printf("psp_name %s reg_sp %X mem_readw(SegPhys(ss)+reg_sp) %X\n", psp_name, reg_sp, mem_readw(SegPhys(ss)+reg_sp));
 		// Report Windows version 4.0 (95) to NESTICLE x.xx so that it uses LFN when available
-		if (uselfn && (!strcmp(psp_name, "NESTICLE") || reg_sp == 0x220A && mem_readw(SegPhys(ss)+reg_sp)/0x100 == 0x1F)) {
+		if (uselfn && (!strcmp(psp_name, "NESTICLE") || (reg_sp == 0x220A && mem_readw(SegPhys(ss)+reg_sp)/0x100 == 0x1F))) {
 			reg_ax = 0;
 			reg_bx = 0x400;
 			reg_cx = 2;
