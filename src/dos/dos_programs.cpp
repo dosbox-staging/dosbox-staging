944166e5fdfe3e102ce308c258cee8f9f79119dc
MasterO2 <43938622+MasterO2@users.noreply.github.com>
2020-09-07 11:02:59 -0400

    Added ShowUsage to Invoke Mount Long Help Message

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -417,12 +417,8 @@ public:
 		if (type == "floppy") incrementFDD();
 		return;
 showusage:
-#if defined (WIN32)
-	   WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"d:\\dosprogs","d:\\dosprogs");
-#else
-	   WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"~/dosprogs","~/dosprogs");
-#endif
-		return;
+	WriteOut(MSG_Get("SHELL_CMD_MOUNT_HELP_LONG"));
+	return;
 	}
 };
 
@@ -1598,11 +1594,6 @@ void DOS_SetupPrograms(void) {
 	MSG_Add("PROGRAM_MOUNT_ERROR_2","%s isn't a directory\n");
 	MSG_Add("PROGRAM_MOUNT_ILL_TYPE","Illegal type %s\n");
 	MSG_Add("PROGRAM_MOUNT_ALREADY_MOUNTED","Drive %c already mounted with %s\n");
-	MSG_Add("PROGRAM_MOUNT_USAGE",
-		"Usage \033[34;1mMOUNT Drive-Letter Local-Directory\033[0m\n"
-		"For example: MOUNT c %s\n"
-		"This makes the directory %s act as the C: drive inside DOSBox.\n"
-		"The directory has to exist.\n");
 	MSG_Add("PROGRAM_MOUNT_UMOUNT_NOT_MOUNTED","Drive %c isn't mounted.\n");
 	MSG_Add("PROGRAM_MOUNT_UMOUNT_SUCCESS","Drive %c has successfully been removed.\n");
 	MSG_Add("PROGRAM_MOUNT_UMOUNT_NO_VIRTUAL","Virtual Drives can not be unMOUNTed.\n");
