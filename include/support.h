7844bcfb006304314214d1441e0cc27cb2af6d07
Patryk Obara <dreamer.tan@gmail.com>
2020-09-09 23:18:00 +0200

    Add UTs for safe_strcpy function
    Update documentation, add more asserts.
    
    This function is critical for prevention of buffer overflows, therefore
    I feel justified in adding some death tests as well.

--- include/support.h
+++ include/support.h
@@ -105,25 +105,36 @@ inline int iround(double x) {
 // Use (void) to silent unused warnings.
 // https://en.cppreference.com/w/cpp/error/assert
 
-/// Copy a string into C array
-///
-/// This function copies string pointed by src to fixed-size buffer dst.
-/// At most N bytes from src are copied, where N is size of dst.
-/// If exactly N bytes are copied, then terminating null byte is put
-/// into buffer, thus buffer overrun is prevented.
-///
-/// Function returns pointer to buffer to be compatible with std::strcpy.
-///
-/// Usage:
-///
-///     char buffer[2];
-///     safe_strcpy(buffer, "abc");
-///     // buffer is filled with "a"
-
-template<size_t N>
-char * safe_strcpy(char (& dst)[N], const char * src) noexcept {
+/* Copy a string into C array
+ *
+ * This function copies string pointed by src to a fixed-size buffer dst.
+ * At most N bytes from src are copied, where N is size of dst. If exactly
+ * N bytes are copied, then terminating null byte is put into dst, thus
+ * buffer overrun is prevented.
+ *
+ * Function returns pointer to buffer to be compatible with strcpy.
+ *
+ * This is a safer drop-in replacement for strcpy function (when used to fill
+ * buffers, whose size is known at compilation time), however some caveats
+ * still apply:
+ *
+ * - src cannot be null, otherwise the behaviour is undefined
+ * - dst and src strings must not overlap, otherwise the behaviour is undefined
+ * - src string must be null-terminated, otherwise the behaviour is undefined
+ *
+ * Usage:
+ *
+ *     char buffer[2];
+ *     safe_strcpy(buffer, "abc");
+ *     // buffer is filled with "a"
+ */
+template <size_t N>
+char *safe_strcpy(char (&dst)[N], const char *src) noexcept
+{
+	assert(src != nullptr);
+	assert(src < &dst[0] || src > &dst[N - 1]);
 	snprintf(dst, N, "%s", src);
-	return & dst[0];
+	return &dst[0];
 }
 
 template<size_t N>
---
+++ tests/support.cpp
@@ -0,0 +1,73 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2020-2020  The dosbox-staging team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "support.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+namespace {
+
+TEST(SafeStrcpy, SimpleCopy)
+{
+	char buffer[10] = "";
+	char *ret_value = safe_strcpy(buffer, "abc");
+	EXPECT_EQ(ret_value, &buffer[0]);
+	EXPECT_STREQ("abc", buffer);
+}
+
+TEST(SafeStrcpy, CopyFromNonArray)
+{
+	char buffer[10] = "";
+	std::string str = "abc";
+	EXPECT_STREQ("abc", safe_strcpy(buffer, str.c_str()));
+}
+
+TEST(SafeStrcpy, EmptyStringOverwrites)
+{
+	char buffer[4] = "abc";
+	EXPECT_STREQ("", safe_strcpy(buffer, ""));
+}
+
+TEST(SafeStrcpy, StringLongerThanBuffer)
+{
+	char buffer[5] = "";
+	char long_input[] = "1234567890";
+	ASSERT_LT(ARRAY_LEN(buffer), strlen(long_input));
+	EXPECT_STREQ("1234", safe_strcpy(buffer, long_input));
+}
+
+TEST(SafeStrcpyDeathTest, PassNull)
+{
+	char buf[] = "12345678";
+	EXPECT_DEBUG_DEATH({ safe_strcpy(buf, nullptr); }, "");
+}
+
+TEST(SafeStrcpyDeathTest, ProtectFromCopyingOverlappingString)
+{
+	char buf[] = "12345678";
+	char *overlapping = &buf[2];
+	ASSERT_LE(buf, overlapping);
+	ASSERT_LE(overlapping, buf + ARRAY_LEN(buf));
+	EXPECT_DEBUG_DEATH({ safe_strcpy(buf, overlapping); }, "");
+}
+
+} // namespace
