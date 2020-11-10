482c407e85caac007881089d5512dcf66b65bfd9
Patryk Obara <dreamer.tan@gmail.com>
2020-11-10 20:31:59 +0100

    Add LS program stub to replace LS command
    Making LS a program prevents the problem with invoking programs called
    ls.com or ls.exe - one game, that has such executable name is
    Links LS 1997.
    
    This change introduces a small "regression" - command parameters cannot
    be passed without space, as for DOS intenal commands, e.g.:
    
      dir.txt - equivalent of dir *.txt (usually used for e.g. dir/w)
      ls.txt - does not work any more - but works exactly like GNU ls, the
               first parameter is passed after a space.
    
    The stub invokes existing LS command implementation (as LS shares some
    code with DIR command).  When shared code will be made
    public/non-static, the rest of LS command implementation will be moved
    to a new program file.
    
    Fixes: #504

---
+++ src/dos/program_ls.cpp
@@ -0,0 +1,40 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Copyright (C) 2020-2020  The dosbox-staging team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "program_ls.h"
+
+#include <string>
+
+#include "shell.h"
+#include "support.h"
+
+void LS::Run()
+{
+	std::string tmp = "";
+	cmd->GetStringRemain(tmp);
+	char args[CMD_MAXLINE];
+	safe_strcpy(args, tmp.c_str());
+	first_shell->CMD_LS(args);
+}
+
+void LS_ProgramStart(Program **make)
+{
+	*make = new LS;
+}
