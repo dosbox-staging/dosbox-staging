c76aeac9e33510793ec691059c644e53c2a203b7
kcgen <1557255+kcgen@users.noreply.github.com>
2020-12-13 17:37:27 -0800

    Add C_MT32EMU guards around MT-32 header and source

--- src/midi/midi_mt32.cpp
+++ src/midi/midi_mt32.cpp
@@ -1,3 +1,29 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2012-2020  sergm <sergm@bigmir.net>
+ *  Copyright (C) 2020       Nikos Chantziaras <realnc@gmail.com> (settings)
+ *  Copyright (C) 2020       The dosbox-staging team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "midi_mt32.h"
+
+#if C_MT32EMU
+
 #include <SDL_thread.h>
 #include <SDL_endian.h>
 #include "control.h"
@@ -294,3 +320,6 @@ void MidiHandler_mt32::renderingLoop() {
 		}
 	}
 }
+
+
+#endif // C_MT32EMU
--- src/midi/midi_mt32.h
+++ src/midi/midi_mt32.h
@@ -1,16 +1,38 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2012-2020  sergm <sergm@bigmir.net>
+ *  Copyright (C) 2020       The dosbox-staging team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #ifndef DOSBOX_MIDI_MT32_H
 #define DOSBOX_MIDI_MT32_H
 
 #include "midi_handler.h"
 
-#include "mixer.h"
+#if C_MT32EMU
 
 #define MT32EMU_API_TYPE 3
 #include <mt32emu/mt32emu.h>
-
 #if MT32EMU_VERSION_MAJOR != 2 || MT32EMU_VERSION_MINOR < 1
 #error Incompatible mt32emu library version
 #endif
+
+#include "mixer.h"
 
 struct SDL_Thread;
 
@@ -54,4 +76,6 @@ private:
 	void renderingLoop();
 };
 
-#endif /* DOSBOX_MIDI_MT32_H */
+#endif // C_MT32EMU
+
+#endif
