91c945241ff40b3814a543aa5e9d7d9abef4aa97
Patryk Obara <dreamer.tan@gmail.com>
2020-12-30 00:33:23 +0100

    Simplify loop obtaining info about mount points
    It's not necessary to start a "fake" directory listing to obtain a drive
    label - we can get it directly from the Drive object instead.

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -133,12 +133,6 @@ public:
 
 	void ListMounts()
 	{
-		char name[DOS_NAMELENGTH_ASCII];Bit32u size;Bit16u date;Bit16u time;Bit8u attr;
-		/* Command uses dta so set it to our internal dta */
-		RealPt save_dta = dos.dta();
-		dos.dta(dos.tables.tempdta);
-		DOS_DTA dta(dos.dta());
-
 		const std::string header_drive = MSG_Get("PROGRAM_MOUNT_STATUS_DRIVE");
 		const std::string header_type = MSG_Get("PROGRAM_MOUNT_STATUS_TYPE");
 		const std::string header_label = MSG_Get("PROGRAM_MOUNT_STATUS_LABEL");
@@ -159,25 +153,16 @@ public:
 
 		WriteOut(MSG_Get("PROGRAM_MOUNT_STATUS_1"));
 		print_row(header_drive, header_type, header_label);
-
 		for (int i = 0; i < term_width; i++)
 			WriteOut_NoParsing("-");
 
-		for (int d = 0; d < DOS_DRIVES; d++) {
-			if (!Drives[d]) continue;
-
-			char root[7] = {static_cast<char>('A' + d),':','\\','*','.','*',0};
-			bool ret = DOS_FindFirst(root,DOS_ATTR_VOLUME);
-			if (ret) {
-				dta.GetResult(name,size,date,time,attr);
-				DOS_FindNext(); //Mark entry as invalid
-			} else {
-				name[0] = 0;
+		for (uint8_t d = 0; d < DOS_DRIVES; d++) {
+			if (Drives[d]) {
+				print_row(std::string{drive_letter(d)},
+				          Drives[d]->GetInfo(),
+				          To_Label(Drives[d]->GetLabel()));
 			}
-			print_row(std::string{root[0]}, Drives[d]->GetInfo(),
-			          To_Label(name));
 		}
-		dos.dta(save_dta);
 	}
 
 	void Run(void) {
