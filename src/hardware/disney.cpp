7f3bc42a23b4a0840470fea3c1a6ddb6ad0852f0
kcgen <1557255+kcgen@users.noreply.github.com>
2021-01-30 08:13:52 -0800

    Re-order headers in Disney

--- src/hardware/disney.cpp
+++ src/hardware/disney.cpp
@@ -16,9 +16,10 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#include "dosbox.h"
 
 #include <string.h>
-#include "dosbox.h"
+
 #include "inout.h"
 #include "mixer.h"
 #include "pic.h"
