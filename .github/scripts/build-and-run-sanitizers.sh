3934075e78e86cfb9a4aa941a3d2548ed96adad2
Patryk Obara <dreamer.tan@gmail.com>
2021-02-01 19:00:33 +0100

    Use meson for sanitizer builds
    Replace build part in build-and-run-sanitizers.sh script without
    changing structure of the CI job too much. Unfortunately it means we
    can't run Clang-based sanitizers any more.

--- .github/scripts/build-and-run-sanitizers.sh
+++ .github/scripts/build-and-run-sanitizers.sh
@@ -11,26 +11,31 @@
 #   ./build-and-run-sanitizers.sh COMPILER VERSION SANITIZER [SANITIZER [...]]"
 #
 # Examples:
-#   ./build-and-run-sanitizers.sh clang 8 msan usan
-#   ./build-and-run-sanitizers.sh gcc 9 asan uasan usan tsan
+#   ./build-and-run-sanitizers.sh gcc address undefined
+#   ./build-and-run-sanitizers.sh clang thread
+#
+# SANITIZER values are the names supported by meson for b_sanitize build
+# option: "address", "thread", "undefined", "memory", "address,undefined"
 #
 set -euo pipefail
 
+# let build logs be printed to the output, we don't need them stored in artifacts
+set -x
+
 # Check the arguments
-if [[ "$#" -lt 3 ]]; then
-	echo "Usage: $0 COMPILER VERSION SANITIZER [SANITIZER [...]]"
-	exit 1
-fi
+#if [[ "$#" -lt 3 ]]; then
+#	echo "Usage: $0 COMPILER SANITIZER [SANITIZER [...]]"
+#	exit 1
+#fi
 
 # Defaults and arguments
 compiler="${1}"
-compiler_version="${2}"
 logs="${compiler}-logs"
-shift 2
+shift 1
 sanitizers=("$@")
 
 # Move to the top of our source directory
-cd "$(dirname "${0}")/../.."
+cd "$(git rev-parse --show-toplevel)"
 
 # Make a directory to hold our build and run output
 mkdir -p "${logs}"
@@ -40,18 +45,16 @@ export LSAN_OPTIONS="suppressions=.lsan-suppress:verbosity=0"
 
 for sanitizer in "${sanitizers[@]}"; do
 
-	# Build DOSBox for each sanitizer
-	time ./scripts/build.sh \
-		--compiler "${compiler}" \
-		--version-postfix "${compiler_version}" \
-		--build-type "${sanitizer}" \
-		&> "${logs}/${compiler}-${sanitizer}-compile.log"
+	# Build for each sanitizer
+	meson setup -Db_sanitize="${sanitizer}" "${sanitizer}-build"
+	ninja -C "${sanitizer}-build"
 
 	# Exercise the testcase(s) for each sanitizer
 	# Sanitizers return non-zero if one or more issues were found,
 	# so we or-to-true to ensure our script doesn't end here.
-	time xvfb-run ./src/dosbox -c "autotype -w 0.1 e x i t enter" \
-		&> "${logs}/${compiler}-${sanitizer}-EnterExit.log" || true
+	time xvfb-run "./${sanitizer}-build/dosbox" \
+		-c "autotype -w 0.1 e x i t enter" \
+		&> "${logs}/${sanitizer}-EnterExit.log" || true
 
 done
 
