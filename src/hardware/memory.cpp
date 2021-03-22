39d18e081a3d35b2d10e0152b0de90aa8aac7672
Patryk Obara <dreamer.tan@gmail.com>
2021-03-22 06:32:54 +0100

    Fix format-pedantic warning when logging memory base

--- src/hardware/memory.cpp
+++ src/hardware/memory.cpp
@@ -574,7 +574,7 @@ public:
 		}
 		memset((void*)MemBase, 0, memsize * 1024 * 1024);
 		memory.pages = (memsize * 1024 * 1024) / 4096;
-		LOG_MSG("MEMORY: Base address: %p", MemBase);
+		LOG_MSG("MEMORY: Base address: %p", static_cast<void *>(MemBase));
 		LOG_MSG("MEMORY: Using %d DOS memory pages (%u MiB)",
 		        static_cast<int>(memory.pages), memsize);
 
