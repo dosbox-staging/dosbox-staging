a970a3b18140a4bcf4203dc7b63b4646205bd205
ripsaw8080 <ripsaw8080@users.sourceforge.net>
2021-06-06 02:39:14 +0000

    Fix regression in r4444: Hercules machine type cursor translation.
    git-svn-id: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4457

--- src/ints/int10_modes.cpp
+++ src/ints/int10_modes.cpp
@@ -507,10 +507,7 @@ static void FinishSetMode(bool clearmem) {
 
 	// Set cursor shape
 	if (CurMode->type==M_TEXT) {
-		if (machine==MCH_HERC)
-			INT10_SetCursorShape(0x0c,0x0d);
-		else
-			INT10_SetCursorShape(0x06,0x07);
+		INT10_SetCursorShape(0x06,0x07);
 	}
 	// Set cursor pos for page 0..7
 	for (Bit8u ct=0;ct<8;ct++) INT10_SetCursorPos(0,0,ct);
