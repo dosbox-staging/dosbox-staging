6c2f279e19941521e40512aea772416a87cfab16
kcgen <1557255+kcgen@users.noreply.github.com>
2021-07-14 14:43:35 -0700

    fixup! Return 8-bit data in read_crtc_data_other in VGA other

--- src/hardware/vga_other.cpp
+++ src/hardware/vga_other.cpp
@@ -132,14 +132,11 @@ static uint8_t read_crtc_data_other(Bitu /*port*/, Bitu /*iolen*/)
 	case 0x02:		//Horizontal sync position
 		return vga.other.hsyncp;
 	case 0x03:		//Horizontal and vertical sync width
-
 		// hsyncw and vsyncw should only be populated with their lower 4-bits
 		assert(vga.other.hsyncw >> 4 == 0);
 		assert(vga.other.vsyncw >> 4 == 0);
-
 		if (machine == MCH_TANDY)
-			return static_cast<uint8_t>(vga.other.hsyncw |
-			                            (vga.other.vsyncw << 4));
+			return static_cast<uint8_t>(vga.other.hsyncw | (vga.other.vsyncw << 4));
 		else
 			return vga.other.hsyncw;
 	case 0x04:		//Vertical total
