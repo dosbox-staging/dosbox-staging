e20aa8cd13f92b53d7fee26adc0d3e876beb7e34
kcgen <1557255+kcgen@users.noreply.github.com>
2021-07-18 09:31:39 -0700

    Delete the BatchFile only after exiting its function (#1154)
    The previous code had the BatchFile member function deleting
    the object itself (delete this) prior to returning from the
    object's function, which is flagged as use-after-free
    scenario when using smart-pointers (the return call operates
    from a now-deleted function).
    
    This commit simply moves the scope of the delete outside the
    funcion call.

--- src/shell/shell_batch.cpp
+++ src/shell/shell_batch.cpp
@@ -62,8 +62,9 @@ BatchFile::BatchFile(DOS_Shell *host,
 
 BatchFile::~BatchFile() {
 	delete cmd;
-	shell->bf=prev;
-	shell->echo=echo;
+	assert(shell);
+	shell->bf = prev;
+	shell->echo = echo;
 }
 
 // TODO: Refactor this sprawling function into smaller ones without GOTOs
@@ -71,8 +72,7 @@ bool BatchFile::ReadLine(char * line) {
 	//Open the batchfile and seek to stored postion
 	if (!DOS_OpenFile(filename.c_str(),(DOS_NOT_INHERIT|OPEN_READ),&file_handle)) {
 		LOG(LOG_MISC,LOG_ERROR)("ReadLine Can't open BatchFile %s",filename.c_str());
-		delete this;
-		return false;
+		return false; // Parent deletes this BatchFile on negative return
 	}
 	DOS_SeekFile(file_handle,&(this->location),DOS_SEEK_SET);
 
@@ -113,10 +113,9 @@ emptyline:
 	} while (val != LINE_FEED && bytes_read);
 	*cmd_write=0;
 	if (!bytes_read && cmd_write == temp) {
-		//Close file and delete bat file
+		// Close the file and delete this BatchFile on return
 		DOS_CloseFile(file_handle);
-		delete this;
-		return false;
+		return false; // Parent deletes this BatchFile on negative return
 	}
 	if (!strlen(temp)) goto emptyline;
 	if (temp[0]==':') goto emptyline;
