2e43907d61b4a0dc18cded9f63fdc0e7371021b7
kcgen <1557255+kcgen@users.noreply.github.com>
2021-08-02 13:35:50 -0700

    Use fixed-width IO handlers in the VGA Seq module

--- src/hardware/vga_seq.cpp
+++ src/hardware/vga_seq.cpp
@@ -23,22 +23,23 @@
 
 #define seq(blah) vga.seq.blah
 
-Bitu read_p3c4(Bitu /*port*/,Bitu /*iolen*/) {
+uint8_t read_p3c4(io_port_t, io_width_t)
+{
 	return seq(index);
 }
 
-void write_p3c4(Bitu /*port*/,Bitu val,Bitu /*iolen*/) {
-	seq(index)=val;
+void write_p3c4(io_port_t, uint8_t val, io_width_t)
+{
+	seq(index) = val;
 }
 
-void write_p3c5(Bitu /*port*/,Bitu val,Bitu iolen) {
-//	LOG_MSG("SEQ WRITE reg %X val %X",seq(index),val);
-	switch(seq(index)) {
-	case 0:		/* Reset */
-		seq(reset)=val;
-		break;
-	case 1:		/* Clocking Mode */
-		if (val!=seq(clocking_mode)) {
+void write_p3c5(io_port_t, uint8_t val, io_width_t)
+{
+	//	LOG_MSG("SEQ WRITE reg %X val %X",seq(index),val);
+	switch (seq(index)) {
+	case 0: /* Reset */ seq(reset) = val; break;
+	case 1: /* Clocking Mode */
+		if (val != seq(clocking_mode)) {
 			// don't resize if only the screen off bit was changed
 			if ((val&(~0x20))!=(seq(clocking_mode)&(~0x20))) {
 				seq(clocking_mode)=val;
@@ -76,30 +77,32 @@ void write_p3c5(Bitu /*port*/,Bitu val,Bitu iolen) {
 	case 3:		/* Character Map Select */
 		{
 			seq(character_map_select)=val;
-			Bit8u font1=(val & 0x3) << 1;
-			if (IS_VGA_ARCH) font1|=(val & 0x10) >> 4;
-			vga.draw.font_tables[0]=&vga.draw.font[font1*8*1024];
-			Bit8u font2=((val & 0xc) >> 1);
-			if (IS_VGA_ARCH) font2|=(val & 0x20) >> 5;
-			vga.draw.font_tables[1]=&vga.draw.font[font2*8*1024];
-		}
+		        auto font1 = static_cast<uint8_t>((val & 0x3) << 1);
+		        if (IS_VGA_ARCH)
+			        font1 |= (val & 0x10) >> 4;
+		        vga.draw.font_tables[0] = &vga.draw.font[font1 * 8 * 1024];
+		        Bit8u font2 = ((val & 0xc) >> 1);
+		        if (IS_VGA_ARCH)
+			        font2 |= (val & 0x20) >> 5;
+		        vga.draw.font_tables[1] = &vga.draw.font[font2 * 8 * 1024];
+	}
+	/*
+	        0,1,4  Selects VGA Character Map (0..7) if bit 3 of the character
+	                        attribute is clear.
+	        2,3,5  Selects VGA Character Map (0..7) if bit 3 of the character
+	                        attribute is set.
+	        Note: Character Maps are placed as follows:
+	        Map 0 at 0k, 1 at 16k, 2 at 32k, 3: 48k, 4: 8k, 5: 24k, 6: 40k, 7: 56k
+	*/
+	break;
+	case 4: /* Memory Mode */
 		/*
-			0,1,4  Selects VGA Character Map (0..7) if bit 3 of the character
-					attribute is clear.
-			2,3,5  Selects VGA Character Map (0..7) if bit 3 of the character
-					attribute is set.
-			Note: Character Maps are placed as follows:
-			Map 0 at 0k, 1 at 16k, 2 at 32k, 3: 48k, 4: 8k, 5: 24k, 6: 40k, 7: 56k
-		*/
-		break;
-	case 4:	/* Memory Mode */
-		/* 
-			0  Set if in an alphanumeric mode, clear in graphics modes.
-			1  Set if more than 64kbytes on the adapter.
-			2  Enables Odd/Even addressing mode if set. Odd/Even mode places all odd
-				bytes in plane 1&3, and all even bytes in plane 0&2.
-			3  If set address bit 0-1 selects video memory planes (256 color mode),
-				rather than the Map Mask and Read Map Select Registers.
+		        0  Set if in an alphanumeric mode, clear in graphics modes.
+		        1  Set if more than 64kbytes on the adapter.
+		        2  Enables Odd/Even addressing mode if set. Odd/Even mode places all odd
+		                bytes in plane 1&3, and all even bytes in plane 0&2.
+		        3  If set address bit 0-1 selects video memory planes (256 color mode),
+		                rather than the Map Mask and Read Map Select Registers.
 		*/
 		seq(memory_mode)=val;
 		if (IS_VGA_ARCH) {
@@ -111,7 +114,7 @@ void write_p3c5(Bitu /*port*/,Bitu val,Bitu iolen) {
 		break;
 	default:
 		if (svga.write_p3c5) {
-			svga.write_p3c5(seq(index), val, iolen);
+			svga.write_p3c5(seq(index), val, io_width_t::byte);
 		} else {
 			LOG(LOG_VGAMISC,LOG_NORMAL)("VGA:SEQ:Write to illegal index %2X",seq(index));
 		}
@@ -119,41 +122,30 @@ void write_p3c5(Bitu /*port*/,Bitu val,Bitu iolen) {
 	}
 }
 
-
-Bitu read_p3c5(Bitu /*port*/,Bitu iolen) {
-//	LOG_MSG("VGA:SEQ:Read from index %2X",seq(index));
-	switch(seq(index)) {
-	case 0:			/* Reset */
-		return seq(reset);
-		break;
-	case 1:			/* Clocking Mode */
-		return seq(clocking_mode);
-		break;
-	case 2:			/* Map Mask */
-		return seq(map_mask);
-		break;
-	case 3:			/* Character Map Select */
-		return seq(character_map_select);
-		break;
-	case 4:			/* Memory Mode */
-		return seq(memory_mode);
-		break;
+uint8_t read_p3c5(io_port_t, io_width_t)
+{
+	//	LOG_MSG("VGA:SEQ:Read from index %2X",seq(index));
+	switch (seq(index)) {
+	case 0: /* Reset */ return seq(reset); break;
+	case 1: /* Clocking Mode */ return seq(clocking_mode); break;
+	case 2: /* Map Mask */ return seq(map_mask); break;
+	case 3: /* Character Map Select */ return seq(character_map_select); break;
+	case 4: /* Memory Mode */ return seq(memory_mode); break;
 	default:
 		if (svga.read_p3c5)
-			return svga.read_p3c5(seq(index), iolen);
+			return svga.read_p3c5(seq(index), io_width_t::byte);
 		break;
 	}
 	return 0;
 }
 
-
 void VGA_SetupSEQ(void) {
 	if (IS_EGAVGA_ARCH) {
-		IO_RegisterWriteHandler(0x3c4,write_p3c4,IO_MB);
-		IO_RegisterWriteHandler(0x3c5,write_p3c5,IO_MB);
+		IO_RegisterWriteHandler(0x3c4, write_p3c4, io_width_t::byte);
+		IO_RegisterWriteHandler(0x3c5, write_p3c5, io_width_t::byte);
 		if (IS_VGA_ARCH) {
-			IO_RegisterReadHandler(0x3c4,read_p3c4,IO_MB);
-			IO_RegisterReadHandler(0x3c5,read_p3c5,IO_MB);
+			IO_RegisterReadHandler(0x3c4, read_p3c4, io_width_t::byte);
+			IO_RegisterReadHandler(0x3c5, read_p3c5, io_width_t::byte);
 		}
 	}
 }
