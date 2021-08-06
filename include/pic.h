50be733d8d3a1eb7347a3c707d89fd8478747959
kcgen <1557255+kcgen@users.noreply.github.com>
2021-08-05 20:46:18 -0700

    Use doubles exclusively in PIC_ API

--- include/pic.h
+++ include/pic.h
@@ -19,6 +19,9 @@
 #ifndef DOSBOX_PIC_H
 #define DOSBOX_PIC_H
 
+#include <cassert>
+#include <cmath>
+#include <cstdint>
 
 /* CPU Cycle Timing */
 extern int32_t CPU_Cycles;
@@ -35,27 +38,28 @@ extern uint32_t PIC_IRQCheck;
 extern uint32_t PIC_Ticks;
 
 // The number of cycles not done yet (ND)
-static INLINE int32_t PIC_TickIndexND()
+static inline int32_t PIC_TickIndexND()
 {
 	return CPU_CycleMax - CPU_CycleLeft - CPU_Cycles;
 }
 
 // Returns the percent cycles completed within the current "millisecond tick" of
 // the CPU
-static INLINE float PIC_TickIndex()
+static inline double PIC_TickIndex()
 {
-	return static_cast<float>(PIC_TickIndexND()) /
-	       static_cast<float>(CPU_CycleMax);
+	return static_cast<double>(PIC_TickIndexND()) / static_cast<double>(CPU_CycleMax);
 }
 
-static INLINE int32_t PIC_MakeCycles(float amount)
+static inline int32_t PIC_MakeCycles(double amount)
 {
-	return static_cast<int32_t>(static_cast<float>(CPU_CycleMax) * amount);
+	const auto cycles = static_cast<double>(CPU_CycleMax) * amount;
+	assert(cycles >= INT32_MIN && cycles <= INT32_MAX);
+	return static_cast<int32_t>(cycles);
 }
 
-static INLINE float PIC_FullIndex()
+static inline double PIC_FullIndex()
 {
-	return static_cast<float>(PIC_Ticks) + PIC_TickIndex();
+	return static_cast<double>(PIC_Ticks) + PIC_TickIndex();
 }
 
 void PIC_ActivateIRQ(uint8_t irq);
@@ -65,7 +69,7 @@ void PIC_runIRQs();
 bool PIC_RunQueue();
 
 //Delay in milliseconds
-void PIC_AddEvent(PIC_EventHandler handler, float delay, uint32_t val = 0);
+void PIC_AddEvent(PIC_EventHandler handler, double delay, uint32_t val = 0);
 void PIC_RemoveEvents(PIC_EventHandler handler);
 void PIC_RemoveSpecificEvents(PIC_EventHandler handler, uint32_t val);
 
