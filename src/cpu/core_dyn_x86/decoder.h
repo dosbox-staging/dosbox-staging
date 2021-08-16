d37baef422000eea4a085d5fc33ae9908c439f4c
Kirk <kklobe@gmail.com>
2021-08-16 11:40:49 -0500

    Optimize cache flushing

--- src/cpu/core_dyn_x86/decoder.h
+++ src/cpu/core_dyn_x86/decoder.h
@@ -2896,7 +2896,8 @@ finish_block:
 	/* Setup the correct end-address */
 	decode.active_block->page.end=--decode.page.index;
 	dyn_mem_execute(cache_addr, cache_bytes);
-	dyn_cache_invalidate(cache_addr, cache_bytes);
+	const size_t cache_flush_bytes = static_cast<size_t>(decode.block->cache.size);
+	dyn_cache_invalidate(cache_addr, cache_flush_bytes);
 	assert(decode.block->cache.size <= cache_bytes);
 	//	LOG_MSG("Created block size %d start %d end
 	//%d",decode.block->cache.size,decode.block->page.start,decode.block->page.end);
