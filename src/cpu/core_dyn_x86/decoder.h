aa95a98a2e5a8052eca994a7140df56e7d62b248
Kirk <kklobe@gmail.com>
2021-08-16 21:23:24 -0500

    Install latest Meson via pip3

--- src/cpu/core_dyn_x86/decoder.h
+++ src/cpu/core_dyn_x86/decoder.h
@@ -2180,7 +2180,8 @@ static CacheBlock * CreateCacheBlock(CodePageHandler * codepage,PhysPt start,Bit
 	decode.block->page.start=decode.page.index;
 	codepage->AddCacheBlock(decode.block);
 
-	auto cache_addr = static_cast<void *>(const_cast<uint8_t *>(decode.block->cache.start-1));
+	auto cache_addr = static_cast<void *>(
+	        const_cast<uint8_t *>(decode.block->cache.start - 1));
 	constexpr size_t cache_bytes = CACHE_MAXSIZE;
 
 	dyn_mem_write(cache_addr, cache_bytes);
@@ -2896,7 +2897,7 @@ finish_block:
 	/* Setup the correct end-address */
 	decode.active_block->page.end=--decode.page.index;
 	dyn_mem_execute(cache_addr, cache_bytes);
-	const size_t cache_flush_bytes = static_cast<size_t>(decode.block->cache.size);
+	const auto cache_flush_bytes = static_cast<size_t>(decode.block->cache.size);
 	dyn_cache_invalidate(cache_addr, cache_flush_bytes);
 	assert(decode.block->cache.size <= cache_bytes);
 	//	LOG_MSG("Created block size %d start %d end
