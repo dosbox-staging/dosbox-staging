9c2fadff229b6873c32244076d5e60b7e4a22201
kcgen <1557255+kcgen@users.noreply.github.com>
2021-08-24 08:41:09 -0700

    Add a safe strlen utility function

--- tests/string_utils_tests.cpp
+++ tests/string_utils_tests.cpp
@@ -120,4 +120,29 @@ TEST(SafeStrcpyDeathTest, ProtectFromCopyingOverlappingString)
 	EXPECT_DEBUG_DEATH({ safe_strcpy(buf, overlapping); }, "");
 }
 
+TEST(SafeStrlen, Simple)
+{
+	char buffer[] = "1234";
+	EXPECT_EQ(4, safe_strlen(buffer));
+}
+
+TEST(SafeStrlen, EmptyString)
+{
+	char buffer[] = "";
+	EXPECT_EQ(0, safe_strlen(buffer));
+}
+
+TEST(SafeStrlen, Uninitialized)
+{
+	char buffer[4];
+	EXPECT_GT(sizeof(buffer), safe_strlen(buffer));
+}
+
+TEST(SafeStrlen, FixedSize)
+{
+	constexpr size_t N = 5;
+	char buffer[N] = "1234";
+	EXPECT_EQ(N - 1, safe_strlen(buffer));
+}
+
 } // namespace
