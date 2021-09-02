64dbc9e2946497bd3536a7b35a49ca759855138d
kcgen <1557255+kcgen@users.noreply.github.com>
2021-09-02 11:35:41 -0700

    Make the 24-bit RGB type self-documenting

--- include/rgb24.h
+++ include/rgb24.h
@@ -1,20 +1,38 @@
 #ifndef DOSBOX_RGB24_H
 #define DOSBOX_RGB24_H
 
-typedef struct rgb24
-{	
-	protected:
-	unsigned char byte[3];
-	
-	public:
-	rgb24() {}
-	rgb24(const rgb24& val) {
-		*this = val; }
+typedef struct rgb24 {
+protected:
+	uint8_t red = 0;
+	uint8_t green = 0;
+	uint8_t blue = 0;
 
-	operator int () const {
-		return (byte[2]<<16)|(byte[1]<<8)|(byte[0]<<0); }
-	int operator& (const rgb24& val) const	{
-		return (char)*this & (char)val; }
+public:
+	constexpr rgb24() {}
+	constexpr rgb24(const rgb24 &val) { *this = val; }
+	constexpr rgb24(const uint8_t r, const uint8_t g, const uint8_t b)
+	        : red(r),
+	          green(g),
+	          blue(b)
+	{}
+
+	constexpr rgb24 &operator=(const rgb24 &) = default;
+
+	constexpr operator int() const
+	{
+		return (blue << 16) | (green << 8) | (red << 0);
+	}
+
+	constexpr static rgb24 byteswap(const rgb24 &in)
+	{
+		return rgb24(in.blue, in.green, in.red);
+	}
+
 } rgb24;
+
+constexpr rgb24 host_to_le(const rgb24 &in) noexcept
+{
+	return rgb24::byteswap(in);
+}
 
 #endif
