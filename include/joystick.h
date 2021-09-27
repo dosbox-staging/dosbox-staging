5bba33aa6f63332bf0925b6fa0582c3d03de6af0
kcgen <1557255+kcgen@users.noreply.github.com>
2021-09-27 16:18:10 -0700

    Add and use a new visible_to_dos state for joysticks

--- include/joystick.h
+++ include/joystick.h
@@ -29,7 +29,7 @@ void JOYSTICK_Button(uint8_t which, int num, bool pressed);
 void JOYSTICK_Move_X(uint8_t which, int16_t x_val);
 void JOYSTICK_Move_Y(uint8_t which, int16_t y_val);
 
-bool JOYSTICK_IsEnabled(uint8_t which);
+bool JOYSTICK_IsAccessible(uint8_t which);
 
 bool JOYSTICK_GetButton(uint8_t which, int num);
 
@@ -40,15 +40,16 @@ double JOYSTICK_GetMove_Y(uint8_t which);
 void JOYSTICK_ParseConfiguredType();
 
 enum JoystickType {
-	JOY_UNSET,
-	JOY_DISABLED, // joystick subsystem is fully disabled (won't even query)
-	JOY_NONE,     // joystick subsystem will be available for mapping only
-	JOY_AUTO,
-	JOY_2AXIS,
-	JOY_4AXIS,
-	JOY_4AXIS_2,
-	JOY_FCS,
-	JOY_CH
+	JOY_UNSET = 1 << 0,
+	JOY_NONE_FOUND = 1 << 1,       // Not a conf option; only set during auto-setup
+	JOY_DISABLED = 1 << 2,         // SDL's joystick subsystem left uninitialized
+	JOY_ONLY_FOR_MAPPING = 1 << 3, // Hidden from DOS, but still mappable
+	JOY_AUTO = 1 << 4,             // Specific type is determined during auto-setup
+	JOY_2AXIS = 1 << 5,
+	JOY_4AXIS = 1 << 6,
+	JOY_4AXIS_2 = 1 << 7,
+	JOY_FCS = 1 << 8,
+	JOY_CH = 1 << 9,
 };
 
 extern JoystickType joytype;
