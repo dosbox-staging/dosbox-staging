e77cf59191afa65c1318de3ce9fb29915aaa2b63
Brandon Roberts <brandon@bxroberts.org>
2021-10-17 20:17:45 -0700

    Add dos files tests + cleanup

---
+++ tests/dos_files_tests.cpp
@@ -0,0 +1,56 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2020-2021  The DOSBox Staging Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* This sample shows how to write a simple unit test for dosbox-staging using
+ * Google C++ testing framework.
+ *
+ * Read Google Test Primer for reference of most available features, macros,
+ * and guidance about writing unit tests:
+ *
+ * https://github.com/google/googletest/blob/master/googletest/docs/primer.md#googletest-primer
+ */
+
+/* Include necessary header files; order of headers should be as follows:
+ *
+ * 1. Header declaring functions/classes being tested
+ * 2. <gtest/gtest.h>, which declares the testing framework
+ * 3. Additional system headers (if needed)
+ * 4. Additional dosbox-staging headers (if needed)
+ */
+
+#include "dos_inc.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+// Open anonymous namespace (this is Google Test requirement)
+
+namespace {
+
+TEST(DOS_MakeName, FailOnNull)
+{
+	Bit8u drive;
+    char fulldir[DOS_PATHLENGTH];
+    bool result = DOS_MakeName("\0",fulldir,&drive);
+    EXPECT_EQ(false, result);
+}
+
+} // namespace
