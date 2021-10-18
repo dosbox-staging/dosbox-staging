6643d5c30e579899beffe5f0ed8b64571b317488
ripsaw8080 <ripsaw8080@users.sourceforge.net>
2021-10-18 13:28:26 +0000

    Register values changed by the EXEC load overlay function; fixes FM music in a Chinese RPG. Improve stack check for wrap-around cases.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4467

--- src/dos/dos_execute.cpp
+++ src/dos/dos_execute.cpp
@@ -409,7 +409,12 @@ bool DOS_Execute(char * name,PhysPt block_pt,Bit8u flags) {
 		SetupCMDLine(pspseg,block);
 	};
 	CALLBACK_SCF(false);		/* Carry flag cleared for caller if successfull */
-	if (flags==OVERLAY) return true;			/* Everything done for overlays */
+	if (flags==OVERLAY) {
+		/* Changed registers */
+		reg_ax=0;
+		reg_dx=0;
+		return true;			/* Everything done for overlays */
+	}
 	RealPt csip,sssp;
 	if (iscom) {
 		csip=RealMake(pspseg,0x100);
@@ -422,7 +427,7 @@ bool DOS_Execute(char * name,PhysPt block_pt,Bit8u flags) {
 		csip=RealMake(loadseg+head.initCS,head.initIP);
 		sssp=RealMake(loadseg+head.initSS,head.initSP);
 		if (head.initSP<4) LOG(LOG_EXEC,LOG_ERROR)("stack underflow/wrap at EXEC");
-		if ((pspseg+memsize)<(loadseg+head.initSS+(head.initSP>>4)))
+		if ((pspseg+memsize)<(RealSeg(sssp)+(RealOff(sssp)>>4)))
 			LOG(LOG_EXEC,LOG_ERROR)("stack outside memory block at EXEC");
 	}
 
