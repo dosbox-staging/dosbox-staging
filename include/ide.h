684ec54f5ead87c3d5839581d59d1b61daea75fb
kcgen <kcgen@users.noreply.github.com>
2021-12-22 07:28:19 -0800

    Add IDE detach routine with returned properties
    Authored-by: Wengier <wengierwu@yahoo.com>

--- include/ide.h
+++ include/ide.h
@@ -13,6 +13,7 @@ extern void (*ide_inits[MAX_IDE_CONTROLLERS])(Section *);
 void IDE_Auto(int8_t &index,bool &slave);
 void IDE_CDROM_Attach(int8_t index,bool slave,uint8_t drive_index);
 void IDE_CDROM_Detach(uint8_t drive_index);
+void IDE_CDROM_Detach_Ret(int8_t &indexret,bool &slaveret,int8_t drive_index);
 void IDE_Hard_Disk_Attach(int8_t index,bool slave,uint8_t bios_disk_index);
 void IDE_Hard_Disk_Detach(uint8_t bios_disk_index);
 void IDE_ResetDiskByBIOS(uint8_t disk);
