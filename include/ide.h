de1a07b930c63c755f66dc602640b54fd0eb7f14
kcgen <kcgen@users.noreply.github.com>
2021-12-22 04:58:09 -0800

    Use standard int types in IDE interfaces

--- include/ide.h
+++ include/ide.h
@@ -5,16 +5,16 @@
 #ifndef DOSBOX_IDE_H
 #define DOSBOX_IDE_H
 
-#define MAX_IDE_CONTROLLERS 	8
+constexpr int MAX_IDE_CONTROLLERS = 4;
 
 extern const char *ide_names[MAX_IDE_CONTROLLERS];
 extern void (*ide_inits[MAX_IDE_CONTROLLERS])(Section *);
 
-void IDE_Auto(signed char &index,bool &slave);
-void IDE_CDROM_Attach(signed char index,bool slave,unsigned char drive_index);
-void IDE_CDROM_Detach(unsigned char drive_index);
-void IDE_Hard_Disk_Attach(signed char index,bool slave,unsigned char bios_disk_index);
-void IDE_Hard_Disk_Detach(unsigned char bios_disk_index);
-void IDE_ResetDiskByBIOS(unsigned char disk);
+void IDE_Auto(int8_t &index,bool &slave);
+void IDE_CDROM_Attach(int8_t index,bool slave,uint8_t drive_index);
+void IDE_CDROM_Detach(uint8_t drive_index);
+void IDE_Hard_Disk_Attach(int8_t index,bool slave,uint8_t bios_disk_index);
+void IDE_Hard_Disk_Detach(uint8_t bios_disk_index);
+void IDE_ResetDiskByBIOS(uint8_t disk);
 
 #endif
