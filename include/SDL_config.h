6aa67cfe16f1f6d65905f4975bf2fd089ced9db6
Wengier <wengierwu@yahoo.com>
2022-01-21 16:03:26 -0500

    Add SDL CD header file

---
+++ include/SDL_config.h
@@ -0,0 +1,15 @@
+/** @name Frames / MSF Conversion Functions
+ *  Conversion functions from frames to Minute/Second/Frames and vice versa
+ */
+/*@{*/
+#define CD_FPS	75
+#define FRAMES_TO_MSF(f, M,S,F)	{					\
+	int value = f;							\
+	*(F) = value%CD_FPS;						\
+	value /= CD_FPS;						\
+	*(S) = value%60;						\
+	value /= 60;							\
+	*(M) = value;							\
+}
+#define MSF_TO_FRAMES(M, S, F)	((M)*60*CD_FPS+(S)*CD_FPS+(F))
+/*@}*/
