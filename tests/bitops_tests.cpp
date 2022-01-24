cf6aa526bee9ceaef4eb8f3492eeab31334c7fbb
kcgen <kcgen@users.noreply.github.com>
2022-01-23 18:52:44 -0800

    Add bit-operations helper functions
    Bit operations can often be done in different ways:
    
     - one based bits? (not recommended, industy uses zero-based)
     - zero based bits? (recommended, but many unaware of this)
     - hex numbers? (common, but hard to read for multi-bits)
     - binary-literals? (gets massive and unreadable for larger
                         bit depth: so then need to pick a different
                         style leading to inconsistencies)
     - decimals? (confusion with counter and purpose)
     - on-the-fly conversion (1 << 4) (OK, but visually messy)
    
    this attempts to standardize these operations and also make
    the code more self-documenting.
    
    The functions are "const-correct", and all can be evaluated
    at compile-time when literals are used.

--- tests/bitops_tests.cpp
+++ tests/bitops_tests.cpp
@@ -24,259 +24,275 @@
 
 namespace {
 
+using namespace bit::literals;
+
 TEST(bitops, enum_vals)
 {
 	// check against bit-shifts
-	EXPECT_FALSE(bit_0 == 0 << 0);
-	EXPECT_TRUE(bit_0 == 1 << 0); // this is why
-	EXPECT_FALSE(bit_0 == 1 << 1);
+	EXPECT_FALSE(b0 == 0 << 0);
+	EXPECT_TRUE(b0 == 1 << 0); // this is why
+	EXPECT_FALSE(b0 == 1 << 1);
 
-	EXPECT_FALSE(bit_5 == 1 << 4);
-	EXPECT_TRUE(bit_5 == 1 << 5); // industry prefers
-	EXPECT_FALSE(bit_5 == 1 << 6);
+	EXPECT_FALSE(b5 == 1 << 4);
+	EXPECT_TRUE(b5 == 1 << 5); // industry prefers
+	EXPECT_FALSE(b5 == 1 << 6);
 
-	EXPECT_FALSE(bit_12 == 1 << 11);
-	EXPECT_TRUE(bit_12 == 1 << 12); // zero-based bit names
-	EXPECT_FALSE(bit_12 == 1 << 13);
+	EXPECT_FALSE(b12 == 1 << 11);
+	EXPECT_TRUE(b12 == 1 << 12); // zero-based bit names
+	EXPECT_FALSE(b12 == 1 << 13);
 
-	EXPECT_FALSE(bit_22 == 1 << 21);
-	EXPECT_TRUE(bit_22 == 1 << 22); // and not one-based
-	EXPECT_FALSE(bit_22 == 1 << 23);
+	EXPECT_FALSE(b22 == 1 << 21);
+	EXPECT_TRUE(b22 == 1 << 22); // and not one-based
+	EXPECT_FALSE(b22 == 1 << 23);
 
-	EXPECT_FALSE(bit_31 == 1 << 30);
-	EXPECT_TRUE(bit_31 == 1 << 31);
+	EXPECT_FALSE(b31 == 1 << 30);
+	EXPECT_TRUE(b31 == 1 << 31);
 
 	// check against bit literals
-	EXPECT_TRUE(bit_0 == 0b1);
-	EXPECT_TRUE(bit_5 == 0b100000);
-	EXPECT_TRUE(bit_12 == 0b1000000000000);
-	EXPECT_TRUE(bit_22 == 0b10000000000000000000000);
-	EXPECT_TRUE(static_cast<uint32_t>(bit_14 | bit_22 | bit_31) ==
+	EXPECT_TRUE(b0 == 0b1);
+	EXPECT_TRUE(b5 == 0b100000);
+	EXPECT_TRUE(b12 == 0b1000000000000);
+	EXPECT_TRUE(b22 == 0b10000000000000000000000);
+	EXPECT_TRUE(static_cast<uint32_t>(b14 | b22 | b31) ==
 	            0b10000000010000000100000000000000);
 
 	// check against magic numbers
-	EXPECT_TRUE(bit_0 == 1);
-	EXPECT_TRUE(bit_5 == 32);
-	EXPECT_TRUE(bit_12 == 4096);
-	EXPECT_TRUE(bit_22 == 4194304);
-	EXPECT_TRUE(static_cast<uint32_t>(bit_14 | bit_22 | bit_31) == 2151694336);
+	EXPECT_TRUE(b0 == 1);
+	EXPECT_TRUE(b5 == 32);
+	EXPECT_TRUE(b12 == 4096);
+	EXPECT_TRUE(b22 == 4194304);
+	EXPECT_TRUE(static_cast<uint32_t>(b14 | b22 | b31) == 2151694336);
 
 	// check some combos
-	EXPECT_FALSE((bit_4 | bit_5) == 0b1'1000);
-	EXPECT_TRUE((bit_4 | bit_5) == 0b11'0000);
-	EXPECT_FALSE((bit_4 | bit_5) == 0b110'0000);
+	EXPECT_FALSE((b4 | b5) == 0b1'1000);
+	EXPECT_TRUE((b4 | b5) == 0b11'0000);
+	EXPECT_FALSE((b4 | b5) == 0b110'0000);
 }
 
 TEST(bitops, nominal_byte)
 {
-	const auto even_bits = bit_0 | bit_2 | bit_4 | bit_6;
-	const auto odd_bits = bit_1 | bit_3 | bit_5 | bit_7;
+	const auto even_bits = (b0 | b2 | b4 | b6);
+	const auto odd_bits = (b1 | b3 | b5 | b7);
 
 	uint8_t reg = 0;
-	set_bits(reg, odd_bits);
+	bit::set(reg, odd_bits);
 	EXPECT_EQ(reg, 0b10101010);
 
-	set_bits(reg, even_bits);
+	bit::set(reg, even_bits);
 	EXPECT_EQ(reg, 0b11111111);
 
-	EXPECT_TRUE(are_bits_set(reg, bit_0));
-	EXPECT_TRUE(are_bits_set(reg, bit_3));
-	EXPECT_TRUE(are_bits_set(reg, bit_7));
-	EXPECT_TRUE(are_bits_set(reg, even_bits));
-	EXPECT_TRUE(are_bits_set(reg, odd_bits));
+	EXPECT_TRUE(bit::is(reg, b0));
+	EXPECT_TRUE(bit::is(reg, b3));
+	EXPECT_TRUE(bit::is(reg, b7));
+	EXPECT_TRUE(bit::is(reg, even_bits));
+	EXPECT_TRUE(bit::is(reg, odd_bits));
 
-	EXPECT_FALSE(are_bits_cleared(reg, bit_0));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_3));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_7));
-	EXPECT_FALSE(are_bits_cleared(reg, even_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, b0));
+	EXPECT_FALSE(bit::cleared(reg, b3));
+	EXPECT_FALSE(bit::cleared(reg, b7));
+	EXPECT_FALSE(bit::cleared(reg, even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits));
 
-	clear_bits(reg, odd_bits); // odd-off, even-on
+	bit::clear(reg, odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
-	EXPECT_TRUE(are_bits_set(reg, even_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, odd_bits));
+	EXPECT_TRUE(bit::is(reg, even_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits));
 
-	toggle_bits(reg, odd_bits); // both are on
-	EXPECT_TRUE(are_bits_set(reg, (odd_bits | even_bits)));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	bit::flip(reg, odd_bits); // both is on
+	EXPECT_TRUE(bit::is(reg, odd_bits | even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	toggle_bits(reg, even_bits); // odd-on, even-off
-	EXPECT_TRUE(are_bits_set(reg, odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, even_bits));
+	bit::flip(reg, even_bits); // odd-on, even-off
+	EXPECT_TRUE(bit::is(reg, odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, even_bits));
 
-	toggle_bits(reg, even_bits | odd_bits); // odd-off, even-on
+	bit::flip(reg, even_bits | odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
 
 	// set all bits
-	set_all_bits(reg);
+	bit::set_all(reg);
 	EXPECT_EQ(reg, 0b1111'1111);
-	EXPECT_TRUE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_TRUE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	// toggle all bits
-	toggle_all_bits(reg);
+	// flip all bits
+	bit::flip_all(reg);
 	EXPECT_EQ(reg, 0b0000'0000);
-	EXPECT_FALSE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_FALSE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits | even_bits));
 }
 
 TEST(bitops, nominal_word)
 {
-	const auto even_bits = bit_8 | bit_10 | bit_12 | bit_14;
-	const auto odd_bits = bit_9 | bit_11 | bit_13 | bit_15;
+	const auto even_bits = (b8 | b10 | b12 | b14);
+	const auto odd_bits = (b9 | b11 | b13 | b15);
 
 	uint16_t reg = 0;
-	set_bits(reg, odd_bits);
+	bit::set(reg, odd_bits);
 	EXPECT_EQ(reg, 0b10101010'00000000);
 
-	set_bits(reg, even_bits);
+	bit::set(reg, even_bits);
 	EXPECT_EQ(reg, 0b11111111'00000000);
 
-	EXPECT_FALSE(are_bits_cleared(reg, bit_8));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_12));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_15));
-	EXPECT_FALSE(are_bits_cleared(reg, even_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, b8));
+	EXPECT_FALSE(bit::cleared(reg, b12));
+	EXPECT_FALSE(bit::cleared(reg, b15));
+	EXPECT_FALSE(bit::cleared(reg, even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits));
 
-	clear_bits(reg, odd_bits); // odd-off, even-on
+	bit::clear(reg, odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
-	EXPECT_TRUE(are_bits_set(reg, even_bits));
-	EXPECT_TRUE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, odd_bits));
+	EXPECT_TRUE(bit::is(reg, even_bits));
+	EXPECT_TRUE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits));
 
-	toggle_bits(reg, odd_bits); // both are on
-	EXPECT_TRUE(are_bits_set(reg, (odd_bits | even_bits)));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	bit::flip(reg, odd_bits); // both is on
+	EXPECT_TRUE(bit::is(reg, odd_bits | even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	toggle_bits(reg, even_bits); // odd-on, even-off
-	EXPECT_TRUE(are_bits_set(reg, odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, even_bits));
+	bit::flip(reg, even_bits); // odd-on, even-off
+	EXPECT_TRUE(bit::is(reg, odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, even_bits));
 
-	toggle_bits(reg, even_bits | odd_bits); // odd-off, even-on
+	bit::flip(reg, even_bits | odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
 
 	// set all bits
-	set_all_bits(reg);
+	bit::set_all(reg);
 	EXPECT_EQ(reg, 0b11111111'11111111);
-	EXPECT_TRUE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_TRUE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	// toggle all bits
-	toggle_all_bits(reg);
+	// flip all bits
+	bit::flip_all(reg);
 	EXPECT_EQ(reg, 0b00000000'00000000);
-	EXPECT_FALSE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_FALSE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits | even_bits));
 }
 
 TEST(bitops, nominal_dword)
 {
-	const auto even_bits = bit_16 | bit_18 | bit_20 | bit_22 | bit_24 |
-	                       bit_26 | bit_28 | bit_30;
-	const auto odd_bits = bit_17 | bit_19 | bit_21 | bit_23 | bit_25 |
-	                      bit_27 | bit_29 | bit_31;
+	const auto even_bits = (b16 | b18 | b20 | b22 | b24 | b26 | b28 | b30);
+	const auto odd_bits = (b17 | b19 | b21 | b23 | b25 | b27 | b29 | b31);
 
 	uint32_t reg = 0;
 
-	set_bits(reg, even_bits);
+	bit::set(reg, even_bits);
 	EXPECT_EQ(reg, 0b01010101'01010101'00000000'00000000);
 
-	set_bits(reg, odd_bits);
+	bit::set(reg, odd_bits);
 	EXPECT_EQ(reg, 0b11111111'11111111'00000000'00000000);
 
-	EXPECT_FALSE(are_bits_cleared(reg, bit_16));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_24));
-	EXPECT_FALSE(are_bits_cleared(reg, bit_31));
-	EXPECT_FALSE(are_bits_cleared(reg, even_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, b16));
+	EXPECT_FALSE(bit::cleared(reg, b24));
+	EXPECT_FALSE(bit::cleared(reg, b31));
+	EXPECT_FALSE(bit::cleared(reg, even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits));
 
-	clear_bits(reg, odd_bits); // odd-off, even-on
+	bit::clear(reg, odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
-	EXPECT_TRUE(are_bits_set(reg, even_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, odd_bits));
+	EXPECT_TRUE(bit::is(reg, even_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits));
 
-	toggle_bits(reg, odd_bits); // both are on
-	EXPECT_TRUE(are_bits_set(reg, (odd_bits | even_bits)));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	bit::flip(reg, odd_bits); // both is on
+	EXPECT_TRUE(bit::is(reg, odd_bits | even_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	toggle_bits(reg, even_bits); // odd-on, even-off
-	EXPECT_TRUE(are_bits_set(reg, odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, even_bits));
+	bit::flip(reg, even_bits); // odd-on, even-off
+	EXPECT_TRUE(bit::is(reg, odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, even_bits));
 
-	toggle_bits(reg, even_bits | odd_bits); // odd-off, even-on
+	bit::flip(reg, even_bits | odd_bits); // odd-off, even-on
 	EXPECT_EQ(reg, even_bits);
 
 	// set all bits
-	set_all_bits(reg);
+	bit::set_all(reg);
 	EXPECT_EQ(reg, 0b11111111'11111111'11111111'11111111);
-	EXPECT_TRUE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_TRUE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::cleared(reg, odd_bits | even_bits));
 
-	// toggle all bits
-	toggle_all_bits(reg);
+	// flip all bits
+	bit::flip_all(reg);
 	EXPECT_EQ(reg, 0b00000000'00000000'00000000'00000000);
-	EXPECT_FALSE(are_bits_set(reg, even_bits | odd_bits));
-	EXPECT_FALSE(are_any_bits_set(reg, even_bits | odd_bits));
-	EXPECT_TRUE(are_bits_cleared(reg, (odd_bits | even_bits)));
+	EXPECT_FALSE(bit::is(reg, even_bits | odd_bits));
+	EXPECT_FALSE(bit::any(reg, even_bits | odd_bits));
+	EXPECT_TRUE(bit::cleared(reg, odd_bits | even_bits));
 }
 
 TEST(bitops, bits_too_wide_for_byte)
 {
 	uint8_t reg = 0;
-	set_bits(reg, bit_7);
-	EXPECT_TRUE(are_bits_set(reg, bit_7));
-	EXPECT_DEBUG_DEATH({ set_bits(reg, bit_8); }, "");
-	EXPECT_DEBUG_DEATH({ are_bits_set(reg, bit_8); }, "");
+	bit::set(reg, b7);
+	EXPECT_TRUE(bit::is(reg, b7));
+	EXPECT_DEBUG_DEATH({ bit::set(reg, b8); }, "");
+	EXPECT_DEBUG_DEATH({ bit::is(reg, b8); }, "");
 
-	clear_bits(reg, bit_7);
-	EXPECT_TRUE(are_bits_cleared(reg, bit_7));
-	EXPECT_DEBUG_DEATH({ clear_bits(reg, bit_8); }, "");
-	EXPECT_DEBUG_DEATH({ are_bits_cleared(reg, bit_8); }, "");
+	bit::clear(reg, b7);
+	EXPECT_TRUE(bit::cleared(reg, b7));
+	EXPECT_DEBUG_DEATH({ bit::clear(reg, b8); }, "");
+	EXPECT_DEBUG_DEATH({ bit::cleared(reg, b8); }, "");
 
-	toggle_bits(reg, bit_7);
-	EXPECT_TRUE(are_bits_set(reg, bit_7));
-	EXPECT_DEBUG_DEATH({ toggle_bits(reg, bit_8); }, "");
+	bit::flip(reg, b7);
+	EXPECT_TRUE(bit::is(reg, b7));
+	EXPECT_DEBUG_DEATH({ bit::flip(reg, b8); }, "");
 }
 
 TEST(bitops, bits_too_wide_for_word)
 {
 	uint16_t reg = 0;
-	set_bits(reg, bit_8);
-	EXPECT_TRUE(are_bits_set(reg, bit_8));
-	EXPECT_DEBUG_DEATH({ set_bits(reg, bit_16); }, "");
-	EXPECT_DEBUG_DEATH({ are_bits_set(reg, bit_16); }, "");
+	bit::set(reg, b8);
+	EXPECT_TRUE(bit::is(reg, b8));
+	EXPECT_DEBUG_DEATH({ bit::set(reg, b16); }, "");
+	EXPECT_DEBUG_DEATH({ bit::is(reg, b16); }, "");
 
-	clear_bits(reg, bit_8);
-	EXPECT_TRUE(are_bits_cleared(reg, bit_8));
-	EXPECT_DEBUG_DEATH({ clear_bits(reg, bit_16); }, "");
-	EXPECT_DEBUG_DEATH({ are_bits_cleared(reg, bit_16); }, "");
+	bit::clear(reg, b8);
+	EXPECT_TRUE(bit::cleared(reg, b8));
+	EXPECT_DEBUG_DEATH({ bit::clear(reg, b16); }, "");
+	EXPECT_DEBUG_DEATH({ bit::cleared(reg, b16); }, "");
 
-	toggle_bits(reg, bit_8);
-	EXPECT_TRUE(are_bits_set(reg, bit_8));
-	EXPECT_DEBUG_DEATH({ toggle_bits(reg, bit_16); }, "");
+	bit::flip(reg, b8);
+	EXPECT_TRUE(bit::is(reg, b8));
+	EXPECT_DEBUG_DEATH({ bit::flip(reg, b16); }, "");
 }
 
 TEST(bitops, bits_not_too_wide_for_dword)
 {
 	uint32_t reg = 0;
-	set_bits(reg, bit_8);
-	set_bits(reg, bit_24);
-	set_bits(reg, bit_31);
-	EXPECT_TRUE(are_bits_set(reg, (bit_8 | bit_24 | bit_31)));
+	bit::set(reg, b8);
+	bit::set(reg, b24);
+	bit::set(reg, b31);
+	EXPECT_TRUE(bit::is(reg, b8 | b24 | b31));
 
-	clear_bits(reg, bit_8);
-	clear_bits(reg, bit_24);
-	clear_bits(reg, bit_31);
-	EXPECT_TRUE(are_bits_cleared(reg, (bit_8 | bit_24 | bit_31)));
+	bit::clear(reg, b8);
+	bit::clear(reg, b24);
+	bit::clear(reg, b31);
+	EXPECT_TRUE(bit::cleared(reg, b8 | b24 | b31));
 
-	toggle_bits(reg, bit_8);
-	toggle_bits(reg, bit_24);
-	toggle_bits(reg, bit_31);
-	EXPECT_TRUE(are_bits_set(reg, (bit_8 | bit_24 | bit_31)));
+	bit::flip(reg, b8);
+	bit::flip(reg, b24);
+	bit::flip(reg, b31);
+	EXPECT_TRUE(bit::is(reg, b8 | b24 | b31));
+}
+
+// Masking operations
+TEST(bitops, masking)
+{
+	using namespace bit;
+
+	// Prepopulated register with bit 4 cleared:
+	constexpr uint8_t reg = (b0 | b1 | b2 | b3 /*| b4*/ | b5 | b6 | b7);
+
+	EXPECT_TRUE(is(mask_on(reg, b4), b4));
+	EXPECT_TRUE(cleared(mask_off(reg, b4), b4));
+	EXPECT_TRUE(any(mask_to(reg, true, b4), b4));
+	EXPECT_TRUE(cleared(mask_to(reg, false, b4), b4));
+	EXPECT_TRUE(any(mask_flip(reg, b4), b4));
+	EXPECT_TRUE(is(mask_flip_all(reg), b4));
 }
 
 } // namespace
