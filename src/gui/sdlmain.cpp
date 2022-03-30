f8171fd658a7bab5d0b9f8d0e182ffcfc47cb0bc
kekko <kekko@mail.com>
2022-03-30 00:07:07 -0400

    Add resizable Voodoo OpenGL mode

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -76,6 +76,10 @@
 #include "video.h"
 
 #if C_OPENGL
+#include "../hardware/voodoo.h"
+#include "../hardware/voodoo_types.h"
+#include "../hardware/voodoo_data.h"
+#include "../hardware/voodoo_opengl.h"
 //Define to report opengl errors
 //#define DB_OPENGL_ERROR
 
@@ -1454,8 +1458,8 @@ SDL_Window* GFX_SetSDLWindowMode(uint16_t width, uint16_t height, SCREEN_TYPES s
 			return nullptr;
 		}
 		SetTransparency();
+        SDL_SetWindowResizable(sdl.window, SDL_TRUE);
         GFX_SetTitle(-1, -1, false); //refresh title.
-        sdl.surface = SDL_GetWindowSurface(sdl.window);
         SDL_GetWindowSize(sdl.window, &currWidth, &currHeight);
         sdl.update_display_contents = ((width == currWidth) && (height == currHeight));
 
@@ -1466,6 +1470,8 @@ SDL_Window* GFX_SetSDLWindowMode(uint16_t width, uint16_t height, SCREEN_TYPES s
             if (SDL_GL_MakeCurrent(sdl.window, sdl.opengl.context) != 0) LOG_MSG("WARNING: SDL2 unable to make current GL context");
         }
 #endif
+        sdl.surface = SDL_GetWindowSurface(sdl.window);
+
 
         return sdl.window;
     }
@@ -1477,7 +1483,7 @@ SDL_Window* GFX_SetSDLWindowMode(uint16_t width, uint16_t height, SCREEN_TYPES s
      * if one is not interested in scaling.
      * On Android, desktop res is the only way.
      */
-    SDL_SetWindowResizable(sdl.window, SDL_FALSE);
+    SDL_SetWindowResizable(sdl.window, SDL_TRUE);
     if (GFX_IsFullscreen()) {
         SDL_DisplayMode displayMode;
         SDL_GetWindowDisplayMode(sdl.window, &displayMode);
@@ -3966,6 +3972,13 @@ void GFX_RegenerateWindow(Section *sec) {
 
 static void HandleVideoResize(int width, int height)
 {
+    if (sdl.desktop.lazy_fullscreen) {
+        new_height = height;
+        new_width = width;
+        voodoo_ogl_update_dimensions();
+        return;
+    }
+
 	/* Maybe a screen rotation has just occurred, so we simply resize.
 	There may be a different cause for a forced resized, though.    */
 	if (sdl.desktop.full.display_res && sdl.desktop.fullscreen) {
