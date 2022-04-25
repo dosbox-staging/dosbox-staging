3a6863923d1b9c9b4fc36d9dc530f9ed097f2b96
Wengier <wengierwu@yahoo.com>
2022-04-24 23:24:26 -0400

    Move PPScale class to ppscale.h

--- src/libs/ppscale/ppscale.h
+++ src/libs/ppscale/ppscale.h
@@ -38,4 +38,53 @@ int pp_scale /* magnify an image in a pixel-perfect manner */
 }
 #endif
 
+class PPScale {
+public:
+	PPScale(const int source_w,
+	        const int source_h,
+	        const double aspect_ratio,
+	        const bool source_is_doubled,
+	        const int canvas_w,
+	        const int canvas_h)
+
+	{
+		assert(source_w > 0);
+		assert(source_h > 0);
+		assert(canvas_w > 0);
+		assert(canvas_h > 0);
+		assert(aspect_ratio > 0.1);
+
+		// divide the source if it's been doubled
+		effective_source_w = source_w / (source_is_doubled ? 2 : 1);
+		effective_source_h = source_h / (source_is_doubled ? 2 : 1);
+
+		// call the pixel-perfect library to get the scale factors
+		constexpr double aspect_weight = 1.14; // pixel-perfect constant
+		const int err = pp_getscale(effective_source_w, effective_source_h,
+		                            aspect_ratio, canvas_w, canvas_h,
+		                            aspect_weight, &scale_x, &scale_y);
+
+		// if the scaling failed, ensure the the multipliers are valid
+		if (err) {
+			scale_x = 1;
+			scale_y = 1;
+		}
+		// calculate the output dimensions
+		output_w = effective_source_w * scale_x;
+		output_h = effective_source_h * scale_y;
+		assert(output_w > 0);
+		assert(output_h > 0);
+	}
+	// default constructor
+	PPScale() {}
+
+	int effective_source_w = 0;
+	int effective_source_h = 0;
+	int output_w = 0;
+	int output_h = 0;
+private:
+	int scale_x = 0;
+	int scale_y = 0;
+};
+
 #endif /* PPSCALE_H */
