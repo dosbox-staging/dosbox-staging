eb1a8ad158d94e511abb44f1f88a1e9142a110d1
Kirk <kklobe@gmail.com>
2022-04-30 20:09:27 -0500

    Integrate modified PDCurses for debugger

--- src/debug/debug_gui.cpp
+++ src/debug/debug_gui.cpp
@@ -34,6 +34,10 @@
 #include "debug.h"
 #include "debug_inc.h"
 
+#if !PDCURSES
+#error SYSTEM CURSES INCLUDED, SHOULD BE PDCURSES
+#endif
+
 struct _LogGroup {
 	char const *front = nullptr;
 	bool enabled = false;
@@ -294,12 +298,8 @@ void DBGUI_StartUp(void) {
 	noecho();       /* don't echo input */
 	nodelay(dbg.win_main,true);
 	keypad(dbg.win_main,true);
-	#ifndef WIN32
-	printf("\033[8;50;80t");
-	fflush(NULL);
-	resizeterm(50,80);
+	resize_term(50,80);
 	touchwin(dbg.win_main);
-	#endif
 	old_cursor_state = curs_set(0);
 	start_color();
 	cycle_count=0;
