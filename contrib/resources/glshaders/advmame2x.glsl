461c2ebd8f9592d027aa5b7de76e2acf89d31a0b
John Novak <john@johnovak.net>
2022-05-12 22:31:11 +1000

    Use gamma-correct interpolation in shaders that benefit from it

--- contrib/resources/glshaders/advmame2x.glsl
+++ contrib/resources/glshaders/advmame2x.glsl
@@ -25,6 +25,9 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#pragma use_srgb_texture
+#pragma use_srgb_framebuffer
+
 varying vec2 v_texCoord;
 uniform sampler2D rubyTexture;
 uniform vec2 rubyInputSize;
