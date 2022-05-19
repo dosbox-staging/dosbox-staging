f3c835745a7f2e47e30cf2ba20cf503726b2c850
John Novak <jnovak@skedulo.com>
2022-05-20 08:31:41 +1000

    Use the mixer rate by default for GUS

--- src/hardware/gus.cpp
+++ src/hardware/gus.cpp
@@ -602,7 +602,7 @@ Gus::Gus(uint16_t port, uint8_t dma, uint8_t irq, const std::string &ultradir)
 	// Register the Audio and DMA channels
 	const auto mixer_callback = std::bind(&Gus::AudioCallback, this,
 	                                      std::placeholders::_1);
-	audio_channel = MIXER_AddChannel(mixer_callback, 1, "GUS");
+	audio_channel = MIXER_AddChannel(mixer_callback, 0, "GUS");
 
 	// Let the mixer command adjust the GUS's internal amplitude level's
 	const auto set_level_callback = std::bind(&Gus::SetLevelCallback, this, _1);
