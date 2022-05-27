749183e6322137b33eee78333d68d7a611fbe4be
kcgen <kcgen@users.noreply.github.com>
2022-05-27 12:03:36 -0700

    Don't markdown lint 3rd party directories

--- scripts/verify-markdown.sh
+++ scripts/verify-markdown.sh
@@ -3,18 +3,22 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
 # Copyright (C) 2020-2021  kcgen <kcgen@users.noreply.github.com>
- 
-# This script exists only to easily run mdl (markdownlint) on all
-# *.md files in the repo.
 #
-# You can pass additional parameters to this script itself, e.g.:
+# This is a convenient helper to run mdl (markdownlint) on the
+# *.md files in the repo, except for those inside directories
+# that are likely to hold 3rd party documentation.
+#
+# You can pass additional mdl arguments to this script itself, e.g.:
 #
 #   ./verify-markdown.sh --verbose --json
 
 set -euo pipefail
 
 list_markdown_files () {
-	git ls-files | grep '.md$'
+	git ls-files -- \
+	  '*.md' \
+	  ':!:src/libs/*.md' \
+	  ':!:contrib/resources/*.md'
 }
 
 main () {
