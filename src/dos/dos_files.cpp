f8e2632cdb69b7ef73485ab621693a900b2e10b6
ripsaw8080 <ripsaw8080@users.sourceforge.net>
2022-05-28 00:23:50 +0000

    Fix "Invalid Access Code" error when opening files. Preserve last error code when no error occurs.
    Imported-from: https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@4476

--- src/dos/dos_files.cpp
+++ src/dos/dos_files.cpp
@@ -566,8 +566,12 @@ bool DOS_OpenFile(char const * name,Bit8u flags,Bit16u * entry,bool fcb) {
 	if (device) {
 		Files[handle]=new DOS_Device(*Devices[devnum]);
 	} else {
+		Bit16u olderror=dos.errorcode;
+		dos.errorcode=0;
 		exists=Drives[drive]->FileOpen(&Files[handle],fullname,flags);
 		if (exists) Files[handle]->SetDrive(drive);
+		if (dos.errorcode) return false;
+		dos.errorcode=olderror;
 	}
 	if (exists || device ) { 
 		Files[handle]->AddRef();
@@ -760,9 +764,10 @@ bool DOS_CreateTempFile(char * const name,Bit16u * entry) {
 			tempname++;
 		}
 	}
+	Bit16u olderror=dos.errorcode;
 	dos.errorcode=0;
 	/* add random crap to the end of the name and try to open */
-	srand(time(0));
+	srand(static_cast<unsigned int>(time(NULL)));
 	do {
 		Bit32u i;
 		for (i=0;i<8;i++) {
@@ -772,6 +777,7 @@ bool DOS_CreateTempFile(char * const name,Bit16u * entry) {
 	} while (DOS_FileExists(name));
 	DOS_CreateFile(name,0,entry);
 	if (dos.errorcode) return false;
+	dos.errorcode=olderror;
 	return true;
 }
 
