f3e3d739a35f14a3cca4afe0aceee21218a90daf
John Novak <john@johnovak.net>
2022-06-11 21:11:39 +1000

    Do no expose implementation details in the AdlibGold interface
    Further simplify implementation & cleanup

--- src/hardware/adlib_gold.h
+++ src/hardware/adlib_gold.h
@@ -25,51 +25,14 @@
 #include "mixer.h"
 
 #include "../libs/TDA8425_emu/TDA8425_emu.h"
-#include "../libs/YM7128B_emu/YM7128B_emu.h"
 
-class AdlibGoldSurroundProcessor {
-public:
-	AdlibGoldSurroundProcessor(const uint16_t sample_rate);
-	~AdlibGoldSurroundProcessor();
-
-	void ControlWrite(const uint8_t val) noexcept;
-	AudioFrame Process(const AudioFrame &frame) noexcept;
-
-	// prevent copying and assignment
-	AdlibGoldSurroundProcessor(const AdlibGoldSurroundProcessor &) = delete;
-	AdlibGoldSurroundProcessor &operator=(const AdlibGoldSurroundProcessor &) = delete;
-
-private:
-	YM7128B_ChipIdeal *chip = nullptr;
-
-	struct {
-		uint8_t sci  = 0;
-		uint8_t a0   = 0;
-		uint8_t addr = 0;
-		uint8_t data = 0;
-	} control_state = {};
-};
-
-class AdlibGoldStereoProcessor {
-public:
-	AdlibGoldStereoProcessor(const uint16_t sample_rate);
-	~AdlibGoldStereoProcessor();
-
-	void Reset() noexcept;
-	void ControlWrite(const TDA8425_Reg reg, const TDA8425_Register data) noexcept;
-	AudioFrame Process(const AudioFrame &frame) noexcept;
-
-	// prevent copying and assignment
-	AdlibGoldStereoProcessor(const AdlibGoldStereoProcessor &) = delete;
-	AdlibGoldStereoProcessor &operator=(const AdlibGoldStereoProcessor &) = delete;
-
-private:
-	TDA8425_Chip *chip = nullptr;
-};
+class SurroundProcessor;
+class StereoProcessor;
 
 class AdlibGold {
 public:
 	AdlibGold(const uint16_t sample_rate);
+	~AdlibGold();
 
 	void SurroundControlWrite(const uint8_t val) noexcept;
 	void StereoControlWrite(const TDA8425_Reg reg,
@@ -78,8 +41,8 @@ public:
 	void Process(const int16_t *in, const uint32_t frames, float *out) noexcept;
 
 private:
-	std::unique_ptr<AdlibGoldSurroundProcessor> surround_processor;
-	std::unique_ptr<AdlibGoldStereoProcessor> stereo_processor;
+	std::unique_ptr<SurroundProcessor> surround_processor;
+	std::unique_ptr<StereoProcessor> stereo_processor;
 };
 
 #endif
