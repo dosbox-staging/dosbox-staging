4211688d97227695b8dcc9b00a1eb7ab3c07600e
John Novak <jnovak@skedulo.com>
2022-06-20 22:04:36 +1000

    Rename adlib.cpp & adlib.h to opl.cpp & opl.h

--- src/hardware/opl.cpp
+++ src/hardware/opl.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2002-2021  The DOSBox Team
+ *  Copyright (C) 2002-2022  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "adlib.h"
+#include "opl.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -790,3 +790,4 @@ void OPL_ShutDown(Section * /*sec*/)
 	delete opl;
 	opl = nullptr;
 }
+
