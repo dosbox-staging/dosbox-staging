bdefb35293fc030c9b0b6a58dbc9be182175c34c
Kirk <kklobe@gmail.com>
2022-07-30 14:24:48 -0500

    Use int instead of char
    char can be unsigned by default on some platforms,
    causing the first condition in DEBUG_RefreshPage()
    to always be false.

--- include/debug.h
+++ include/debug.h
@@ -27,7 +27,7 @@ bool DEBUG_IntBreakpoint(uint8_t intNum);
 void DEBUG_Enable(bool pressed);
 void DEBUG_CheckExecuteBreakpoint(uint16_t seg, uint32_t off);
 bool DEBUG_ExitLoop(void);
-void DEBUG_RefreshPage(char scroll);
+void DEBUG_RefreshPage(int scroll);
 Bitu DEBUG_EnableDebugger(void);
 
 extern Bitu cycle_count;
