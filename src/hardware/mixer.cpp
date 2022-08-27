502a6fdc8f24bd2f933ec48df346a432f6baee2b
John Novak <jnovak@skedulo.com>
2022-08-27 21:49:45 +1000

    Refine error message

--- src/hardware/mixer.cpp
+++ src/hardware/mixer.cpp
@@ -849,7 +849,7 @@ bool MixerChannel::TryParseAndSetCustomFilter(const std::string &filter_prefs)
 
 	if (!(single_filter || dual_filter)) {
 		LOG_WARNING("%s: Invalid custom filter definition: '%s'. Must be "
-		            "specified in \"lfp|hpf <ORDER> <CUTOFF_FREQUENCY>\" format",
+		            "specified in \"lfp|hpf ORDER CUTOFF_FREQUENCY\" format",
 		            name.c_str(),
 		            filter_prefs.c_str());
 		return false;
