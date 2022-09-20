ccff42a55f30633a409f45faf1c57e7011e65aa7
FeralChild64 <>
2022-09-20 17:48:18 +0200

    Drop non-UTF8 translations

--- contrib/resources/translations/encode.sh
+++ contrib/resources/translations/encode.sh
@@ -32,45 +32,28 @@ pushd "$trans_dir" > /dev/null || exit
 echo "In directory $trans_dir:"
 
 # (default)
-# keyb us 437
-tconv_l CP437 "utf-8/en.txt" en
-nconv_t CP437 "utf-8/en.txt" en.utf8
+# keyb us
+nconv_t CP437 "utf-8/en.txt" en
 
 # keyb de
-# keyb de 850
-tconv_l CP850 "utf-8/de.txt" de
-nconv_t UTF-8 "utf-8/de.txt" de.utf8
+nconv_t UTF-8 "utf-8/de.txt" de
 
 # keyb es
-# keyb es 850
-tconv_l CP850 "utf-8/es.txt" es
-nconv_t UTF-8 "utf-8/es.txt" es.utf8
+nconv_t UTF-8 "utf-8/es.txt" es
 
 # keyb fr
-# keyb fr 850
-tconv_l CP850 "utf-8/fr.txt" fr
-nconv_t UTF-8 "utf-8/fr.txt" fr.utf8
+nconv_t UTF-8 "utf-8/fr.txt" fr
 
 # keyb it
-# keyb it 850
-tconv_l CP850 "utf-8/it.txt" it
-nconv_t UTF-8 "utf-8/it.txt" it.utf8
+nconv_t UTF-8 "utf-8/it.txt" it
 
 # keyb nl
-# keyb nl 850
-# keyb nl 858
-tconv_l CP850 "utf-8/nl.txt" nl
+nconv_t UTF-8 "utf-8/nl.txt" nl
 
 # keyb pl
-# keyb pl 852
-# keyb pl 437
-tconv_l CP852 "utf-8/pl.txt" pl
-tconv_t CP437 "utf-8/pl.txt" pl.cp437
-nconv_t UTF-8 "utf-8/pl.txt" pl.utf8
+nconv_t UTF-8 "utf-8/pl.txt" pl
 
 # keyb ru
-# keyb ru 866
-tconv_l CP866 "utf-8/ru.txt" ru
-nconv_t UTF-8 "utf-8/ru.txt" ru.utf8
+nconv_t UTF-8 "utf-8/ru.txt" ru
 
 popd > /dev/null || exit
