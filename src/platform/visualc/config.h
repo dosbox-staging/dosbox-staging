abc0897cd16d651300339f54f9b7a482223d9d33
kcgen <kcgen@users.noreply.github.com>
2022-09-22 16:11:57 -0700

    Codify the use of Semantic Versioning

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -1,4 +1,10 @@
-/* Version number of package */
+// Emulator Semantic Version (MAJOR.MINOR.PATCH), incremented as follows:
+//  - MAJOR version when you make incompatible API changes
+//  - MINOR version when you add functionality in a backwards compatible manner
+//  - PATCH version when you make backwards compatible bug fixes
+// Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+// Ref: https://semver.org/
+
 #define VERSION "0.79.1"
 
 /* This macro is going to be overriden via CI */
