87b9bf4807ea1276c3844766f23d8311af2ab12e
kcgen <kcgen@users.noreply.github.com>
2022-09-25 09:20:05 -0700

    Move the uptime logic from the shell and into a function (#1967)

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -408,9 +408,18 @@ static void DOSBOX_RealInit(Section * sec) {
 	VGA_SetRatePreference(section->Get_string("dos_rate"));
 }
 
-void DOSBOX_Init() {
-	Section_prop * secprop;
-	Prop_int* Pint;
+// Returns decimal seconds of elapsed uptime.
+// The first call starts the uptime counter (and returns 0.0 seconds of uptime).
+double DOSBOX_GetUptime()
+{
+	static auto start_ms = GetTicks();
+	return GetTicksSince(start_ms) / millis_in_second;
+}
+
+void DOSBOX_Init()
+{
+	Section_prop *secprop;
+	Prop_int *Pint;
 	Prop_int *pint = nullptr;
 	Prop_hex* Phex;
 	Prop_string* Pstring; // use pstring when touching properties
@@ -1232,6 +1241,11 @@ void DOSBOX_Init() {
 	        "# This is the configuration file for " CANONICAL_PROJECT_NAME " (%s).\n"
 	        "# Lines starting with a '#' character are comments.\n");
 	MSG_Add("CONFIG_SUGGESTED_VALUES", "Possible values");
+
+	// Initialize the uptime counter when launching the first shell. This
+	// ensures that slow-performing configurable tasks (like loading MIDI
+	// SF2 files) have already been performed and won't affect this time.
+	(void)DOSBOX_GetUptime();
 
 	control->SetStartUp(&SHELL_Init);
 }
