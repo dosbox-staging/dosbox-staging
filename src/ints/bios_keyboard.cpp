c67a7b648a583234b2da634ea3b0b34c0ea7d02c
Ricardo <rsribeiro@users.noreply.github.com>
2022-11-11 11:15:32 -0300

    Remove ctrl modifier from international 1 key

--- src/ints/bios_keyboard.cpp
+++ src/ints/bios_keyboard.cpp
@@ -126,7 +126,7 @@ static struct {
       { 0x565c, 0x567c,   none,   none }, /* (102-key) */
       { 0x8500, 0x8700, 0x8900, 0x8b00 }, /* F11 */
       { 0x8600, 0x8800, 0x8a00, 0x8c00 }, /* F12 */
-      { 0x352f, 0x353f,   none, 0x35f0 }  /* international 1 */
+      { 0x352f, 0x353f,   none,   none }  /* international 1 */
       };
 
 bool BIOS_AddKeyToBuffer(uint16_t code) {
