121032ec06c16c8796e35e5af35826667f286705
John Novak <jnovak@skedulo.com>
2022-12-19 15:32:21 +1000

    Reorganise shaders into subdirectories

--- contrib/resources/glshaders/none.glsl
+++ contrib/resources/glshaders/none.glsl
@@ -26,17 +26,25 @@
 #pragma use_srgb_framebuffer
 
 varying vec2 v_texCoord;
+
 #if defined(VERTEX)
+
 uniform vec2 rubyTextureSize;
 uniform vec2 rubyInputSize;
+
 attribute vec4 a_position;
+
 void main() {
-	gl_Position = a_position;
-	v_texCoord = vec2(a_position.x+1.0,1.0-a_position.y)/2.0*rubyInputSize/rubyTextureSize;
+    gl_Position = a_position;
+    v_texCoord = vec2(a_position.x + 1.0, 1.0 - a_position.y) / 2.0 * rubyInputSize / rubyTextureSize;
 }
+
 #elif defined(FRAGMENT)
+
 uniform sampler2D rubyTexture;
+
 void main() {
 	gl_FragColor = texture2D(rubyTexture, v_texCoord);
 }
+
 #endif
