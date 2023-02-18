b0994ef9020d9a629f9c5121b525945f5d146ebc
FeralChild64 <unknown>
2023-02-18 16:51:24 +0100

    Move keyboard and mouse code to a common directory

--- src/hardware/serialport/serialmouse.cpp
+++ src/hardware/serialport/serialmouse.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2022-2022  The DOSBox Staging Team
+ *  Copyright (C) 2022-2023  The DOSBox Staging Team
  *  Copyright (C) 2002-2021  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -30,7 +30,7 @@
 #include "checks.h"
 #include "math_utils.h"
 
-#include "../mouse/mouse_interfaces.h"
+#include "../input/mouse_interfaces.h"
 
 CHECK_NARROWING();
 
