6e50eb5ab7ce8edd7fac61d554794d47495b8d7d
Kappa971 <62349018+Kappa971@users.noreply.github.com>
2023-02-26 14:55:50 +0100

    Improve text formatting

--- contrib/resources/translations/encode.bat
+++ contrib/resources/translations/encode.bat
@@ -1,40 +1,40 @@
-@echo off
+@ECHO OFF
 
-set trans_dir=%~dp0
+SET trans_dir=%~dp0
+PUSHD "%trans_dir%"
+ECHO In directory %trans_dir%:
 
-pushd "%trans_dir%"
+REM (default)
+REM keyb us
+CALL :nconv_t "utf-8\en.txt", en
 
-echo In directory %trans_dir%:
+REM keyb de
+CALL :nconv_t "utf-8\de.txt", de
 
-rem (default)
-rem keyb us
-call :nconv_t "utf-8/en.txt" , en
+REM keyb es
+CALL :nconv_t "utf-8\es.txt", es
 
-rem keyb de
-call :nconv_t "utf-8/de.txt" , de
+REM keyb fr
+CALL :nconv_t "utf-8\fr.txt", fr
 
-rem keyb es
-call :nconv_t "utf-8/es.txt" , es
+REM keyb it
+CALL :nconv_t "utf-8\it.txt", it
 
-rem keyb fr
-call :nconv_t "utf-8/fr.txt" , fr
+REM keyb nl
+CALL :nconv_t "utf-8\nl.txt", nl
 
-rem keyb it
-call :nconv_t "utf-8/it.txt" , it
+REM keyb pl
+CALL :nconv_t "utf-8\pl.txt", pl
 
-rem keyb nl
-call :nconv_t "utf-8/nl.txt" , nl
+REM keyb ru
+CALL :nconv_t "utf-8\ru.txt", ru
 
-rem keyb pl
-call :nconv_t "utf-8/pl.txt" , pl
+PAUSE
+EXIT /B %ERRORLEVEL%
 
-rem keyb ru
-call :nconv_t "utf-8/ru.txt" , ru
-
-exit
-
-rem Normalization - for UTF-8 format locale
+REM Normalization - for UTF-8 format locale
 :nconv_t
-echo Normalizing %~1 to %~2.lng
+ECHO Normalizing %~1 to %~2.lng
 tools\uconv -f UTF-8 -t UTF-8 -x nfc "%~1" > "%~2.lng"
 tools\dos2unix "%~2.lng"
+EXIT /B 0
