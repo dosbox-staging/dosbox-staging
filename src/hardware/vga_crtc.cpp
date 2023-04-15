11f25061c6c153a8bd775a2cbd3e3c9e9e5d2a97
kcgen <kcgen@users.noreply.github.com>
2023-04-15 10:49:06 -0700

    Double-scan CGA modes when using a VGA architecture

--- src/hardware/vga_crtc.cpp
+++ src/hardware/vga_crtc.cpp
@@ -145,9 +145,9 @@ void vga_write_p3d5(io_port_t, io_val_t value, io_width_t)
 		if (IS_VGA_ARCH)
 			vga.config.line_compare=(vga.config.line_compare & 0x5ff)|(val&0x40)<<3;
 
-		if (IS_VGA_ARCH && (vga.mode == M_EGA || vga.mode == M_VGA)) {
-			// When using a VGA card in EGA and VGA modes, take
-			// special care to repeat lines (excluding CGA modes)
+		// When using a VGA card, all 200-line modes are line-doubled
+		if (IS_VGA_ARCH && (vga.mode == M_VGA || vga.mode == M_EGA ||
+		                    vga.mode == M_CGA4 || vga.mode == M_CGA2)) {
 			if ((vga.crtc.maximum_scan_line ^ val) & 0x20) {
 				crtc(maximum_scan_line)=val;
 				VGA_StartResize();
