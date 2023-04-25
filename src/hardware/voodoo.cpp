1387e97562172764ad6508b52d9a4cb4649ad9bd
Patryk Obara <dreamer.tan@gmail.com>
2023-04-25 13:10:17 +0200

    Update copyright in significantly retouched files

--- src/hardware/voodoo.cpp
+++ src/hardware/voodoo.cpp
@@ -5,6 +5,7 @@
  */
 
 /*
+ *  Copyright (C) 2023       The DOSBox Staging Team
  *  Copyright (C) 2002-2011  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
