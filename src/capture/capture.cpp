d5727d15613275e6a90ae08b86d55e45e1a5c8fc
John Novak <john@johnnovak.net>
2023-05-03 22:07:18 +1000

    Initial cleanup of try_capture_image

--- src/capture/capture.cpp
+++ src/capture/capture.cpp
@@ -360,61 +360,79 @@ void try_capture_image([[maybe_unused]] int width,
 		png_color palette[256];
 
 		CaptureState &= ~CAPTURE_IMAGE;
-		/* Open the actual file */
-		FILE *fp = CAPTURE_OpenFile("Screenshot", ".png");
+
+		FILE* fp = CAPTURE_OpenFile("Screenshot", ".png");
 		if (!fp) {
 			return;
 		}
-		/* First try to allocate the png structures */
-		png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL,NULL, NULL);
+
+		png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 		if (!png_ptr) {
 			fclose(fp);
 			return;
 		}
+
 		info_ptr = png_create_info_struct(png_ptr);
 		if (!info_ptr) {
 			png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
 			fclose(fp);
 			return;
 		}
-	
-		/* Finalize the initing of png library */
+
 		png_init_io(png_ptr, fp);
-		png_set_compression_level(png_ptr,Z_BEST_COMPRESSION);
-		
-		/* set other zlib parameters */
+		png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);
+
 		png_set_compression_mem_level(png_ptr, 8);
-		png_set_compression_strategy(png_ptr,Z_DEFAULT_STRATEGY);
+		png_set_compression_strategy(png_ptr, Z_DEFAULT_STRATEGY);
 		png_set_compression_window_bits(png_ptr, 15);
 		png_set_compression_method(png_ptr, 8);
 		png_set_compression_buffer_size(png_ptr, 8192);
-	
-		if (bpp==8) {
-			png_set_IHDR(png_ptr, info_ptr, width, height,
-				8, PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE,
-				PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+		if (bpp == 8) {
+			png_set_IHDR(png_ptr,
+			             info_ptr,
+			             width,
+			             height,
+			             8,
+			             PNG_COLOR_TYPE_PALETTE,
+			             PNG_INTERLACE_NONE,
+			             PNG_COMPRESSION_TYPE_DEFAULT,
+			             PNG_FILTER_TYPE_DEFAULT);
+
 			for (auto i = 0; i < 256; ++i) {
-				palette[i].red=pal[i*4+0];
-				palette[i].green=pal[i*4+1];
-				palette[i].blue=pal[i*4+2];
+				palette[i].red   = pal[i * 4 + 0];
+				palette[i].green = pal[i * 4 + 1];
+				palette[i].blue  = pal[i * 4 + 2];
 			}
-			png_set_PLTE(png_ptr, info_ptr, palette,256);
+			png_set_PLTE(png_ptr, info_ptr, palette, 256);
+
 		} else {
-			png_set_bgr( png_ptr );
-			png_set_IHDR(png_ptr, info_ptr, width, height,
-				8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-				PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+			png_set_bgr(png_ptr);
+			png_set_IHDR(png_ptr,
+			             info_ptr,
+			             width,
+			             height,
+			             8,
+			             PNG_COLOR_TYPE_RGB,
+			             PNG_INTERLACE_NONE,
+			             PNG_COMPRESSION_TYPE_DEFAULT,
+			             PNG_FILTER_TYPE_DEFAULT);
 		}
+
 #ifdef PNG_TEXT_SUPPORTED
 		constexpr char keyword[] = "Software";
+		static_assert(sizeof(keyword) < 80, "libpng limit");
+
 		constexpr char value[] = CANONICAL_PROJECT_NAME " " VERSION;
 		constexpr int num_text = 1;
-		static_assert(sizeof(keyword) < 80, "libpng limit");
+
 		png_text texts[num_text] = {};
+
 		texts[0].compression = PNG_TEXT_COMPRESSION_NONE;
-		texts[0].key = const_cast<png_charp>(keyword);
-		texts[0].text = const_cast<png_charp>(value);
+		texts[0].key         = const_cast<png_charp>(keyword);
+		texts[0].text        = const_cast<png_charp>(value);
 		texts[0].text_length = sizeof(value);
+
 		png_set_text(png_ptr, info_ptr, texts, num_text);
 #endif
 		png_write_info(png_ptr, info_ptr);
@@ -425,72 +443,118 @@ void try_capture_image([[maybe_unused]] int width,
 		uint8_t doubleRow[SCALER_MAXWIDTH * 4];
 
 		for (auto i = 0; i < height; ++i) {
-			auto srcLine = data + (i >> row_divisor) * pitch;
+			auto srcLine    = data + (i >> row_divisor) * pitch;
 			auto rowPointer = srcLine;
+
 			switch (bpp) {
 			case 8:
 				if (is_double_width) {
-					for (auto x = 0; x < width; ++x)
-						doubleRow[x * 2 + 0] = doubleRow[x * 2 + 1] = srcLine[x];
+					for (auto x = 0; x < width; ++x) {
+						doubleRow[x * 2 + 0] =
+						        doubleRow[x * 2 + 1] = srcLine[x];
+					}
 					rowPointer = doubleRow;
 				}
 				break;
+
 			case 15:
 				if (is_double_width) {
 					for (auto x = 0; x < width; ++x) {
-						const auto pixel = host_to_le(reinterpret_cast<uint16_t *>(srcLine)[x]);
-						doubleRow[x * 6 + 0] = doubleRow[x * 6 + 3] = ((pixel & 0x001f) * 0x21) >> 2;
-						doubleRow[x * 6 + 1] = doubleRow[x * 6 + 4] = ((pixel & 0x03e0) * 0x21) >> 7;
-						doubleRow[x * 6 + 2] = doubleRow[x * 6 + 5] = ((pixel & 0x7c00) * 0x21) >> 12;
+						const auto pixel = host_to_le(
+						        reinterpret_cast<uint16_t*>(
+						                srcLine)[x]);
+
+						doubleRow[x * 6 + 0] = doubleRow[x * 6 + 3] =
+						        ((pixel & 0x001f) * 0x21) >> 2;
+						doubleRow[x * 6 + 1] = doubleRow[x * 6 + 4] =
+						        ((pixel & 0x03e0) * 0x21) >> 7;
+						doubleRow[x * 6 + 2] = doubleRow[x * 6 + 5] =
+						        ((pixel & 0x7c00) * 0x21) >> 12;
 					}
 				} else {
 					for (auto x = 0; x < width; ++x) {
-						const auto pixel = host_to_le(reinterpret_cast<uint16_t *>(srcLine)[x]);
-						doubleRow[x * 3 + 0] = ((pixel & 0x001f) * 0x21) >> 2;
-						doubleRow[x * 3 + 1] = ((pixel & 0x03e0) * 0x21) >> 7;
-						doubleRow[x * 3 + 2] = ((pixel & 0x7c00) * 0x21) >> 12;
+						const auto pixel = host_to_le(
+						        reinterpret_cast<uint16_t*>(
+						                srcLine)[x]);
+
+						doubleRow[x * 3 + 0] = ((pixel & 0x001f) *
+						                        0x21) >>
+						                       2;
+						doubleRow[x * 3 + 1] = ((pixel & 0x03e0) *
+						                        0x21) >>
+						                       7;
+						doubleRow[x * 3 + 2] = ((pixel & 0x7c00) *
+						                        0x21) >>
+						                       12;
 					}
 				}
 				rowPointer = doubleRow;
 				break;
+
 			case 16:
 				if (is_double_width) {
 					for (auto x = 0; x < width; ++x) {
-						const auto pixel = host_to_le(reinterpret_cast<uint16_t *>(srcLine)[x]);
-						doubleRow[x * 6 + 0] = doubleRow[x * 6 + 3] = ((pixel & 0x001f) * 0x21) >> 2;
-						doubleRow[x * 6 + 1] = doubleRow[x * 6 + 4] = ((pixel & 0x07e0) * 0x41) >> 9;
-						doubleRow[x * 6 + 2] = doubleRow[x * 6 + 5] = ((pixel & 0xf800) * 0x21) >> 13;
+						const auto pixel = host_to_le(
+						        reinterpret_cast<uint16_t*>(
+						                srcLine)[x]);
+						doubleRow[x * 6 + 0] = doubleRow[x * 6 + 3] =
+						        ((pixel & 0x001f) * 0x21) >> 2;
+						doubleRow[x * 6 + 1] = doubleRow[x * 6 + 4] =
+						        ((pixel & 0x07e0) * 0x41) >> 9;
+						doubleRow[x * 6 + 2] = doubleRow[x * 6 + 5] =
+						        ((pixel & 0xf800) * 0x21) >> 13;
 					}
 				} else {
 					for (auto x = 0; x < width; ++x) {
-						const auto pixel = host_to_le(reinterpret_cast<uint16_t *>(srcLine)[x]);
-						doubleRow[x * 3 + 0] = ((pixel & 0x001f) * 0x21) >> 2;
-						doubleRow[x * 3 + 1] = ((pixel & 0x07e0) * 0x41) >> 9;
-						doubleRow[x * 3 + 2] = ((pixel & 0xf800) * 0x21) >> 13;
+						const auto pixel = host_to_le(
+						        reinterpret_cast<uint16_t*>(
+						                srcLine)[x]);
+						doubleRow[x * 3 + 0] = ((pixel & 0x001f) *
+						                        0x21) >>
+						                       2;
+						doubleRow[x * 3 + 1] = ((pixel & 0x07e0) *
+						                        0x41) >>
+						                       9;
+						doubleRow[x * 3 + 2] = ((pixel & 0xf800) *
+						                        0x21) >>
+						                       13;
 					}
 				}
 				rowPointer = doubleRow;
 				break;
+
 			case 24:
 				if (is_double_width) {
 					for (auto x = 0; x < width; ++x) {
-						const auto pixel = host_to_le(reinterpret_cast<rgb24 *>(srcLine)[x]);
-						reinterpret_cast<rgb24 *>(doubleRow)[x * 2 + 0] = pixel;
-						reinterpret_cast<rgb24 *>(doubleRow)[x * 2 + 1] = pixel;
+						const auto pixel = host_to_le(
+						        reinterpret_cast<rgb24*>(
+						                srcLine)[x]);
+
+						reinterpret_cast<rgb24*>(
+						        doubleRow)[x * 2 + 0] = pixel;
+						reinterpret_cast<rgb24*>(
+						        doubleRow)[x * 2 + 1] = pixel;
+
 						rowPointer = doubleRow;
 					}
 				}
 				// There is no else statement here because
 				// rowPointer is already defined as srcLine
 				// above which is already 24-bit single row
-
 				break;
+
 			case 32:
 				if (is_double_width) {
 					for (auto x = 0; x < width; ++x) {
-						doubleRow[x * 6 + 0] = doubleRow[x * 6 + 3] = srcLine[x * 4 + 0];
-						doubleRow[x * 6 + 1] = doubleRow[x * 6 + 4] = srcLine[x * 4 + 1];
-						doubleRow[x * 6 + 2] = doubleRow[x * 6 + 5] = srcLine[x * 4 + 2];
+						doubleRow[x * 6 + 0] =
+						        doubleRow[x * 6 + 3] =
+						                srcLine[x * 4 + 0];
+						doubleRow[x * 6 + 1] =
+						        doubleRow[x * 6 + 4] =
+						                srcLine[x * 4 + 1];
+						doubleRow[x * 6 + 2] =
+						        doubleRow[x * 6 + 5] =
+						                srcLine[x * 4 + 2];
 					}
 				} else {
 					for (auto x = 0; x < width; ++x) {
@@ -502,13 +566,12 @@ void try_capture_image([[maybe_unused]] int width,
 				rowPointer = doubleRow;
 				break;
 			}
+
 			png_write_row(png_ptr, rowPointer);
 		}
-		/* Finish writing */
+
 		png_write_end(png_ptr, 0);
-		/*Destroy PNG structs*/
 		png_destroy_write_struct(&png_ptr, &info_ptr);
-		/*close file*/
 		fclose(fp);
 	}
 }
@@ -600,28 +663,35 @@ void try_capture_video([[maybe_unused]] int width,
 				switch ( bpp) {
 				case 8:
 					for (auto x = 0; x < countWidth; ++x)
-						doubleRow[x * 2 + 0] = doubleRow[x * 2 + 1] = srcLine[x];
+						doubleRow[x * 2 + 0] =
+						        doubleRow[x * 2 + 1] = srcLine[x];
 					break;
 				case 15:
 				case 16:
 					for (auto x = 0; x < countWidth; ++x)
-						((uint16_t *)doubleRow)[x*2+0] =
-						((uint16_t *)doubleRow)[x*2+1] = ((uint16_t *)srcLine)[x];
+						((uint16_t*)doubleRow)[x * 2 + 0] = ((
+						        uint16_t*)doubleRow)[x * 2 + 1] =
+						        ((uint16_t*)srcLine)[x];
 					break;
 				case 24:
 					for (auto x = 0; x < countWidth; ++x) {
-						const auto pixel = reinterpret_cast<rgb24 *>(srcLine)[x];
-						reinterpret_cast<uint32_t *>(doubleRow)[x * 2 + 0] = pixel;
-						reinterpret_cast<uint32_t *>(doubleRow)[x * 2 + 1] = pixel;
+						const auto pixel =
+						        reinterpret_cast<rgb24*>(
+						                srcLine)[x];
+						reinterpret_cast<uint32_t*>(
+						        doubleRow)[x * 2 + 0] = pixel;
+						reinterpret_cast<uint32_t*>(
+						        doubleRow)[x * 2 + 1] = pixel;
 					}
 					break;
 				case 32:
 					for (auto x = 0; x < countWidth; ++x)
-						((uint32_t *)doubleRow)[x*2+0] =
-						((uint32_t *)doubleRow)[x*2+1] = ((uint32_t *)srcLine)[x];
+						((uint32_t*)doubleRow)[x * 2 + 0] = ((
+						        uint32_t*)doubleRow)[x * 2 + 1] =
+						        ((uint32_t*)srcLine)[x];
 					break;
 				}
-                rowPointer=doubleRow;
+				rowPointer = doubleRow;
 			} else {
 				if (bpp == 24) {
 					for (auto x = 0; x < width; ++x) {
