c89ee48c335254236e855f57ad51997bfa7dfba1
John Novak <john@johnnovak.net>
2023-05-04 17:44:09 +1000

    Extract capture image code into its own file

---
+++ src/capture/capture_image.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (C) 2023  The DOSBox Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DOSBOX_CAPTURE_IMAGE_H
+#define DOSBOX_CAPTURE_IMAGE_H
+
+void capture_image(const uint16_t width, const uint16_t height,
+                   const uint8_t bits_per_pixel, const uint16_t pitch,
+                   const uint8_t capture_flags, const uint8_t* image_data,
+                   const uint8_t* palette_data);
+
+#endif
