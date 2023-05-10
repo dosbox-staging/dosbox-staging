9194e481d3598d4d80acbc93a4a127bc7448e3a4
John Novak <john@johnnovak.net>
2023-05-10 19:00:04 +1000

    Break apart capture_image into separate functions

--- src/capture/capture_image.cpp
+++ src/capture/capture_image.cpp
@@ -33,69 +33,41 @@
 #include <png.h>
 #include <zlib.h>
 
-void capture_image(const uint16_t width, const uint16_t height,
-                   const uint8_t bits_per_pixel, const uint16_t pitch,
-                   const uint8_t capture_flags, const uint8_t* image_data,
-                   const uint8_t* palette_data)
+static void write_png_info(const png_structp png_ptr, const png_infop info_ptr,
+                           const uint16_t width, const uint16_t height,
+                           const uint8_t bits_per_pixel, const uint8_t* palette_data)
 {
-/*
-	LOG_MSG("CAPTURE: Capturing image, width: %d, height: %d, "
-	        "bitsPerPixel: %d, pitch: %d, doubleWidth: %d, doubleHeight: %d",
-	        width,
-	        height,
-	        bits_per_pixel,
-	        pitch,
-	        (capture_flags & CaptureFlagsDoubleWidth),
-	        (capture_flags & CaptureFlagsDoubleHeight));
-*/
-	png_structp png_ptr    = {};
-	png_infop info_ptr     = {};
-	png_color palette[256] = {};
-
-	FILE* fp = CAPTURE_CreateFile("raw image", ".png");
-	if (!fp) {
-		return;
-	}
-
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (!png_ptr) {
-		fclose(fp);
-		return;
-	}
-
-	info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr) {
-		png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-		fclose(fp);
-		return;
-	}
-
-	png_init_io(png_ptr, fp);
 	png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);
 
+	// TODO document magic values
 	png_set_compression_mem_level(png_ptr, 8);
 	png_set_compression_strategy(png_ptr, Z_DEFAULT_STRATEGY);
 	png_set_compression_window_bits(png_ptr, 15);
 	png_set_compression_method(png_ptr, 8);
 	png_set_compression_buffer_size(png_ptr, 8192);
 
+	constexpr auto PngBitDepth = 8;
+
 	if (bits_per_pixel == 8) {
 		png_set_IHDR(png_ptr,
 		             info_ptr,
 		             width,
 		             height,
-		             8,
+		             PngBitDepth,
 		             PNG_COLOR_TYPE_PALETTE,
 		             PNG_INTERLACE_NONE,
 		             PNG_COMPRESSION_TYPE_DEFAULT,
 		             PNG_FILTER_TYPE_DEFAULT);
 
-		for (auto i = 0; i < 256; ++i) {
+		constexpr auto NumPaletteEntries     = 256;
+		png_color palette[NumPaletteEntries] = {};
+
+		for (auto i = 0; i < NumPaletteEntries; ++i) {
 			palette[i].red   = palette_data[i * 4 + 0];
 			palette[i].green = palette_data[i * 4 + 1];
 			palette[i].blue  = palette_data[i * 4 + 2];
 		}
-		png_set_PLTE(png_ptr, info_ptr, palette, 256);
+		png_set_PLTE(png_ptr, info_ptr, palette, NumPaletteEntries);
 
 	} else {
 		png_set_bgr(png_ptr);
@@ -103,7 +75,7 @@ void capture_image(const uint16_t width, const uint16_t height,
 		             info_ptr,
 		             width,
 		             height,
-		             8,
+		             PngBitDepth,
 		             PNG_COLOR_TYPE_RGB,
 		             PNG_INTERLACE_NONE,
 		             PNG_COMPRESSION_TYPE_DEFAULT,
@@ -114,7 +86,7 @@ void capture_image(const uint16_t width, const uint16_t height,
 	char keyword[] = "Software";
 	static_assert(sizeof(keyword) < 80, "libpng limit");
 
-	char value[] = CANONICAL_PROJECT_NAME " " VERSION;
+	char value[]           = CANONICAL_PROJECT_NAME " " VERSION;
 	constexpr int num_text = 1;
 
 	png_text texts[num_text] = {};
@@ -127,10 +99,17 @@ void capture_image(const uint16_t width, const uint16_t height,
 	png_set_text(png_ptr, info_ptr, texts, num_text);
 #endif
 	png_write_info(png_ptr, info_ptr);
+}
 
+static void write_png_image_data(const png_structp png_ptr, const uint16_t width,
+                                 const uint16_t height, const uint8_t bits_per_pixel,
+                                 const uint16_t pitch, const uint8_t capture_flags,
+                                 const uint8_t* image_data)
+{
 	const bool is_double_width = (capture_flags & CaptureFlagDoubleWidth);
 	const auto row_divisor = (capture_flags & CaptureFlagDoubleHeight) ? 1 : 0;
 
+	// TODO seems risky; perhaps use vector instead
 	uint8_t row_buffer[SCALER_MAXWIDTH * 4];
 
 	for (auto i = 0; i < height; ++i) {
@@ -257,8 +236,51 @@ void capture_image(const uint16_t width, const uint16_t height,
 
 		png_write_row(png_ptr, row_pointer);
 	}
+}
 
-	png_write_end(png_ptr, 0);
+void capture_image(const uint16_t width, const uint16_t height,
+                   const uint8_t bits_per_pixel, const uint16_t pitch,
+                   const uint8_t capture_flags, const uint8_t* image_data,
+                   const uint8_t* palette_data)
+{
+	LOG_MSG("CAPTURE: Capturing image, width: %d, height: %d, "
+	        "bitsPerPixel: %d, pitch: %d, doubleWidth: %d, doubleHeight: %d",
+	        width,
+	        height,
+	        bits_per_pixel,
+	        pitch,
+	        (capture_flags & CaptureFlagDoubleWidth),
+	        (capture_flags & CaptureFlagDoubleHeight));
+
+	FILE* fp = CAPTURE_CreateFile("raw image", ".png");
+	if (!fp) {
+		return;
+	}
+
+	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING,
+	                                              nullptr,
+	                                              nullptr,
+	                                              nullptr);
+	if (!png_ptr) {
+		fclose(fp);
+		return;
+	}
+
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
+		png_destroy_write_struct(&png_ptr, (png_infopp) nullptr);
+		fclose(fp);
+		return;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	write_png_info(png_ptr, info_ptr, width, height, bits_per_pixel, palette_data);
+
+	write_png_image_data(
+	        png_ptr, width, height, bits_per_pixel, pitch, capture_flags, image_data);
+
+	png_write_end(png_ptr, nullptr);
 	png_destroy_write_struct(&png_ptr, &info_ptr);
 	fclose(fp);
 }
