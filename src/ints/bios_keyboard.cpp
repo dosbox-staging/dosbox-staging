629997a37a963f323e9d62d840525b62b19fd72e
MeAreJeenius <61266239+MeAreJeenius@users.noreply.github.com>
2023-05-19 21:47:02 -0600

    Remove alt code cast check
    When using an alt code greater than 255,
    check_cast would cause an assert failure,
    crashing the program.
    
    The intended behavior is to have the alt code
    be equal to the typed code modulus 256.
    
    This is the same as reading the bottom 8 bits
    of the alt code, which static_cast does for us.

--- src/ints/bios_keyboard.cpp
+++ src/ints/bios_keyboard.cpp
@@ -420,9 +420,11 @@ static Bitu IRQ1_Handler(void) {
 		}
 		if(flags1 &0x08) {
 			const auto token    = mem_readb(BIOS_KEYBOARD_TOKEN);
-			const auto alt      = get_key_codes_for(scancode).alt & 0xff;
-			const auto combined = token * 10 + alt;
-			mem_writeb(BIOS_KEYBOARD_TOKEN, check_cast<uint8_t>(combined));
+			const auto alt      = get_key_codes_for(scancode).alt;
+			const auto combined = (token * 10 + alt) &
+			                      std::numeric_limits<uint8_t>::max();
+			mem_writeb(BIOS_KEYBOARD_TOKEN,
+			           static_cast<uint8_t>(combined));
 		} else if (flags1 & 0x04) {
 			add_key(get_key_codes_for(scancode).control);
 		} else if (((flags1 & 0x3) != 0) ^ ((flags1 & 0x20) != 0)) {
