7a01787d572e0e1b21a8ff1362a854d15ecc3ac4
John Novak <john@johnnovak.net>
2023-06-23 18:40:45 +1000

    Add unit tests for the RGB conversion functions

--- include/rgb.h
+++ include/rgb.h
@@ -27,6 +27,10 @@
 
 #include "support.h"
 
+static constexpr auto Rgb5Max = 31;
+static constexpr auto Rgb6Max = 63;
+static constexpr auto Rgb8Max = 255;
+
 //***************************************************************************
 // Conversion between 8-bit and 5/6-bit RGB values
 //***************************************************************************
@@ -35,6 +39,7 @@ constexpr uint8_t rgb6_to_8(const uint8_t c)
 {
 	// Yields identical values to `(c * 255 + 31) / 63` over the whole input
 	// range
+	assert(c <= Rgb6Max);
 	return check_cast<uint8_t>((c * 259 + 33) >> 6);
 }
 
@@ -42,6 +47,7 @@ constexpr uint8_t rgb5_to_8(const uint8_t c)
 {
 	// Yields identical values to `(c * 255 + 15) / 31` over the whole input
 	// range
+	assert(c <= Rgb5Max);
 	return check_cast<uint8_t>((c * 527 + 23) >> 6);
 }
 
@@ -55,11 +61,8 @@ constexpr uint8_t rgb8_to_5(const uint8_t c)
 	return check_cast<uint8_t>((c * 249 + 1014) >> 11);
 }
 
-static constexpr auto num_5bit_values = 32; // 2^5
-static constexpr auto num_6bit_values = 64; // 2^6
-
-using rgb5_to_8_lut_t = std::array<uint8_t, num_5bit_values>;
-using rgb6_to_8_lut_t = std::array<uint8_t, num_6bit_values>;
+using rgb5_to_8_lut_t = std::array<uint8_t, Rgb5Max + 1>;
+using rgb6_to_8_lut_t = std::array<uint8_t, Rgb6Max + 1>;
 
 static constexpr rgb5_to_8_lut_t generate_rgb5_to_8_lut()
 {
@@ -81,14 +84,14 @@ static constexpr rgb6_to_8_lut_t generate_rgb6_to_8_lut()
 
 inline uint8_t rgb5_to_8_lut(const uint8_t c)
 {
-	assert(c < num_5bit_values);
+	assert(c <= Rgb5Max);
 	static constexpr auto lut = generate_rgb5_to_8_lut();
 	return lut[c];
 }
 
 inline uint8_t rgb6_to_8_lut(const uint8_t c)
 {
-	assert(c < num_6bit_values);
+	assert(c <= Rgb6Max);
 	static constexpr auto lut = generate_rgb6_to_8_lut();
 	return lut[c];
 }
@@ -114,9 +117,7 @@ inline float linear_to_srgb(const float c)
 // LUT-backed 8-bit sRGB to linear RGB conversion
 //***************************************************************************
 
-constexpr auto num_8bit_values = 256; // 2^8
-
-using srgb8_to_lin_lut_t = std::array<float, num_8bit_values>;
+using srgb8_to_lin_lut_t = std::array<float, Rgb8Max + 1>;
 
 static srgb8_to_lin_lut_t generate_srgb8_to_lin_lut()
 {
@@ -141,17 +142,15 @@ inline float srgb8_to_linear_lut(const uint8_t c)
 // LUT-backed linear RGB to 8-bit sRGB conversion
 //***************************************************************************
 
-static constexpr auto max_8bit_value        = 255;
-static constexpr auto lin_to_srgb8_lut_size = 16384;
+static constexpr auto LinToSrgb8LutSize = 16384;
 
-using lin_to_srgb8_lut_t = std::array<uint8_t, lin_to_srgb8_lut_size>;
+using lin_to_srgb8_lut_t = std::array<uint8_t, LinToSrgb8LutSize>;
 
 static uint16_t lin_to_srgb8_lut_key(float c)
 {
-	const auto key = static_cast<uint16_t>(
-	        round(c * (lin_to_srgb8_lut_size - 1)));
+	const auto key = static_cast<uint16_t>(round(c * (LinToSrgb8LutSize - 1)));
 
-	assert(key < lin_to_srgb8_lut_size);
+	assert(key < LinToSrgb8LutSize);
 	return key;
 }
 
@@ -159,10 +158,10 @@ static lin_to_srgb8_lut_t generate_lin_to_srgb8_lut()
 {
 	lin_to_srgb8_lut_t lut = {};
 
-	for (auto i = 0; i < lin_to_srgb8_lut_size; ++i) {
-		const auto lin  = (1.0f / (lin_to_srgb8_lut_size - 1)) * i;
+	for (auto i = 0; i < LinToSrgb8LutSize; ++i) {
+		const auto lin  = (1.0f / (LinToSrgb8LutSize - 1)) * i;
 		const auto key  = lin_to_srgb8_lut_key(lin);
-		const auto srgb = linear_to_srgb(lin) * max_8bit_value;
+		const auto srgb = linear_to_srgb(lin) * Rgb8Max;
 
 		lut[key] = static_cast<uint8_t>(round(srgb));
 	}
---
+++ tests/rgb_tests.cpp
@@ -0,0 +1,126 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2023-2023  The DOSBox Staging Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "rgb.h"
+
+#include <gtest/gtest.h>
+
+constexpr uint8_t rgb5_to_8_reference(const uint8_t c)
+{
+	return check_cast<uint8_t>((c * 255 + 15) / 31);
+}
+
+constexpr uint8_t rgb6_to_8_reference(const uint8_t c)
+{
+	return check_cast<uint8_t>((c * 255 + 31) / 63);
+}
+
+constexpr auto AbsError = 0.000001f;
+
+TEST(rgb, rgb5_to_8)
+{
+	for (auto c = 0; c <= Rgb5Max; ++c) {
+		EXPECT_EQ(rgb5_to_8(c), rgb5_to_8_reference(c));
+	}
+}
+
+TEST(rgb, rgb6_to_8)
+{
+	for (auto c = 0; c <= Rgb6Max; ++c) {
+		EXPECT_EQ(rgb6_to_8(c), rgb6_to_8_reference(c));
+	}
+}
+
+TEST(rgb, rgb8_to_5)
+{
+	for (auto c = 0; c <= Rgb5Max; ++c) {
+		const auto c8 = rgb5_to_8_reference(c);
+		EXPECT_EQ(rgb8_to_5(c8), c);
+	}
+}
+
+TEST(rgb, rgb8_to_6)
+{
+	for (auto c = 0; c <= Rgb6Max; ++c) {
+		const auto c8 = rgb6_to_8_reference(c);
+		EXPECT_EQ(rgb8_to_6(c8), c);
+	}
+}
+
+TEST(rgb, rgb5_to_8_lut)
+{
+	for (auto c = 0; c <= Rgb5Max; ++c) {
+		EXPECT_EQ(rgb5_to_8_lut(c), rgb5_to_8_reference(c));
+	}
+}
+
+TEST(rgb, rgb6_to_8_lut)
+{
+	for (auto c = 0; c <= Rgb6Max; ++c) {
+		EXPECT_EQ(rgb6_to_8_lut(c), rgb6_to_8_reference(c));
+	}
+}
+
+TEST(rgb, srgb_to_linear)
+{
+	EXPECT_NEAR(srgb_to_linear(0.0f), 0.0000000f, AbsError);
+	EXPECT_NEAR(srgb_to_linear(0.2f), 0.0331048f, AbsError);
+	EXPECT_NEAR(srgb_to_linear(0.8f), 0.6038270f, AbsError);
+	EXPECT_NEAR(srgb_to_linear(1.0f), 1.0000000f, AbsError);
+}
+
+TEST(rgb, linear_to_srgb)
+{
+	EXPECT_NEAR(linear_to_srgb(0.0000000f), 0.0f, AbsError);
+	EXPECT_NEAR(linear_to_srgb(0.0331048f), 0.2f, AbsError);
+	EXPECT_NEAR(linear_to_srgb(0.6038270f), 0.8f, AbsError);
+	EXPECT_NEAR(linear_to_srgb(1.0000000f), 1.0f, AbsError);
+}
+
+TEST(rgb, srgb_linear_roundtrip)
+{
+	constexpr auto NumIter = 1000;
+	for (auto i = 0; i < NumIter; ++i) {
+		const float srgb1 = i * (1.0f / NumIter);
+		const auto lin    = srgb_to_linear(srgb1);
+		const auto srgb2  = linear_to_srgb(lin);
+		EXPECT_NEAR(srgb1, srgb2, AbsError);
+	}
+}
+
+TEST(rgb, srgb8_to_linear_lut)
+{
+	for (auto c = 0; c <= Rgb8Max; ++c) {
+		const auto expected = srgb_to_linear(c * (1.0f / Rgb8Max));
+		EXPECT_NEAR(srgb8_to_linear_lut(c), expected, AbsError);
+	}
+}
+
+TEST(rgb, linear_to_srgb8_lut)
+{
+	// This is good enough accuracy with a (16 * 1024) element LUT
+	constexpr auto NumIter = 500;
+	for (auto i = 0; i < NumIter; ++i) {
+		const float lin        = i * (1.0f / NumIter);
+		const uint8_t expected = round(linear_to_srgb(lin) * Rgb8Max);
+		EXPECT_EQ(linear_to_srgb8_lut(lin), expected);
+	}
+}
+
