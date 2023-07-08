d295f3638a4926e4dd862b0edc1dd48f3d1d1bc2
John Novak <john@johnnovak.net>
2023-07-09 08:38:49 +1000

    Use KB and MB instead of KiB and MiB
    KiB and MiB is an attempt at a new standard that never got adopted, and
    besides, everybody used KB and MB in the DOS era.

--- src/hardware/vga.cpp
+++ src/hardware/vga.cpp
@@ -130,7 +130,7 @@ void VGA_LogInitialization(const char *adapter_name,
 	const auto mem_in_kib = vga.vmemsize / 1024;
 	LOG_INFO("VIDEO: Initialized %s with %d-%s of %s supporting %d modes",
 	         adapter_name, mem_in_kib < 1024 ? mem_in_kib : mem_in_kib / 1024,
-	         mem_in_kib < 1024 ? "KiB" : "MiB", ram_type,
+	         mem_in_kib < 1024 ? "KB" : "MB", ram_type,
 	         check_cast<int16_t>(num_modes));
 }
 
