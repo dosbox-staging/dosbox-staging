7d5f399910ad55145a01b65680fd7964f9c041f0
John Novak <john@johnnovak.net>
2023-09-04 15:14:01 +1000

    Improve Rgb555 and remove redundant comments

--- include/rgb555.h
+++ include/rgb555.h
@@ -28,7 +28,6 @@
 
 class Rgb555 {
 public:
-	// Default constructor
 	constexpr Rgb555() = default;
 
 	constexpr Rgb555(const uint16_t _pixel)
@@ -36,26 +35,19 @@ public:
 		pixel = _pixel;
 	}
 
-	// Construct from separate RGB 8-bit values
 	constexpr Rgb555(const uint8_t r8, const uint8_t g8, const uint8_t b8)
 	{
-		FromRgb888(r8, g8, b8);
+		pixel = Rgb555::PixelFromRgb888(r8, g8, b8);
 	}
 
-	// Equality comparison
 	constexpr bool operator==(const Rgb555& rhs) const
 	{
 		return pixel == rhs.pixel;
 	}
 
-	// Update from separate RGB 8-bit values
-	constexpr void FromRgb888(const uint8_t r8, const uint8_t g8, const uint8_t b8)
+	constexpr bool operator!=(const Rgb555& that) const
 	{
-		const auto r5 = ((r8 >> 3) << r5_offset) & r5_mask;
-		const auto g5 = ((g8 >> 2) << g5_offset) & g5_mask;
-		const auto b5 = ((b8 >> 3) << b5_offset) & b5_mask;
-
-		pixel = static_cast<uint16_t>(r5 | g5 | b5);
+		return !operator==(that);
 	}
 
 	Rgb888 ToRgb888() const
@@ -66,7 +58,6 @@ public:
 		return Rgb888(r8, g8, b8);
 	}
 
-	// Write to separate RGB 8-bit values
 	void ToRgb888(uint8_t& r8, uint8_t& g8, uint8_t& b8) const
 	{
 		r8 = Red5To8(pixel);
@@ -74,21 +65,26 @@ public:
 		b8 = Blue5To8(pixel);
 	}
 
-	// Scoped conversion helper: RGB Red 5-bit to 8-bit
+	// Scoped conversion helpers
+	static constexpr Rgb555 FromRgb888(const Rgb888 rgb888)
+	{
+		return Rgb555(Rgb555::PixelFromRgb888(rgb888.red,
+		                                      rgb888.green,
+		                                      rgb888.blue));
+	}
+
 	static uint8_t Red5To8(const uint16_t val)
 	{
 		const auto red5 = (val & r5_mask) >> r5_offset;
 		return rgb5_to_8_lut(static_cast<uint8_t>(red5));
 	}
 
-	// Scoped conversion helper: RGB Green 5-bit to 8-bit
 	static uint8_t Green5To8(const uint16_t val)
 	{
 		const auto green5 = (val & g5_mask) >> g5_offset;
 		return rgb5_to_8_lut(static_cast<uint8_t>(green5));
 	}
 
-	// Scoped conversion helper: RGB Blue 5-bit to 8-bit
 	static uint8_t Blue5To8(const uint16_t val)
 	{
 		const auto blue5 = (val & b5_mask) >> b5_offset;
@@ -108,6 +104,16 @@ private:
 	static constexpr uint8_t r5_offset = 10;
 	static constexpr uint8_t g5_offset = 5;
 	static constexpr uint8_t b5_offset = 0;
+
+	static constexpr uint16_t PixelFromRgb888(const uint8_t r8,
+	                                          const uint8_t g8, const uint8_t b8)
+	{
+		const auto r5 = ((r8 >> 3) << r5_offset) & r5_mask;
+		const auto g5 = ((g8 >> 2) << g5_offset) & g5_mask;
+		const auto b5 = ((b8 >> 3) << b5_offset) & b5_mask;
+
+		return static_cast<uint16_t>(r5 | g5 | b5);
+	}
 };
 
 #endif
