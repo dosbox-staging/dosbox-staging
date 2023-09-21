d89cd8d2b7993912f57f6b86322f1785535f0d68
FeralChild64 <FeralChild64@users.noreply.github.com>
2023-09-21 22:07:42 +0200

    Simplify translation workflow, remove obsolete files

---
+++ contrib/resources/translations/normalize.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+
+REM SPDX-License-Identifier: GPL-2.0-or-later
+REM
+REM Copyright (C) 2023-2023  The DOSBox Staging Team
+
+SET translation_dir=%~dp0
+PUSHD "%translation_dir%"
+
+IF NOT EXIST tools\uconv.exe GOTO readme
+IF NOT EXIST tools\dos2unix.exe GOTO readme
+
+ECHO In directory %translation_dir%:
+
+FOR %%f IN (*.lng) DO (
+	CALL :nconv_t "%%f"
+)
+
+PAUSE
+EXIT /B %ERRORLEVEL%
+
+REM Normalization - for UTF-8 format locale
+
+:nconv_t
+ECHO Normalizing file %~1
+tools\uconv -f UTF-8 -t UTF-8 -x nfc "%~1" -o "~work.tmp"
+tools\dos2unix "~work.tmp" > NUL
+MOVE /Y "~work.tmp" "%~1" > NUL
+EXIT /B 0
+
+:readme
+TYPE tools\readme.txt
+ECHO.
+PAUSE
+EXIT /B
---
+++ contrib/resources/translations/normalize.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022-2023  The DOSBox Staging Team
+# Copyright (C) 2020-2021  Patryk Obara <patryk.obara@gmail.com>
+
+
+# Normalization - for UTF-8 format locale
+#
+nconv_t () {
+	echo "Normalizing file $1"
+	temp_file="$(mktemp)"
+	uconv -f UTF-8 -t UTF-8 -x '::nfc;' "$1" -o "$temp_file"
+	mv "$temp_file" "$1"
+}
+
+translation_dir=$(dirname "$0")
+pushd "$translation_dir" > /dev/null || exit
+
+echo "In directory $translation_dir:"
+
+file_list="*.lng"
+for f in $file_list
+do
+	nconv_t "$f"
+done
+
+popd > /dev/null || exit
