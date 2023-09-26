1127d1e582e3461ab848f6c08eb7d90accea1bf8
kcgen <kcgen@users.noreply.github.com>
2023-09-25 23:19:06 -0700

    Format voodoo pixel conversion macros

--- src/hardware/voodoo.cpp
+++ src/hardware/voodoo.cpp
@@ -333,55 +333,57 @@ static constexpr uint8_t dither_matrix_2x2[16] =
  *
  *************************************/
 
-#define EXTRACT_565_TO_888(val, a, b, c)					\
-	(a) = (((val) >> 8) & 0xf8) | (((val) >> 13) & 0x07);	\
-	(b) = (((val) >> 3) & 0xfc) | (((val) >> 9) & 0x03);	\
-	(c) = (((val) << 3) & 0xf8) | (((val) >> 2) & 0x07);	\
+#define EXTRACT_565_TO_888(val, a, b, c) \
+	(a) = (((val) >> 8) & 0xf8) | (((val) >> 13) & 0x07); \
+	(b) = (((val) >> 3) & 0xfc) | (((val) >> 9) & 0x03); \
+	(c) = (((val) << 3) & 0xf8) | (((val) >> 2) & 0x07);
 
-#define EXTRACT_x555_TO_888(val, a, b, c)					\
-	(a) = (((val) >> 7) & 0xf8) | (((val) >> 12) & 0x07);	\
-	(b) = (((val) >> 2) & 0xf8) | (((val) >> 7) & 0x07);	\
-	(c) = (((val) << 3) & 0xf8) | (((val) >> 2) & 0x07);	\
+#define EXTRACT_x555_TO_888(val, a, b, c) \
+	(a) = (((val) >> 7) & 0xf8) | (((val) >> 12) & 0x07); \
+	(b) = (((val) >> 2) & 0xf8) | (((val) >> 7) & 0x07); \
+	(c) = (((val) << 3) & 0xf8) | (((val) >> 2) & 0x07);
 
-#define EXTRACT_555x_TO_888(val, a, b, c)					\
-	(a) = (((val) >> 8) & 0xf8) | (((val) >> 13) & 0x07);	\
-	(b) = (((val) >> 3) & 0xf8) | (((val) >> 8) & 0x07);	\
-	(c) = (((val) << 2) & 0xf8) | (((val) >> 3) & 0x07);	\
+#define EXTRACT_555x_TO_888(val, a, b, c) \
+	(a) = (((val) >> 8) & 0xf8) | (((val) >> 13) & 0x07); \
+	(b) = (((val) >> 3) & 0xf8) | (((val) >> 8) & 0x07); \
+	(c) = (((val) << 2) & 0xf8) | (((val) >> 3) & 0x07);
 
-#define EXTRACT_1555_TO_8888(val, a, b, c, d)				\
-	(a) = ((int16_t)(val) >> 15) & 0xff;						\
-	EXTRACT_x555_TO_888(val, b, c, d)						\
+#define EXTRACT_1555_TO_8888(val, a, b, c, d) \
+	(a) = ((int16_t)(val) >> 15) & 0xff; \
+	EXTRACT_x555_TO_888(val, b, c, d)
 
-#define EXTRACT_5551_TO_8888(val, a, b, c, d)				\
-	EXTRACT_555x_TO_888(val, a, b, c)						\
-	(d) = ((val) & 0x0001) ? 0xff : 0x00;					\
+#define EXTRACT_5551_TO_8888(val, a, b, c, d) \
+	EXTRACT_555x_TO_888(val, a, b, c)(d) = ((val) & 0x0001) ? 0xff : 0x00;
 
-#define EXTRACT_x888_TO_888(val, a, b, c)					\
-	(a) = ((val) >> 16) & 0xff;								\
-	(b) = ((val) >> 8) & 0xff;								\
-	(c) = ((val) >> 0) & 0xff;								\
+#define EXTRACT_x888_TO_888(val, a, b, c) \
+	(a) = ((val) >> 16) & 0xff; \
+	(b) = ((val) >> 8) & 0xff; \
+	(c) = ((val) >> 0) & 0xff;
 
-#define EXTRACT_888x_TO_888(val, a, b, c)					\
-	(a) = ((val) >> 24) & 0xff;								\
-	(b) = ((val) >> 16) & 0xff;								\
-	(c) = ((val) >> 8) & 0xff;								\
+#define EXTRACT_888x_TO_888(val, a, b, c) \
+	(a) = ((val) >> 24) & 0xff; \
+	(b) = ((val) >> 16) & 0xff; \
+	(c) = ((val) >> 8) & 0xff;
 
-#define EXTRACT_8888_TO_8888(val, a, b, c, d)				\
-	(a) = ((val) >> 24) & 0xff;								\
-	(b) = ((val) >> 16) & 0xff;								\
-	(c) = ((val) >> 8) & 0xff;								\
-	(d) = ((val) >> 0) & 0xff;								\
+#define EXTRACT_8888_TO_8888(val, a, b, c, d) \
+	(a) = ((val) >> 24) & 0xff; \
+	(b) = ((val) >> 16) & 0xff; \
+	(c) = ((val) >> 8) & 0xff; \
+	(d) = ((val) >> 0) & 0xff;
 
-#define EXTRACT_4444_TO_8888(val, a, b, c, d)				\
-	(a) = (((val) >> 8) & 0xf0) | (((val) >> 12) & 0x0f);	\
-	(b) = (((val) >> 4) & 0xf0) | (((val) >> 8) & 0x0f);	\
-	(c) = (((val) >> 0) & 0xf0) | (((val) >> 4) & 0x0f);	\
-	(d) = (((val) << 4) & 0xf0) | (((val) >> 0) & 0x0f);	\
+#define EXTRACT_4444_TO_8888(val, a, b, c, d) \
+	(a) = (((val) >> 8) & 0xf0) | (((val) >> 12) & 0x0f); \
+	(b) = (((val) >> 4) & 0xf0) | (((val) >> 8) & 0x0f); \
+	(c) = (((val) >> 0) & 0xf0) | (((val) >> 4) & 0x0f); \
+	(d) = (((val) << 4) & 0xf0) | (((val) >> 0) & 0x0f);
 
-#define EXTRACT_332_TO_888(val, a, b, c)					\
-	(a) = (((val) >> 0) & 0xe0) | (((val) >> 3) & 0x1c) | (((val) >> 6) & 0x03); \
-	(b) = (((val) << 3) & 0xe0) | (((val) >> 0) & 0x1c) | (((val) >> 3) & 0x03); \
-	(c) = (((val) << 6) & 0xc0) | (((val) << 4) & 0x30) | (((val) << 2) & 0xc0) | (((val) << 0) & 0x03); \
+#define EXTRACT_332_TO_888(val, a, b, c) \
+	(a) = (((val) >> 0) & 0xe0) | (((val) >> 3) & 0x1c) | \
+	      (((val) >> 6) & 0x03); \
+	(b) = (((val) << 3) & 0xe0) | (((val) >> 0) & 0x1c) | \
+	      (((val) >> 3) & 0x03); \
+	(c) = (((val) << 6) & 0xc0) | (((val) << 4) & 0x30) | \
+	      (((val) << 2) & 0xc0) | (((val) << 0) & 0x03);
 
 /*************************************
  *
