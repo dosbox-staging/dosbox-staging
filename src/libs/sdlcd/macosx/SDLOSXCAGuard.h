7e184268626e87fbd7c302b86495f55e89f61c8d
C.W. Betts <computers57@hotmail.com>
2023-10-09 21:22:43 -0600

    Convert SDLOSXCAGuard.? to UTF-8.

--- src/libs/sdlcd/macosx/SDLOSXCAGuard.h
+++ src/libs/sdlcd/macosx/SDLOSXCAGuard.h
@@ -25,7 +25,7 @@
     Note: This file hasn't been modified so technically we have to keep the disclaimer :-(
 
 
-    Copyright:  © Copyright 2002 Apple Computer, Inc. All rights reserved.
+    Copyright:  Â© Copyright 2002 Apple Computer, Inc. All rights reserved.
 
     Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
             ("Apple") in consideration of your agreement to the following terms, and your
@@ -34,7 +34,7 @@
             please do not use, install, modify or redistribute this Apple software.
 
             In consideration of your agreement to abide by the following terms, and subject
-            to these terms, Apple grants you a personal, non-exclusive license, under AppleÕs
+            to these terms, Apple grants you a personal, non-exclusive license, under Appleâ€™s
             copyrights in this original Apple software (the "Apple Software"), to use,
             reproduce, modify and redistribute the Apple Software, with or without
             modifications, in source and/or binary forms; provided that if you redistribute
