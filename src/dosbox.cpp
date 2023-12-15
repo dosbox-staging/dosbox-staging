5226ffb172665f93b946287ea34f8dbbef426176
John Novak <john@johnnovak.net>
2023-12-15 12:47:07 +1000

    Remove trailing whitespaces from config descriptions

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -571,7 +571,7 @@ void DOSBOX_Init()
 	        "               (default).\n"
 	        "  halfline:    Supports the low-resolution halfline VESA 2.0 mode used by\n"
 	        "               Extreme Assault. Use only if needed, as it's not S3 compatible.\n"
-	        "  all:         All modes for a given video memory size, however some games \n"
+	        "  all:         All modes for a given video memory size, however some games\n"
 	        "               may not use them properly (flickering) or may need\n"
 	        "               more system memory to use them.");
 
