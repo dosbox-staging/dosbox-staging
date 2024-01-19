eceeae95b73a0e382f728fb9465803be1c15e435
Kirk Klobe <kklobe@gmail.com>
2024-01-18 20:00:03 -0600

    Fix linux compile error

--- src/hardware/vga_other.cpp
+++ src/hardware/vga_other.cpp
@@ -487,7 +487,7 @@ static void update_cga16_color_pcjr()
 
 			auto to_linear_rgb = [=](const float v) -> uint8_t {
 				// Only operate on reasonably positive v-values
-				if (!isnormal(v) || v <= 0.0f)
+				if (!std::isnormal(v) || v <= 0.0f)
 					return 0;
 				// switch to linear RGB space and scale to the 8-bit range
 				constexpr int max_8bit = UINT8_MAX;
