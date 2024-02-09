dosbox-staging/dosbox-staging

commit 512efbd9aebc49b280e9903dc9d51b8f781f7e10

Fix invalid iterator crash in debug build
The CurMode iterator was being initalized in a global.
This leads to undefined behavior as to when the constructor gets run.
On Windows and Linux, this leads to a crash in VGA init code.

src/ints/int10.cpp
@@ -732,6 +732,7 @@ static void SetupTandyBios(void) {
 }
 
 void INT10_Init(Section* /*sec*/) {
+	CurMode = std::prev(ModeList_VGA.end());
 	INT10_SetupPalette();
 	INT10_InitVGA();
 	if (IS_TANDY_ARCH) SetupTandyBios();

src/ints/int10_modes.cpp
@@ -587,7 +587,7 @@ static void init_all_palettes(const cga_colors_t& cga_colors)
 	// clang-format on
 }
 
-video_mode_block_iterator_t CurMode = std::prev(ModeList_VGA.end());
+video_mode_block_iterator_t CurMode = {};
 
 static void log_invalid_video_mode_error(const uint16_t mode) {
 	LOG_ERR("INT10H: Trying to set invalid video mode: %02Xh", mode);
@@ -2322,4 +2322,3 @@ void INT10_SetupPalette()
 	auto cga_colors = configure_cga_colors();
 	init_all_palettes(cga_colors);
 }
-

commit 94490ea6b0e441efed6520125f9b99105a3c38bf

Set initial monochrome palette in INT10_Init()
This fixes the crash on startup with `machine = cga_mono` introduced by
512efbd9 (Fix invalid iterator crash in debug build).

src/gui/render.cpp
@@ -1375,11 +1375,6 @@ void RENDER_Init(Section* sec)
 		set_default_viewport_setting();
 	}
 
-	// Set monochrome palette
-	const auto mono_palette = to_monochrome_palette_enum(
-	        section->Get_string("monochrome_palette").c_str());
-	VGA_SetMonochromePalette(mono_palette);
-
 	// Only use the default 1x rendering scaler
 	render.scale.size = 1;
 

src/ints/int10.cpp
@@ -17,14 +17,14 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-
 #include "dosbox.h"
-#include "mem.h"
-#include "callback.h"
-#include "regs.h"
-#include "inout.h"
+
 #include "int10.h"
+#include "callback.h"
+#include "inout.h"
+#include "mem.h"
 #include "mouse.h"
+#include "regs.h"
 #include "setup.h"
 
 Int10Data int10;
@@ -731,6 +731,9 @@ static void SetupTandyBios(void) {
 	}
 }
 
+// forward declaration
+MonochromePalette RENDER_GetMonochromePalette();
+
 void INT10_Init(Section*)
 {
 	CurMode = std::prev(ModeList_VGA.end());
@@ -747,8 +750,11 @@ void INT10_Init(Section*)
 	CALLBACK_Setup(call_10, &INT10_Handler, CB_IRET, "Int 10 video");
 	RealSetVec(0x10, CALLBACK_RealPointer(call_10));
 
-	// Init the 0x40 segment and init the data structures in the video ROM area
+	// Init the 0x40 segment and the data structures in the video ROM area
 	INT10_SetupRomMemory();
 	INT10_Seg40Init();
 	INT10_SetVideoMode(0x3);
+
+	// Init monochrome palette from the config for Hercules and CGA mono
+	VGA_SetMonochromePalette(RENDER_GetMonochromePalette());
 }