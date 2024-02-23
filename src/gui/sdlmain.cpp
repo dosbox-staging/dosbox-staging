dosbox-staging/dosbox-staging

commit a07c416f134d729002bc2b699005c5b1dc2ac606

Raise window to foreground after creation

src/gui/sdlmain.cpp
@@ -1439,6 +1439,8 @@ finish:
 	if (sdl.draw.callback)
 		sdl.draw.callback(GFX_CallBackRedraw);
 
+	SDL_RaiseWindow(sdl.window);
+
 	// Ensure the time to change window modes isn't counted against
 	// our paced timing. This is a rare event that depends on host
 	// latency (and not the rendering pipeline).

commit ed2014b4aa2bfafeabdcd9d4a8ee233b0db2a828

Only raise window when it is newly created
Regression from commit a07c416

src/gui/sdlmain.cpp
@@ -1394,6 +1394,8 @@ static SDL_Window* SetWindowMode(const RenderingBackend rendering_backend,
 
 		GFX_RefreshTitle();
 
+		SDL_RaiseWindow(sdl.window);
+
 		if (!fullscreen) {
 			goto finish;
 		}
@@ -1437,8 +1439,6 @@ finish:
 	// Force redraw after changing the window
 	if (sdl.draw.callback)
 		sdl.draw.callback(GFX_CallBackRedraw);
-
-	SDL_RaiseWindow(sdl.window);
 
 	// Ensure the time to change window modes isn't counted against
 	// our paced timing. This is a rare event that depends on host