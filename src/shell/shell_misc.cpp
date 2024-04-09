eba04d470b1d15ee6a452827556e25566dd19313
MeAreJeenius <61266239+MeAreJeenius@users.noreply.github.com>
2024-04-08 19:14:46 -0600

    Inherit batch file echo state from parent

--- src/shell/shell_misc.cpp
+++ src/shell/shell_misc.cpp
@@ -467,18 +467,20 @@ bool DOS_Shell::ExecuteProgram(std::string_view name, std::string_view args)
 	auto extension = fullname.substr(fullname.size() - extension_size);
 
 	if (iequals(extension, ".BAT")) {
+		const auto current_echo = batchfiles.empty()
+		                             ? echo
+		                             : batchfiles.top().Echo();
 		if (!batchfiles.empty() && !call) {
 			batchfiles.pop();
 		}
 
-		auto reader = FileReader::GetFileReader(fullname);
-		if (reader) {
+		if (auto reader = FileReader::GetFileReader(fullname)) {
 			batchfiles.emplace(*psp,
 			                   std::make_unique<FileReader>(
 			                           std::move(*reader)),
 			                   name,
 			                   args,
-			                   echo);
+			                   current_echo);
 		} else {
 			WriteOut("Could not open %s", fullname.c_str());
 		}
