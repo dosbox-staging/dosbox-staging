5c0c25820c1f5b5c3647f3e625fea797163a1e56
John Novak <john@johnnovak.net>
2024-04-19 19:03:43 +1000

    Move console output functions into a new console module

---
+++ include/console.h
@@ -0,0 +1,46 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2025-2025  The DOSBox Staging Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DOSBOX_CONSOLE_H
+#define DOSBOX_CONSOLE_H
+
+#include <string>
+
+#include "string_utils.h"
+
+void CONSOLE_RawWrite(const std::string_view output);
+
+template <typename... Args>
+void CONSOLE_Write(const char* format, const Args&... args)
+{
+	const auto str = format_str(format, args...);
+	CONSOLE_RawWrite(str);
+}
+
+template <typename... Args>
+void CONSOLE_Write(const std::string& format, const Args&... args)
+{
+	CONSOLE_Write(format.c_str(), args...);
+}
+
+void CONSOLE_ResetLastWrittenChar(char c);
+void CONSOLE_InjectMissingNewline();
+
+#endif // DOSBOX_CONSOLE_H
