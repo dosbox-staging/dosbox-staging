70b342a2f57ae09fbf0c54560683f2e84a8117e2
John Novak <john@johnnovak.net>
2024-04-20 09:55:32 +1000

    Normalise PROGRAM_CONFIG_VALUE_ERROR message

--- src/misc/programs.cpp
+++ src/misc/programs.cpp
@@ -893,8 +893,8 @@ void CONFIG::Run(void)
 				if (!change_success) {
 					trim(value);
 					WriteOut(MSG_Get("PROGRAM_CONFIG_VALUE_ERROR"),
-					         value.c_str(),
-					         pvars[1].c_str());
+					         pvars[1].c_str(),
+					         value.c_str());
 				}
 
 				tsec->ExecuteInit(false);
@@ -1068,7 +1068,8 @@ void PROGRAMS_Init(Section* sec)
 	MSG_Add("PROGRAM_CONFIG_SECTION_ERROR", "Section [%s] doesn't exist.\n");
 
 	MSG_Add("PROGRAM_CONFIG_VALUE_ERROR",
-	        "'%s' is not a valid value for setting '%s'.\n");
+	        "Invalid [color=light-green]'%s'[reset] setting: [color=white]'%s'[reset], "
+	        "using the existing value\n");
 
 	MSG_Add("PROGRAM_CONFIG_GET_SYNTAX",
 	        "Usage: [color=light-green]config[reset] -get "
