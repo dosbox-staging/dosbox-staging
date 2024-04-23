d62ed0bf2410f2ecfaf907f11be9eec536312fac
John Novak <john@johnnovak.net>
2024-04-23 13:42:25 +1000

    Disable sign conversion warnings in the `CHECK_NARROWING()` macro

--- include/checks.h
+++ include/checks.h
@@ -1,7 +1,7 @@
 /*
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *
- *  Copyright (C) 2022-2022  The DOSBox Staging Team
+ *  Copyright (C) 2022-2024  The DOSBox Staging Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -57,7 +57,6 @@
 
 #ifdef __GNUC__
 #	define CHECK_NARROWING() \
-		_Pragma("GCC diagnostic warning \"-Wsign-conversion\"") \
 		_Pragma("GCC diagnostic warning \"-Wconversion\"") \
 		_Pragma("GCC diagnostic warning \"-Wnarrowing\"") \
 		END_MACRO
