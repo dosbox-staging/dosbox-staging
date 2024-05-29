ffe13c6182d011b1c92d147d8bc094efd34a469b
Daniel Bomar <dbdaniel42@gmail.com>
2024-05-28 22:03:04 -0500

    Report that the read-only attribute is set on all files on a read-only drive

--- src/dos/dos_files.cpp
+++ src/dos/dos_files.cpp
@@ -970,6 +970,9 @@ bool DOS_GetFileAttr(const char* const name, FatAttributeFlags* attr)
 	}
 
 	if (Drives.at(drive)->GetFileAttr(fullname, attr)) {
+		if (Drives.at(drive)->IsReadOnly()) {
+			attr->read_only = true;
+		}
 		return true;
 	} else {
 		*attr = 0;
