7519d86f0141d31a7bef79882e40f61e46745322
John Novak <john@johnnovak.net>
2024-06-15 08:40:32 +1000

    Use notifications API for generic config errors and warnings

--- src/misc/setup.cpp
+++ src/misc/setup.cpp
@@ -33,6 +33,7 @@
 #include "control.h"
 #include "cross.h"
 #include "fs_utils.h"
+#include "notifications.h"
 #include "string_utils.h"
 #include "support.h"
 #include "version.h"
@@ -232,10 +233,14 @@ bool Property::IsValidValue(const Value& in)
 		}
 	}
 
-	LOG_WARNING("CONFIG: Invalid '%s' setting: '%s', using '%s'",
-	            propname.c_str(),
-	            in.ToString().c_str(),
-	            default_value.ToString().c_str());
+	// TODO duplicate?
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "CONFIG",
+	                      "Invalid [color=light-green]'%s'[reset] setting: "
+	                      "[color=white]'%s'[reset], using [color=white]'%s'[reset]",
+	                      propname.c_str(),
+	                      in.ToString().c_str(),
+	                      default_value.ToString().c_str());
 
 	return false;
 }
@@ -244,12 +249,16 @@ bool Property::IsValueDeprecated(const Value& val) const
 {
 	const auto is_deprecated = contains(deprecated_and_alternate_values, val);
 	if (is_deprecated) {
-		LOG_WARNING("CONFIG: Setting '%s = %s' is deprecated, "
-		            "falling back to the alternate: '%s = %s'",
-		            propname.c_str(),
-		            val.ToString().c_str(),
-		            propname.c_str(),
-		            GetAlternateForDeprecatedValue(val).ToString().c_str());
+		// TODO duplicate?
+		NOTIFY_DisplayWarning(
+		        Notification::Source::Console,
+		        "CONFIG",
+		        "Setting [color=light-green]'%s = %s'[reset] is deprecated, "
+		        "falling back to the alternate: [color=light-green]'%s = %s'[reset]",
+		        propname.c_str(),
+		        val.ToString().c_str(),
+		        propname.c_str(),
+		        GetAlternateForDeprecatedValue(val).ToString().c_str());
 	}
 	return is_deprecated;
 }
@@ -337,7 +346,13 @@ std::string Property::GetHelp() const
 		}
 	}
 	if (result.empty()) {
-		LOG_WARNING("CONFIG: No help available for '%s'.", propname.c_str());
+		// TODO duplicate?
+		NOTIFY_DisplayWarning(Notification::Source::Console,
+		                      "CONFIG",
+		                      "No help is available for "
+		                      "[color=light-green]'%s'[reset].",
+		                      propname.c_str());
+
 		return "No help available for '" + propname + "'\n";
 	}
 	return result;
@@ -379,7 +394,12 @@ std::string Property::GetHelpForHost() const
 		}
 	}
 	if (result.empty()) {
-		LOG_WARNING("CONFIG: No help available for '%s'.", propname.c_str());
+		// TODO duplicate?
+		NOTIFY_DisplayWarning(Notification::Source::Console,
+		                      "CONFIG",
+		                      "No help is available for "
+		                      "[color=light-green]'%s'[reset].",
+		                      propname.c_str());
 	}
 	return result;
 }
@@ -423,13 +443,18 @@ bool Prop_int::ValidateValue(const Value& in)
 		va = mi;
 	}
 
-	LOG_WARNING("CONFIG: Invalid '%s' setting: '%s'. "
-	            "Value outside of the valid range %s-%s, using '%d'",
-	            propname.c_str(),
-	            in.ToString().c_str(),
-	            min_value.ToString().c_str(),
-	            max_value.ToString().c_str(),
-	            va);
+		// TODO duplicate?
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "CONFIG",
+	                      "Invalid [color=light-green]'%s'[reset] setting: "
+	                      "[color=white]'%s'[reset]. "
+	                      "Value is outside of the valid range of %s-%s, "
+	                      "using [color=white]'%d'[reset]",
+	                      propname.c_str(),
+	                      in.ToString().c_str(),
+	                      min_value.ToString().c_str(),
+	                      max_value.ToString().c_str(),
+	                      va);
 
 	value = va;
 	return true;
@@ -453,14 +478,18 @@ bool Prop_int::IsValidValue(const Value& in)
 		return true;
 	}
 
-	LOG_WARNING(
-	        "CONFIG: Invalid '%s' setting: '%s'. "
-	        "Value outside of the valid range %s-%s, using '%s'",
-	        propname.c_str(),
-	        in.ToString().c_str(),
-	        min_value.ToString().c_str(),
-	        max_value.ToString().c_str(),
-	        default_value.ToString().c_str());
+	// TODO duplicate?
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "CONFIG",
+	                      "Invalid [color=light-green]'%s'[reset] setting: "
+						  "[color=white]'%s'[reset]. "
+	                      "Value is outside of the valid range of %s-%s, "
+						  "using [color=white]'%s'[reset]",
+	                      propname.c_str(),
+	                      in.ToString().c_str(),
+	                      min_value.ToString().c_str(),
+	                      max_value.ToString().c_str(),
+	                      default_value.ToString().c_str());
 
 	return false;
 }
@@ -526,10 +555,15 @@ bool Prop_string::IsValidValue(const Value& in)
 		}
 	}
 
-	LOG_WARNING("CONFIG: Invalid '%s' setting: '%s', using '%s'",
-	            propname.c_str(),
-	            in.ToString().c_str(),
-	            default_value.ToString().c_str());
+	// TODO duplicate?
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "CONFIG",
+	                      "Invalid [color=light-green]'%s'[reset] setting: "
+	                      "[color=white]'%s'[reset], "
+	                      "using [color=white]'%s'[reset]",
+	                      propname.c_str(),
+	                      in.ToString().c_str(),
+	                      default_value.ToString().c_str());
 
 	return false;
 }
@@ -568,10 +602,12 @@ bool Prop_bool::SetValue(const std::string& input)
 	if (!is_valid) {
 		SetValue(default_value.ToString());
 
-		LOG_WARNING("CONFIG: Invalid '%s' setting: '%s', using '%s'",
-		            propname.c_str(),
-		            input.c_str(),
-		            default_value.ToString().c_str());
+		// TODO duplicate?
+		NOTIFY_DisplayWarning(Notification::Source::Console, "CONFIG",
+		                      "Invalid '%s' setting: '%s', using '%s'",
+		                      propname.c_str(),
+		                      input.c_str(),
+		                      default_value.ToString().c_str());
 	}
 	return is_valid;
 }
@@ -1047,9 +1083,17 @@ bool Section_prop::HandleInputline(const std::string& line)
 		}
 
 		if (p->IsDeprecated()) {
-			LOG_WARNING("CONFIG: Deprecated option '%s'\n\n%s\n",
-			            name.c_str(),
-			            p->GetHelpForHost().c_str());
+			// TODO is this message correct? setting instead of option?
+			NOTIFY_DisplayWarning(Notification::Source::Console,
+			                      "CONFIG",
+			                      "Deprecated option "
+								  "[color=light-green]'%s'[color=white]",
+			                      name.c_str());
+
+			NOTIFY_DisplayWarning(Notification::Source::Console,
+			                      "CONFIG",
+			                      "%s",
+			                      p->GetHelp().c_str());
 
 			if (!p->IsDeprecatedButAllowed()) {
 				return false;
@@ -1059,7 +1103,11 @@ bool Section_prop::HandleInputline(const std::string& line)
 		return p->SetValue(val);
 	}
 
-	LOG_WARNING("CONFIG: Invalid option '%s'", name.c_str());
+	// TODO is this message correct? setting instead of option?
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "CONFIG",
+	                      "Invalid option [color=light-green]'%s'[reset]",
+	                      name.c_str());
 	return false;
 }
 
@@ -1751,8 +1799,12 @@ Verbosity Config::GetStartupVerbosity() const
 		             : Verbosity::High;
 	}
 
-	LOG_WARNING("SETUP: Invalid 'startup_verbosity' setting: '%s', using 'high'",
-	            user_choice.c_str());
+	NOTIFY_DisplayWarning(Notification::Source::Console,
+	                      "SETUP",
+	                      "Invalid [color=light-green]'startup_verbosity'[reset] setting: "
+	                      "[color=white]'%s'[reset], using [color=white]'high'[reset]",
+	                      user_choice.c_str());
+
 	return Verbosity::High;
 }
 
