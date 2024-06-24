dosbox-staging/dosbox-staging

commit 72f0f7ca388493e4942d0cea04a7f379631a1dcb

Get rid of fixed-point math in `MixerChannel:Mix`

src/hardware/mixer.cpp
@@ -1013,8 +1013,6 @@ void MixerChannel::SetSampleRate(const int new_sample_rate_hz)
 
 	sample_rate_hz = target_rate_hz;
 
-	freq_add = (sample_rate_hz << FreqShift) / mixer.sample_rate_hz;
-
 	envelope.Update(sample_rate_hz,
 	                peak_amplitude,
 	                EnvelopeMaxExpansionOverMs,
@@ -1052,17 +1050,18 @@ void MixerChannel::Mix(const int frames_requested)
 
 	frames_needed = frames_requested;
 
-	while (frames_needed > frames_done) {
-		auto frames_remaining = frames_needed - frames_done;
-		frames_remaining *= freq_add;
-		frames_remaining = (frames_remaining >> FreqShift) +
-		                   ((frames_remaining & FreqMask) != 0);
+	auto freq_add = static_cast<float>(sample_rate_hz) /
+	                static_cast<float>(mixer.sample_rate_hz);
 
-		// avoid underflow
+	while (frames_needed > frames_done) {
+		auto frames_remaining = iceil(
+		        static_cast<float>(frames_needed - frames_done) + freq_add);
+
+		// Avoid underflow
 		if (frames_remaining <= 0) {
 			break;
 		}
-		// avoid overflow
+		// Avoid overflow
 		frames_remaining = std::min(frames_remaining, MixerBufferByteSize);
 
 		handler(frames_remaining);

include/mixer.h
@@ -286,9 +286,6 @@ private:
 
 	std::set<ChannelFeature> features = {};
 
-	// This gets added the frequency counter each mixer step
-	int freq_add = 0u;
-
 	// When this flows over a new sample needs to be read from the device
 	int freq_counter = 0u;
 

commit 34e04cbe99112d0c52a070d62d85d10265d6cf91

Fix audio stretching regression of `MixerChannel::Mix`
Fixes regression introduced in 72f0f7ca388

This regression is hard to test. One reliable test case is the
"Conquests of the Longbow: The Legend of Robin Hood" demo. At the start
of the demo, there is a digitised speech sample that goes "Robin Hood,
your bow is needed!"

Without this fix, the "Robin Hood" part is almost completely cut off.

src/hardware/mixer.cpp
@@ -1048,12 +1048,13 @@ void MixerChannel::Mix(const int frames_requested)
 
 	frames_needed = frames_requested;
 
-	auto freq_add = static_cast<float>(sample_rate_hz) /
-	                static_cast<float>(mixer.sample_rate_hz);
+	auto stretch_factor = static_cast<float>(sample_rate_hz) /
+	                      static_cast<float>(mixer.sample_rate_hz);
 
 	while (frames_needed > frames_done) {
 		auto frames_remaining = iceil(
-		        static_cast<float>(frames_needed - frames_done) + freq_add);
+		        static_cast<float>(frames_needed - frames_done) *
+		        stretch_factor);
 
 		// Avoid underflow
 		if (frames_remaining <= 0) {