7c23c6cb6a30ef164d3f63f995192ee29f4f5a19
Kirk Klobe <kklobe@gmail.com>
2024-08-25 07:20:07 -0600

    Cleanup FPU code
    De-blah, de-Bitu, de-union, etc., intended to be no functional changes.

--- src/fpu/fpu_instructions.h
+++ src/fpu/fpu_instructions.h
@@ -117,7 +117,7 @@ static void FPU_FPOP(void)
 	// maybe set zero in it as well
 	TOP = ((TOP + 1) & 7);
 	//	LOG(LOG_FPU,LOG_ERROR)("popped from %d  %g off the
-	//stack",top,fpu.regs[top].d);
+	// stack",top,fpu.regs[top].d);
 	return;
 }
 
@@ -160,37 +160,35 @@ static double FROUND(double in)
 #define BIAS80 16383
 #define BIAS64 1023
 
-static Real64 FPU_FLD80(PhysPt addr)
-{
+static Real64 FPU_FLD80(PhysPt addr) {
 	struct {
 		int16_t begin = 0;
 		FPU_Reg eind  = {};
 	} test = {};
 
 	test.eind.ll = mem_readq(addr);
-	test.begin   = mem_readw(addr + 8);
-
-	int64_t exp64      = (((test.begin & 0x7fff) - BIAS80));
-	int64_t blah       = ((exp64 > 0) ? exp64 : -exp64) & 0x3ff;
-	int64_t exp64final = ((exp64 > 0) ? blah : -blah) + BIAS64;
+	test.begin = mem_readw(addr+8);
+   
+	int64_t exp64 = (((test.begin&0x7fff) - BIAS80));
+	int64_t blah = ((exp64 >0)?exp64:-exp64)&0x3ff;
+	int64_t exp64final = ((exp64 >0)?blah:-blah) +BIAS64;
 
 	int64_t mant64 = (test.eind.ll >> 11) & LONGTYPE(0xfffffffffffff);
-	int64_t sign   = (test.begin & 0x8000) ? 1 : 0;
+	int64_t sign = (test.begin&0x8000)?1:0;
 	FPU_Reg result;
-	result.ll = (sign << 63) | (exp64final << 52) | mant64;
+	result.ll = (sign <<63)|(exp64final << 52)| mant64;
 
 	if (test.eind.l.lower == 0 && test.eind.l.upper == INT32_MIN &&
 	    (test.begin & INT16_MAX) == INT16_MAX) {
-		// Detect INF and -INF (score 3.11 when drawing a slur.)
-		result.d = sign ? -HUGE_VAL : HUGE_VAL;
+		//Detect INF and -INF (score 3.11 when drawing a slur.)
+		result.d = sign?-HUGE_VAL:HUGE_VAL;
 	}
 	return result.d;
 
-	// mant64= test.mant80/2***64    * 2 **53
+	//mant64= test.mant80/2***64    * 2 **53 
 }
 
-static void FPU_ST80(PhysPt addr, Bitu reg)
-{
+static void FPU_ST80(PhysPt addr,Bitu reg) {
 	struct {
 		int16_t begin = 0;
 		FPU_Reg eind  = {};
@@ -199,34 +197,31 @@ static void FPU_ST80(PhysPt addr, Bitu reg)
 	int64_t sign80 = (fpu.regs[reg].ll & LONGTYPE(0x8000000000000000)) ? 1 : 0;
 	int64_t exp80       = fpu.regs[reg].ll & LONGTYPE(0x7ff0000000000000);
 	int64_t exp80final  = (exp80 >> 52);
-	int64_t mant80      = fpu.regs[reg].ll & LONGTYPE(0x000fffffffffffff);
+	int64_t mant80 = fpu.regs[reg].ll&LONGTYPE(0x000fffffffffffff);
 	int64_t mant80final = (mant80 << 11);
-	if (fpu.regs[reg].d != 0) { // Zero is a special case
+	if(fpu.regs[reg].d != 0){ //Zero is a special case
 		// Elvira wants the 8 and tcalc doesn't
 		mant80final |= LONGTYPE(0x8000000000000000);
-		// Ca-cyber doesn't like this when result is zero.
+		//Ca-cyber doesn't like this when result is zero.
 		exp80final += (BIAS80 - BIAS64);
 	}
-	test.begin = (static_cast<int16_t>(sign80) << 15) |
-	             static_cast<int16_t>(exp80final);
+	test.begin = (static_cast<int16_t>(sign80)<<15)| static_cast<int16_t>(exp80final);
 	test.eind.ll = mant80final;
 	mem_writeq(addr, test.eind.ll);
-	mem_writew(addr + 8, test.begin);
+	mem_writew(addr+8,test.begin);
 }
 
 static void FPU_FLD_F32(PhysPt addr, Bitu store_to)
 {
-	union {
-		float f;
-		uint32_t l;
-	} blah;
-	blah.l               = mem_readd(addr);
-	fpu.regs[store_to].d = static_cast<Real64>(blah.f);
+	const auto mem_val = mem_readd(addr);
+	float f;
+	memcpy(&f, &mem_val, sizeof(mem_val));
+	fpu.regs[store_to].d = static_cast<Real64>(f);
 }
 
 static void FPU_FLD_F64(PhysPt addr, Bitu store_to)
 {
-	fpu.regs[store_to].ll = mem_readq(addr);
+	fpu.regs[store_to].ll = static_cast<int64_t>(mem_readq(addr));
 }
 
 static void FPU_FLD_F80(PhysPt addr)
@@ -236,14 +231,14 @@ static void FPU_FLD_F80(PhysPt addr)
 
 static void FPU_FLD_I16(PhysPt addr, Bitu store_to)
 {
-	int16_t blah         = mem_readw(addr);
-	fpu.regs[store_to].d = static_cast<Real64>(blah);
+	const auto mem_val   = static_cast<int16_t>(mem_readw(addr));
+	fpu.regs[store_to].d = static_cast<Real64>(mem_val);
 }
 
 static void FPU_FLD_I32(PhysPt addr, Bitu store_to)
 {
-	int32_t blah         = mem_readd(addr);
-	fpu.regs[store_to].d = static_cast<Real64>(blah);
+	const auto mem_val   = static_cast<int32_t>(mem_readd(addr));
+	fpu.regs[store_to].d = static_cast<Real64>(mem_val);
 }
 
 static void FPU_FLD_I64(PhysPt addr, Bitu store_to)
@@ -254,7 +249,7 @@ static void FPU_FLD_I64(PhysPt addr, Bitu store_to)
 	constexpr int64_t MantissaMax = (1LL << 53);
 
 	FPU_Reg temp_reg;
-	temp_reg.ll = mem_readq(addr);
+	temp_reg.ll = static_cast<int64_t>(mem_readq(addr));
 
 	fpu.regs[store_to].d = static_cast<Real64>(temp_reg.ll);
 	// This is a fix for vertical bars that appear in some games that
@@ -274,9 +269,10 @@ static void FPU_FLD_I64(PhysPt addr, Bitu store_to)
 static void FPU_FBLD(PhysPt addr, Bitu store_to)
 {
 	uint64_t val  = 0;
-	Bitu in       = 0;
+	uint8_t in    = 0;
 	uint64_t base = 1;
-	for (Bitu i = 0; i < 9; i++) {
+
+	for (int i = 0; i < 9; i++) {
 		in = mem_readb(addr + i);
 		val += ((in & 0xf) * base); // in&0xf shouldn't be higher then 9
 		base *= 10;
@@ -286,12 +282,14 @@ static void FPU_FBLD(PhysPt addr, Bitu store_to)
 
 	// last number, only now convert to float in order to get
 	// the best signification
-	Real64 temp = static_cast<Real64>(val);
-	in          = mem_readb(addr + 9);
+	auto temp = static_cast<Real64>(val);
+	in        = mem_readb(addr + 9);
 	temp += ((in & 0xf) * base);
+
 	if (in & 0x80) {
-		temp *= -1.0;
+		temp = -temp;
 	}
+
 	fpu.regs[store_to].d = temp;
 }
 
@@ -314,18 +312,16 @@ static inline void FPU_FLD_I16_EA(PhysPt addr)
 
 static void FPU_FST_F32(PhysPt addr)
 {
-	union {
-		float f;
-		uint32_t l;
-	} blah;
 	// should depend on rounding method
-	blah.f = static_cast<float>(fpu.regs[TOP].d);
-	mem_writed(addr, blah.l);
+	const auto f = static_cast<float>(fpu.regs[TOP].d);
+	uint32_t l;
+	memcpy(&l, &f, sizeof(f));
+	mem_writed(addr, l);
 }
 
 static void FPU_FST_F64(PhysPt addr)
 {
-	mem_writeq(addr, fpu.regs[TOP].ll);
+	mem_writeq(addr, static_cast<uint64_t>(fpu.regs[TOP].ll));
 }
 
 static void FPU_FST_F80(PhysPt addr)
@@ -335,7 +331,7 @@ static void FPU_FST_F80(PhysPt addr)
 
 static void FPU_FST_I16(PhysPt addr)
 {
-	double val = FROUND(fpu.regs[TOP].d);
+	const auto val = FROUND(fpu.regs[TOP].d);
 	mem_writew(addr,
 	           (val < 32768.0 && val >= -32768.0) ? static_cast<int16_t>(val)
 	                                              : 0x8000);
@@ -343,7 +339,7 @@ static void FPU_FST_I16(PhysPt addr)
 
 static void FPU_FST_I32(PhysPt addr)
 {
-	double val = FROUND(fpu.regs[TOP].d);
+	const auto val = FROUND(fpu.regs[TOP].d);
 	mem_writed(addr,
 	           (val < 2147483648.0 && val >= -2147483648.0)
 	                   ? static_cast<int32_t>(val)
@@ -352,20 +348,20 @@ static void FPU_FST_I32(PhysPt addr)
 
 static void FPU_FST_I64(PhysPt addr)
 {
-	FPU_Reg temp_reg;
+	int64_t temp_reg;
 	// If the value was loaded with the FILD/FIST 64-bit memcpy trick,
 	// read the 64-bit integer value instead.
 	if (fpu.regs_memcpy[TOP].has_value()) {
-		temp_reg.ll = fpu.regs_memcpy[TOP].value();
+		temp_reg = fpu.regs_memcpy[TOP].value();
 	} else {
 		double val  = FROUND(fpu.regs[TOP].d);
-		temp_reg.ll = (val <= static_cast<double>(INT64_MAX) &&
-		               val >= static_cast<double>(INT64_MIN))
+		temp_reg    = (val <= static_cast<double>(INT64_MAX) &&
+                            val >= static_cast<double>(INT64_MIN))
 		                    ? static_cast<int64_t>(val)
 		                    : LONGTYPE(0x8000000000000000);
 	}
 
-	mem_writeq(addr, temp_reg.ll);
+	mem_writeq(addr, static_cast<uint64_t>(temp_reg));
 }
 
 static void FPU_FBST(PhysPt addr)
@@ -378,7 +374,7 @@ static void FPU_FBST(PhysPt addr)
 		mem_writeb(addr + 9, 0);
 	}
 
-	uint64_t rndint = static_cast<uint64_t>(FROUND(val.d));
+	auto rndint = static_cast<uint64_t>(FROUND(val.d));
 	// BCD (18 decimal digits) overflow? (0x0DE0B6B3A763FFFF max)
 	if (rndint > LONGTYPE(999999999999999999)) {
 		// write BCD integer indefinite value
@@ -412,7 +408,7 @@ static void FPU_FADD(Bitu op1, Bitu op2)
 static void FPU_FSIN(void)
 {
 	fpu.regs[TOP].d = std::sin(fpu.regs[TOP].d);
-	FPU_SET_C2(0);
+	FPU_SET_C2(false);
 	// flags and such :)
 	return;
 }
@@ -422,7 +418,7 @@ static void FPU_FSINCOS(void)
 	Real64 temp     = fpu.regs[TOP].d;
 	fpu.regs[TOP].d = std::sin(temp);
 	FPU_PUSH(std::cos(temp));
-	FPU_SET_C2(0);
+	FPU_SET_C2(false);
 	// flags and such :)
 	return;
 }
@@ -430,7 +426,7 @@ static void FPU_FSINCOS(void)
 static void FPU_FCOS(void)
 {
 	fpu.regs[TOP].d = std::cos(fpu.regs[TOP].d);
-	FPU_SET_C2(0);
+	FPU_SET_C2(false);
 	// flags and such :)
 	return;
 }
@@ -452,7 +448,7 @@ static void FPU_FPTAN(void)
 {
 	fpu.regs[TOP].d = std::tan(fpu.regs[TOP].d);
 	FPU_PUSH(1.0);
-	FPU_SET_C2(0);
+	FPU_SET_C2(false);
 	// flags and such :)
 	return;
 }
@@ -513,30 +509,28 @@ static void FPU_FST(Bitu st, Bitu other)
 
 static void FPU_FCOM(Bitu st, Bitu other)
 {
-	if (((fpu.tags[st] != TAG_Valid) && (fpu.tags[st] != TAG_Zero)) ||
-	    ((fpu.tags[other] != TAG_Valid) && (fpu.tags[other] != TAG_Zero))) {
-		FPU_SET_C3(1);
-		FPU_SET_C2(1);
-		FPU_SET_C0(1);
-		return;
+	const bool tags_are_invalid =
+	        ((fpu.tags[st] != TAG_Valid && fpu.tags[st] != TAG_Zero) ||
+	         (fpu.tags[other] != TAG_Valid && fpu.tags[other] != TAG_Zero));
+
+	if (tags_are_invalid) {
+		FPU_SET_C3(true);
+		FPU_SET_C2(true);
+		FPU_SET_C0(true);
+	} else if (fpu.regs[st].d == fpu.regs[other].d) {
+		FPU_SET_C3(true);
+		FPU_SET_C2(false);
+		FPU_SET_C0(false);
+	} else if (fpu.regs[st].d < fpu.regs[other].d) {
+		FPU_SET_C3(false);
+		FPU_SET_C2(false);
+		FPU_SET_C0(true);
+	} else {
+		// st > other
+		FPU_SET_C3(false);
+		FPU_SET_C2(false);
+		FPU_SET_C0(false);
 	}
-	if (fpu.regs[st].d == fpu.regs[other].d) {
-		FPU_SET_C3(1);
-		FPU_SET_C2(0);
-		FPU_SET_C0(0);
-		return;
-	}
-	if (fpu.regs[st].d < fpu.regs[other].d) {
-		FPU_SET_C3(0);
-		FPU_SET_C2(0);
-		FPU_SET_C0(1);
-		return;
-	}
-	// st > other
-	FPU_SET_C3(0);
-	FPU_SET_C2(0);
-	FPU_SET_C0(0);
-	return;
 }
 
 static void FPU_FUCOM(Bitu st, Bitu other)
@@ -557,41 +551,33 @@ static void FPU_FRNDINT(void)
 	fpu.regs[TOP].d = rounded;
 }
 
-static void FPU_FPREM(void)
-{
-	Real64 valtop    = fpu.regs[TOP].d;
-	Real64 valdiv    = fpu.regs[STV(1)].d;
-	int64_t ressaved = static_cast<int64_t>((valtop / valdiv));
-	// Some backups
-	//	Real64 res=valtop - ressaved*valdiv;
-	//      res= fmod(valtop,valdiv);
-	fpu.regs[TOP].d = valtop - ressaved * valdiv;
-	FPU_SET_C0(static_cast<Bitu>(ressaved & 4));
-	FPU_SET_C3(static_cast<Bitu>(ressaved & 2));
-	FPU_SET_C1(static_cast<Bitu>(ressaved & 1));
+static void FPU_FPREM(void){
+	Real64 valtop = fpu.regs[TOP].d;
+	Real64 valdiv = fpu.regs[STV(1)].d;
+	int64_t ressaved = static_cast<int64_t>( (valtop/valdiv) );
+// Some backups
+//	Real64 res=valtop - ressaved*valdiv; 
+//      res= fmod(valtop,valdiv);
+	fpu.regs[TOP].d = valtop - ressaved*valdiv;
+	FPU_SET_C0(static_cast<Bitu>(ressaved&4));
+	FPU_SET_C3(static_cast<Bitu>(ressaved&2));
+	FPU_SET_C1(static_cast<Bitu>(ressaved&1));
 	FPU_SET_C2(0);
 }
 
-static void FPU_FPREM1(void)
-{
+static void FPU_FPREM1(void){
 	Real64 valtop = fpu.regs[TOP].d;
 	Real64 valdiv = fpu.regs[STV(1)].d;
-	double quot   = valtop / valdiv;
-	double quotf  = floor(quot);
+	double quot = valtop/valdiv;
+	double quotf = floor(quot);
 	int64_t ressaved;
-	if (quot - quotf > 0.5) {
-		ressaved = static_cast<int64_t>(quotf + 1);
-	} else if (quot - quotf < 0.5) {
-		ressaved = static_cast<int64_t>(quotf);
-	} else {
-		ressaved = static_cast<int64_t>(
-		        (((static_cast<int64_t>(quotf)) & 1) != 0) ? (quotf + 1)
-		                                                   : (quotf));
-	}
-	fpu.regs[TOP].d = valtop - ressaved * valdiv;
-	FPU_SET_C0(static_cast<Bitu>(ressaved & 4));
-	FPU_SET_C3(static_cast<Bitu>(ressaved & 2));
-	FPU_SET_C1(static_cast<Bitu>(ressaved & 1));
+	if (quot-quotf>0.5) ressaved = static_cast<int64_t>(quotf+1);
+	else if (quot-quotf<0.5) ressaved = static_cast<int64_t>(quotf);
+	else ressaved = static_cast<int64_t>((((static_cast<int64_t>(quotf))&1)!=0)?(quotf+1):(quotf));
+	fpu.regs[TOP].d = valtop - ressaved*valdiv;
+	FPU_SET_C0(static_cast<Bitu>(ressaved&4));
+	FPU_SET_C3(static_cast<Bitu>(ressaved&2));
+	FPU_SET_C1(static_cast<Bitu>(ressaved&1));
 	FPU_SET_C2(0);
 }
 
@@ -599,25 +585,27 @@ static void FPU_FXAM(void)
 {
 	if (fpu.regs[TOP].ll & LONGTYPE(0x8000000000000000)) // sign
 	{
-		FPU_SET_C1(1);
+		FPU_SET_C1(true);
 	} else {
-		FPU_SET_C1(0);
+		FPU_SET_C1(false);
 	}
+
 	if (fpu.tags[TOP] == TAG_Empty) {
-		FPU_SET_C3(1);
-		FPU_SET_C2(0);
-		FPU_SET_C0(1);
+		FPU_SET_C3(true);
+		FPU_SET_C2(false);
+		FPU_SET_C0(true);
 		return;
 	}
+
 	if (fpu.regs[TOP].d == 0.0) // zero or normalized number.
 	{
-		FPU_SET_C3(1);
-		FPU_SET_C2(0);
-		FPU_SET_C0(0);
+		FPU_SET_C3(true);
+		FPU_SET_C2(false);
+		FPU_SET_C0(false);
 	} else {
-		FPU_SET_C3(0);
-		FPU_SET_C2(1);
-		FPU_SET_C0(0);
+		FPU_SET_C3(false);
+		FPU_SET_C2(true);
+		FPU_SET_C0(false);
 	}
 }
 
@@ -641,16 +629,16 @@ static void FPU_FYL2XP1(void)
 	return;
 }
 
-static void FPU_FSCALE(void)
-{
+static void FPU_FSCALE(void){
 	fpu.regs[TOP].d *= std::pow(2.0, std::trunc(fpu.regs[STV(1)].d));
-	// FPU_SET_C1(0);
-	return; // 2^x where x is chopped.
+	//FPU_SET_C1(0);
+	return; //2^x where x is chopped.
 }
 
 static void FPU_FSTENV(PhysPt addr)
 {
 	FPU_SET_TOP(TOP);
+
 	if (!cpu.code.big) {
 		mem_writew(addr + 0, static_cast<uint16_t>(fpu.cw));
 		mem_writew(addr + 2, static_cast<uint16_t>(fpu.sw));
@@ -666,19 +654,23 @@ static void FPU_FLDENV(PhysPt addr)
 {
 	uint16_t tag;
 	uint32_t tagbig;
-	Bitu cw;
+	uint16_t cw;
+	uint16_t sw;
+
 	if (!cpu.code.big) {
-		cw     = mem_readw(addr + 0);
-		fpu.sw = mem_readw(addr + 2);
-		tag    = mem_readw(addr + 4);
+		cw  = mem_readw(addr + 0);
+		sw  = mem_readw(addr + 2);
+		tag = mem_readw(addr + 4);
 	} else {
 		cw     = mem_readd(addr + 0);
-		fpu.sw = static_cast<uint16_t>(mem_readd(addr + 4));
+		sw     = static_cast<uint16_t>(mem_readd(addr + 4));
 		tagbig = mem_readd(addr + 8);
 		tag    = static_cast<uint16_t>(tagbig);
 	}
+
 	FPU_SetTag(tag);
 	FPU_SetCW(cw);
+	FPU_SetSW(sw);
 	TOP = FPU_GET_TOP();
 }
 
@@ -686,10 +678,12 @@ static void FPU_FSAVE(PhysPt addr)
 {
 	FPU_FSTENV(addr);
 	PhysPt start = (cpu.code.big ? 28 : 14);
+
 	for (int i = 0; i < 8; i++) {
 		FPU_ST80(addr + start, STV(i));
 		start += 10;
 	}
+
 	FPU_FINIT();
 }
 
@@ -697,21 +691,21 @@ static void FPU_FRSTOR(PhysPt addr)
 {
 	FPU_FLDENV(addr);
 	PhysPt start = (cpu.code.big ? 28 : 14);
+
 	for (int i = 0; i < 8; i++) {
 		fpu.regs[STV(i)].d = FPU_FLD80(addr + start);
 		start += 10;
 	}
 }
 
-static void FPU_FXTRACT(void)
-{
-	// function stores real bias in st and
+static void FPU_FXTRACT(void) {
+	// function stores real bias in st and 
 	// pushes the significant number onto the stack
 	// if double ever uses a different base please correct this function
 
-	FPU_Reg test       = fpu.regs[TOP];
-	int64_t exp80      = test.ll & LONGTYPE(0x7ff0000000000000);
-	int64_t exp80final = (exp80 >> 52) - BIAS64;
+	FPU_Reg test = fpu.regs[TOP];
+	int64_t exp80 =  test.ll&LONGTYPE(0x7ff0000000000000);
+	int64_t exp80final = (exp80>>52) - BIAS64;
 	Real64 mant = test.d / (std::pow(2.0, static_cast<Real64>(exp80final)));
 	fpu.regs[TOP].d = static_cast<Real64>(exp80final);
 	FPU_PUSH(mant);
