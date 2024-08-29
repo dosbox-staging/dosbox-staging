3218ed79e399c9718672fc96e7fe430360947a72
interloper98 <165201780+interloper98@users.noreply.github.com>
2024-08-28 20:38:11 -0700

    Let the mixer channel class return milliseconds per frame
    This function can be used to time events one a 1:1 frame basis
    (against the playback rate of the channel).

--- src/hardware/mixer.cpp
+++ src/hardware/mixer.cpp
@@ -1133,6 +1133,13 @@ float MixerChannel::GetFramesPerBlock() const
 	return static_cast<float>(mixer.blocksize) * stretch_factor;
 }
 
+double MixerChannel::GetMsPerFrame() const
+{
+	// Note: the double return value is used for PIC timing (which uses doubles)
+
+	return MillisInSecond / sample_rate_hz;
+}
+
 void MixerChannel::SetPeakAmplitude(const int peak)
 {
 	peak_amplitude = peak;
