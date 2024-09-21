dosbox-staging/dosbox-staging

commit be3d059469a867623026e6f180a779c547493a64

Handle the loading of 8 vs 9-dot fonts in `INT10_LoadFont()`
The previous way of passing in a special flag seemed to be hack, and it
appears SVGA cards that have the 9x14 font should prefer it in 720 pixel
wide (9-dot) modes.

src/ints/int10_memory.cpp
@@ -85,8 +85,10 @@ void INT10_LoadFont(const PhysPt _font, const bool reload, const int count,
 		MEM_BlockCopy(ftwhere + i * 32, font, height);
 		font += height;
 	}
-	// Load alternate character patterns
-	if (map & 0x80) {
+
+	// Load alternate 9x14 or 9x16 character patterns on VGA based on
+	// state of the clocking mode register
+	if (IS_VGA_ARCH && !vga.seq.clocking_mode.is_eight_dot_mode) {
 		while (auto chr = mem_readb(font++)) {
 			MEM_BlockCopy(ftwhere + chr * 32, font, height);
 			font += height;
@@ -153,28 +155,45 @@ void INT10_LoadFont(const PhysPt _font, const bool reload, const int count,
 	}
 }
 
-void INT10_ReloadFont(void) {
-	Bitu map=0;
-	switch(CurMode->cheight) {
+void INT10_ReloadFont()
+{
+	constexpr auto Reload   = false;
+	constexpr auto NumChars = 256;
+	constexpr auto Offset   = 0;
+	constexpr auto Map      = 0;
+
+	switch (CurMode->cheight) {
 	case 8:
-		INT10_LoadFont(RealToPhysical(int10.rom.font_8_first),false,256,0,map,8);
+
+		INT10_LoadFont(RealToPhysical(int10.rom.font_8_first),
+		               Reload,
+		               NumChars,
+		               Offset,
+		               Map,
+		               CurMode->cheight);
 		break;
+
 	case 14:
-		if (IS_VGA_ARCH && !vga.seq.clocking_mode.is_eight_dot_mode) {
-			map = 0x80;
-		}
-		INT10_LoadFont(RealToPhysical(int10.rom.font_14), false, 256, 0, map, 14);
+		INT10_LoadFont(RealToPhysical(int10.rom.font_14),
+		               Reload,
+		               NumChars,
+		               Offset,
+		               Map,
+		               CurMode->cheight);
 		break;
+
 	case 16:
 	default:
-		if (IS_VGA_ARCH && !vga.seq.clocking_mode.is_eight_dot_mode) {
-			map = 0x80;
-		}
-		INT10_LoadFont(RealToPhysical(int10.rom.font_16), false, 256, 0, map, 16);
+		constexpr auto Height = 16;
+		INT10_LoadFont(RealToPhysical(int10.rom.font_16),
+		               Reload,
+		               NumChars,
+		               Offset,
+		               Map,
+		               Height);
 		break;
 	}
 }
-
 
 void INT10_SetupRomMemory(void) {
 /* This should fill up certain structures inside the Video Bios Rom Area */

src/ints/int10.h
@@ -371,7 +371,7 @@ void INT10_GetPixel(uint16_t x,uint16_t y,uint8_t page,uint8_t * color);
 void INT10_LoadFont(const PhysPt _font, const bool reload, const int count,
                     const int offset, const int map, const int height);
 
-void INT10_ReloadFont(void);
+void INT10_ReloadFont();
 
 // Palette functions
 void INT10_SetBackgroundBorder(uint8_t val);

commit af452a85d5541b08803e25e3cbf1b1f9cffed981

Fix `INT10_ReloadFont()` regression & refactor
This fixes a regression in the "Druid: Daemons of the Mind" installer
where the whitespaces of the custom VGA font appear corrupted
(introduced in be3d059).

`INT10_ReloadFont()` should trigger the loading of alternate characters
for 9-dot VGA fonts only, while `INT10_LoadFont()` should never do this.
This has regressed during a recent cleanup & refactoring job.

This change also implements this conditional alternate charset loading
mechanism more cleanly. Previously, the `font_block` argument was used
as a "side channel" for signalling this criteria via a hack (by using a
magic value of 0x80 which is not a valid value in the real BIOS call).

src/ints/int10.cpp
@@ -234,7 +234,7 @@ static Bitu INT10_Handler(void) {
 			const auto reload      = (reg_al == 0x10);
 			const auto num_chars   = reg_cx;
 			const auto first_char  = reg_dx;
-			const auto font_block  = reg_bl & 0x7f;
+			const auto font_block  = reg_bl;
 			const auto char_height = reg_bh;
 
 			INT10_LoadFont(font_data,
@@ -253,7 +253,7 @@ static Bitu INT10_Handler(void) {
 			const auto reload         = (reg_al == 0x11);
 			constexpr auto NumChars   = 256;
 			constexpr auto FirstChar  = 0;
-			const auto font_block     = reg_bl & 0x7f;
+			const auto font_block     = reg_bl;
 			constexpr auto CharHeight = 14;
 
 			INT10_LoadFont(font_data,
@@ -272,7 +272,7 @@ static Bitu INT10_Handler(void) {
 			const auto reload         = (reg_al == 0x12);
 			constexpr auto NumChars   = 256;
 			constexpr auto FirstChar  = 0;
-			const auto font_block     = reg_bl & 0x7f;
+			const auto font_block     = reg_bl;
 			constexpr auto CharHeight = 8;
 
 			INT10_LoadFont(font_data,
@@ -301,7 +301,7 @@ static Bitu INT10_Handler(void) {
 			const auto reload         = (reg_al == 0x14);
 			constexpr auto NumChars   = 256;
 			constexpr auto FirstChar  = 0;
-			const auto font_block     = reg_bl & 0x7f;
+			const auto font_block     = reg_bl;
 			constexpr auto CharHeight = 16;
 
 			INT10_LoadFont(font_data,

src/ints/int10_memory.cpp
@@ -45,9 +45,9 @@ static const std::vector<uint16_t> map_offset = {
 };
 // clang-format on
 
-void INT10_LoadFont(const PhysPt _font_data, const bool reload,
-                    const int num_chars, const int first_char,
-                    const int _font_block, const int char_height)
+static void load_font(const PhysPt _font_data, const bool reload, const int num_chars,
+                      const int first_char, const int _font_block,
+                      const int char_height, const bool load_alternate_chars)
 {
 	constexpr auto BytesPerChar = 32;
 
@@ -93,13 +93,13 @@ void INT10_LoadFont(const PhysPt _font_data, const bool reload,
 		font_data += char_height;
 	}
 
-	// Load alternate 9x14 or 9x16 character patterns on VGA based on
-	// state of the clocking mode register
-	if (IS_VGA_ARCH && !vga.seq.clocking_mode.is_eight_dot_mode) {
+	// Load alternate 9x14 or 9x16 character patterns on VGA
+	if (load_alternate_chars) {
 		while (auto chr = mem_readb(font_data++)) {
 			MEM_BlockCopy(font_base_addr + chr * BytesPerChar,
 			              font_data,
 			              char_height);
+
 			font_data += char_height;
 		}
 	}
@@ -172,6 +172,14 @@ void INT10_LoadFont(const PhysPt _font_data, const bool reload,
 	}
 }
 
+void INT10_LoadFont(const PhysPt font_data, const bool reload,
+                    const int num_chars, const int first_char,
+                    const int font_block, const int char_height)
+{
+	constexpr auto LoadAlternateChars = false;
+	load_font(font_data, reload, num_chars, first_char, font_block, char_height, LoadAlternateChars);
+}
+
 void INT10_ReloadFont()
 {
 	constexpr auto Reload    = false;
@@ -179,27 +187,51 @@ void INT10_ReloadFont()
 	constexpr auto FirstChar = 0;
 	constexpr auto FontBlock = 0;
 
+	const auto is_vga_9dot_font = (IS_VGA_ARCH &&
+	                               !vga.seq.clocking_mode.is_eight_dot_mode);
+
 	switch (CurMode->cheight) {
 	case 8: {
 		const auto font_data = RealToPhysical(int10.rom.font_8_first);
-		constexpr auto CharHeight = 8;
+		constexpr auto CharHeight         = 8;
+		constexpr auto LoadAlternateChars = false;
 
-		INT10_LoadFont(font_data, Reload, NumChars, FirstChar, FontBlock, CharHeight);
+		load_font(font_data,
+		          Reload,
+		          NumChars,
+		          FirstChar,
+		          FontBlock,
+		          CharHeight,
+		          LoadAlternateChars);
 	} break;
 
 	case 14: {
 		const auto font_data      = RealToPhysical(int10.rom.font_14);
 		constexpr auto CharHeight = 14;
+		const auto load_alternate_chars = is_vga_9dot_font;
 
-		INT10_LoadFont(font_data, Reload, NumChars, FirstChar, FontBlock, CharHeight);
+		load_font(font_data,
+		          Reload,
+		          NumChars,
+		          FirstChar,
+		          FontBlock,
+		          CharHeight,
+		          load_alternate_chars);
 	} break;
 
 	case 16: {
 	default:
 		const auto font_data      = RealToPhysical(int10.rom.font_16);
 		constexpr auto CharHeight = 16;
+		const auto load_alternate_chars = is_vga_9dot_font;
 
-		INT10_LoadFont(font_data, Reload, NumChars, FirstChar, FontBlock, CharHeight);
+		load_font(font_data,
+		          Reload,
+		          NumChars,
+		          FirstChar,
+		          FontBlock,
+		          CharHeight,
+		          load_alternate_chars);
 	} break;
 	}
 }