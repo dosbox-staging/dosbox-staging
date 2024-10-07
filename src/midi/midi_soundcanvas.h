3c084482aa02e62120940687dd924e704b583e62
John Novak <john@johnnovak.net>
2024-10-07 14:00:09 +1000

    Add generic Roland Sound Canvas CLAP plugin support
    This adds support for Sound Canvas CLAP plugins in a completely decoupled,
    generic, and implementation-agnostic manner. MIDI-capable instrument
    plugins likely to be SC-55 emulations are auto-discovered by their
    descriptions alone.
    
    For example, the inclusion criteria for the Sound Canvas SC-55 mk2 v1.01
    emulation is if the plugin's description string contains:
    
    - "sc55 mk2 1.01",
    - or "sc-55mk2 v1.01",
    - or "sc55-mk2 1.01",
    - or a large number of further permutations.
    
    Due to this decoupled mechanism, licensing incompatibilities between any
    3rd-party CLAP plugin we load at runtime and DOSBox Staging (which is
    GPL v2 licensed) become a non-issue. This is because we require no
    knowledge whatsoever of the third party plugins we load; communication
    is 100% accomplished via open protocols:
    
    - CLAP for plugin discovery and instantiation,
    - MIDI for sending data to the plugin (via the CLAP protocol),
    - and raw 32-bit float audio streams to receive data from the plugin
      (via the CLAP protocol)

---
+++ src/midi/midi_soundcanvas.h
@@ -0,0 +1,120 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2024-2024  The DOSBox Staging Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DOSBOX_SOUNDCANVAS_H
+#define DOSBOX_SOUNDCANVAS_H
+
+#include "midi_device.h"
+
+#include <memory>
+#include <optional>
+
+#include "../audio/clap/event_list.h"
+#include "../audio/clap/plugin.h"
+#include "mixer.h"
+#include "rwqueue.h"
+
+enum class SoundCanvasModel {
+	// Roland SC-55
+	Sc55_100,
+	Sc55_110,
+	Sc55_120,
+	Sc55_121,
+	Sc55_200,
+
+	// Roland SC-55mk2
+	Sc55mk2_100,
+	Sc55mk2_101,
+};
+
+struct SoundCanvasSynthModel {
+	SoundCanvasModel model = {};
+
+	const char* config_name        = {};
+	const char* display_name_short = {};
+	const char* display_name_long  = {};
+
+	bool operator==(const SoundCanvasSynthModel* other) const
+	{
+		return model == (*other).model;
+	}
+};
+
+class MidiDeviceSoundCanvas final : public MidiDevice {
+public:
+	// Throws `std::runtime_error` if the MIDI device cannot be initialiased
+	// (e.g., the requested SoundFont cannot be loaded).
+	MidiDeviceSoundCanvas();
+
+	~MidiDeviceSoundCanvas() override;
+
+	// prevent copying
+	MidiDeviceSoundCanvas(const MidiDeviceSoundCanvas&) = delete;
+	// prevent assignment
+	MidiDeviceSoundCanvas& operator=(const MidiDeviceSoundCanvas&) = delete;
+
+	std::string GetName() const override
+	{
+		return MidiDeviceName::SoundCanvas;
+	}
+
+	Type GetType() const override
+	{
+		return MidiDevice::Type::Internal;
+	}
+
+	SoundCanvasSynthModel GetModel() const;
+
+	void SendMidiMessage(const MidiMessage& msg) override;
+	void SendSysExMessage(uint8_t* sysex, size_t len) override;
+
+private:
+	void MixerCallback(const int requested_audio_frames);
+	void ProcessWorkFromFifo();
+
+	int GetNumPendingAudioFrames();
+	void RenderAudioFramesToFifo(const int num_frames = 1);
+	void Render();
+
+	// Managed objects
+	MixerChannelPtr mixer_channel = nullptr;
+	RWQueue<AudioFrame> audio_frame_fifo{1};
+	RWQueue<MidiWork> work_fifo{1};
+
+	struct {
+		std::unique_ptr<Clap::Plugin> plugin = nullptr;
+		Clap::EventList event_list           = {};
+	} clap = {};
+
+	std::thread renderer = {};
+
+	SoundCanvasSynthModel model = {};
+
+	// Used to track the balance of time between the last mixer callback
+	// versus the current MIDI SysEx or Msg event.
+	double last_rendered_ms   = 0.0;
+	double ms_per_audio_frame = 0.0;
+
+	bool had_underruns = false;
+};
+
+void SOUNDCANVAS_ListDevices(MidiDeviceSoundCanvas* device, Program* caller);
+
+#endif // DOSBOX_SOUNDCANVAS_H
