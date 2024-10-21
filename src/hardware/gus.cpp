dosbox-staging/dosbox-staging

commit d75e1057f5c6f7854b7ab1d9b3874cc8a6b8afce

Move mixer to seperate thread

src/hardware/gus.cpp
@@ -296,6 +296,7 @@ private:
 	write_io_array_t write_handlers         = {};
 	std::vector<Voice> voices               = {};
 	std::vector<AudioFrame> rendered_frames = {};
+	std::mutex mutex                        = {};
 
 	const address_array_t dma_addresses = {
 	        {MIN_DMA_ADDRESS, 1, 3, 5, 6, MAX_IRQ_ADDRESS, 0, 0}
@@ -627,6 +628,8 @@ Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pr
           irq1(irq_pref),
           irq2(irq_pref)
 {
+	MIXER_LockMixerThread();
+
 	// port operations are "zero-based" from the datum to the user's port
 	constexpr io_port_t port_datum = 0x200;
 	port_base                      = port_pref - port_datum;
@@ -682,6 +685,8 @@ Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pr
 	SetupEnvironment(port_pref, ultradir);
 
 	LOG_MSG("GUS: Running on port %xh, IRQ %d, and DMA %d", port_pref, irq1, dma1);
+
+	MIXER_UnlockMixerThread();
 }
 
 void Gus::ActivateVoices(uint8_t requested_voices)
@@ -732,6 +737,8 @@ const std::vector<AudioFrame>& Gus::RenderFrames(const int num_requested_frames)
 
 void Gus::RenderUpToNow()
 {
+	std::lock_guard lock(mutex);
+
 	const auto now = PIC_FullIndex();
 
 	// Wake up the channel and update the last rendered time datum.
@@ -760,6 +767,8 @@ void Gus::RenderUpToNow()
 void Gus::AudioCallback(const int num_requested_frames)
 {
 	assert(audio_channel);
+
+	std::lock_guard lock(mutex);
 
 #if 0
 	if (fifo.size())
@@ -1654,6 +1663,9 @@ void Gus::WriteToRegister()
 Gus::~Gus()
 {
 	LOG_MSG("GUS: Shutting down");
+
+	MIXER_LockMixerThread();
+
 	StopAndReset();
 
 	// Prevent discovery of the GUS via the environment
@@ -1676,6 +1688,8 @@ Gus::~Gus()
 	if (dma_channel) {
 		dma_channel->Reset();
 	}
+
+	MIXER_UnlockMixerThread();
 }
 
 void GUS_MirrorAdLibCommandPortWrite([[maybe_unused]] const io_port_t port,

include/audio_vector.h
@@ -1,0 +1,199 @@
+/*
+ *  Copyright (C) 2024-2024  The DOSBox Staging Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef AUDIO_VECTOR_H
+#define AUDIO_VECTOR_H
+
+#include <cstdint>
+#include <vector>
+
+#include "config.h"
+#include "mixer.h"
+
+// The producer constructs the object and the consumer calls AddSamples()
+
+// Currently only used by SoundBlaster
+// Think before using this for other audio devices
+// Usage of this class causes more heap allocations than adding samples directly to an RWQueue
+// SoundBlaster needs this because it can switch between different formats at runtime
+
+// Suitable where incoming samples are guaranteed to be little-endian,
+// such as DOS 16-bit DMA (SB and GUS), as well as raw bin+cue CDDA.
+// (Not suitable for synths that output 16-bit samples or the audio
+//  codecs, which produce host-endian samples)
+
+class AudioVector
+{
+public:
+	AudioVector(int _num_frames) : num_frames(_num_frames) {}
+	virtual ~AudioVector() = default;
+	virtual void AddSamples(MixerChannel* channel) = 0;
+	int num_frames = 0;
+};
+
+class AudioVectorM8 final : public AudioVector
+{
+public:
+	AudioVectorM8(int _num_frames, const uint8_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + _num_frames) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		channel->AddSamples_m8(num_frames, data.data());
+	}
+private:
+	std::vector<uint8_t> data = {};
+};
+
+class AudioVectorM8S final : public AudioVector
+{
+public:
+	AudioVectorM8S(int _num_frames, const int8_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + _num_frames) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		channel->AddSamples_m8s(num_frames, data.data());
+	}
+private:
+	std::vector<int8_t> data = {};
+};
+
+class AudioVectorS8 final : public AudioVector
+{
+public:
+	AudioVectorS8(int _num_frames, const uint8_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + (_num_frames * 2)) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		channel->AddSamples_s8(num_frames, data.data());
+	}
+private:
+	std::vector<uint8_t> data = {};
+};
+
+class AudioVectorS8S final : public AudioVector
+{
+public:
+	AudioVectorS8S(int _num_frames, const int8_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + (_num_frames * 2)) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		channel->AddSamples_s8s(num_frames, data.data());
+	}
+private:
+	std::vector<int8_t> data = {};
+};
+
+class AudioVectorS16 final : public AudioVector
+{
+public:
+	AudioVectorS16(int _num_frames, const int16_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + (_num_frames * 2)) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		#if defined(WORDS_BIGENDIAN)
+		channel->AddSamples_s16_nonnative(num_frames, data.data());
+		#else
+		channel->AddSamples_s16(num_frames, data.data());
+		#endif
+	}
+private:
+	std::vector<int16_t> data = {};
+};
+
+class AudioVectorS16U final : public AudioVector
+{
+public:
+	AudioVectorS16U(int _num_frames, const uint16_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + (_num_frames * 2)) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		#if defined(WORDS_BIGENDIAN)
+		channel->AddSamples_s16u_nonnative(num_frames, data.data());
+		#else
+		channel->AddSamples_s16u(num_frames, data.data());
+		#endif
+	}
+private:
+	std::vector<uint16_t> data = {};
+};
+
+class AudioVectorM16 final : public AudioVector
+{
+public:
+	AudioVectorM16(int _num_frames, const int16_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + _num_frames) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		#if defined(WORDS_BIGENDIAN)
+		channel->AddSamples_m16_nonnative(num_frames, data.data());
+		#else
+		channel->AddSamples_m16(num_frames, data.data());
+		#endif
+	}
+private:
+	std::vector<int16_t> data = {};
+};
+
+class AudioVectorM16U final : public AudioVector
+{
+public:
+	AudioVectorM16U(int _num_frames, const uint16_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + _num_frames) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		#if defined(WORDS_BIGENDIAN)
+		channel->AddSamples_m16u_nonnative(num_frames, data.data());
+		#else
+		channel->AddSamples_m16u(num_frames, data.data());
+		#endif
+	}
+private:
+	std::vector<uint16_t> data = {};
+};
+
+class AudioVectorStretched final : public AudioVector {
+public:
+	AudioVectorStretched(int _num_frames, const int16_t* _data)
+		: AudioVector(_num_frames),
+		  data(_data, _data + _num_frames) {}
+
+	void AddSamples(MixerChannel* channel) final
+	{
+		channel->AddStretched(num_frames, data.data());
+	}
+private:
+	std::vector<int16_t> data = {};
+};
+
+#endif

include/mixer.h
@@ -29,6 +29,7 @@
 #include <functional>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <set>
 #include <string>
 #include <utility>
@@ -38,6 +39,7 @@
 #include "audio_frame.h"
 #include "control.h"
 #include "envelope.h"
+#include "math_utils.h"
 
 #include <Iir.h>
 
@@ -51,9 +53,6 @@ enum class MixerState { Uninitialized, NoSound, On, Muted };
 
 static constexpr int MixerBufferByteSize = 16 * 1024;
 static constexpr int MixerBufferMask     = MixerBufferByteSize - 1;
-
-// TODO This is hacky and should be removed. Only the PS1 Audio uses it.
-extern uint8_t MixTemp[MixerBufferByteSize];
 
 // TODO This seems like is a general-purpose lookup, consider moving it
 extern int16_t lut_u8to16[UINT8_MAX + 1];
@@ -180,6 +179,8 @@ public:
 	std::set<ChannelFeature> GetFeatures() const;
 	const std::string& GetName() const;
 	int GetSampleRate() const;
+	float GetFramesPerTick() const;
+	float GetFramesPerBlock() const;
 
 	void Set0dbScalar(const float f);
 	void UpdateCombinedVolume();
@@ -259,16 +260,58 @@ public:
 
 	void AddStretched(const int num_frames, int16_t* data);
 
-	void FillUp();
 	void Enable(const bool should_enable);
 
 	// Pass-through to the sleeper
 	bool WakeUp();
 
-	// Timing on how many sample frames have been done by the mixer
-	std::atomic<int> frames_done = 0;
+	std::vector<AudioFrame> audio_frames = {};
+	std::recursive_mutex mutex = {};
 
-	bool is_enabled = false;
+	std::atomic<bool> is_enabled = false;
+
+	struct {
+		float level     = 0.0f;
+		float send_gain = 0.0f;
+	} reverb            = {};
+	bool do_reverb_send = false;
+
+
+	struct {
+		float level     = 0.0f;
+		float send_gain = 0.0f;
+	} chorus            = {};
+	bool do_chorus_send = false;
+
+	class Sleeper {
+	public:
+		Sleeper() = delete;
+		Sleeper(MixerChannel& c, const int sleep_after_ms = DefaultWaitMs);
+		bool ConfigureFadeOut(const std::string& prefs);
+		AudioFrame MaybeFadeOrListen(const AudioFrame& frame);
+		void MaybeSleep();
+		bool WakeUp();
+
+	private:
+		void DecrementFadeLevel(const int awake_for_ms);
+
+		MixerChannel& channel;
+
+		// The wait before fading or sleeping is bound between:
+		static constexpr auto MinWaitMs     = 100;
+		static constexpr auto DefaultWaitMs = 500;
+		static constexpr auto MaxWaitMs     = 5000;
+
+		int64_t woken_at_ms            = {};
+		float fadeout_level            = {};
+		float fadeout_decrement_per_ms = {};
+		int fadeout_or_sleep_after_ms  = {};
+
+		bool wants_fadeout = false;
+		bool had_signal    = false;
+	};
+	Sleeper sleeper;
+	bool do_sleep = false;
 
 private:
 	// prevent default construction, copying, and assignment
@@ -279,8 +322,7 @@ private:
 	AudioFrame ConvertNextFrame(const Type* data, const int pos);
 
 	template <class Type, bool stereo, bool signeddata, bool nativeorder>
-	void ConvertSamplesAndMaybeZohUpsample(const Type* data, const int frames,
-	                                       std::vector<float>& out);
+	std::vector<AudioFrame> ConvertSamplesAndMaybeZohUpsample(const Type* data, const int frames);
 
 	void ConfigureResampler();
 	void ClearResampler();
@@ -295,17 +337,14 @@ private:
 
 	std::set<ChannelFeature> features = {};
 
-	// When this flows over a new sample needs to be read from the device
-	int freq_counter = 0u;
-
 	// Timing on how many samples were needed by the mixer
-	int frames_needed = 0;
+	size_t frames_needed = 0;
 
 	// Previous and next sample fames
 	AudioFrame prev_frame = {};
 	AudioFrame next_frame = {};
 
-	int sample_rate_hz = 0;
+	std::atomic<int> sample_rate_hz = 0;
 
 	// Volume gains
 	// ~~~~~!~~~~~~
@@ -398,48 +437,6 @@ private:
 		float pan_right = 0.0f;
 	} crossfeed       = {};
 	bool do_crossfeed = false;
-
-	struct {
-		float level     = 0.0f;
-		float send_gain = 0.0f;
-	} reverb            = {};
-	bool do_reverb_send = false;
-
-	struct {
-		float level     = 0.0f;
-		float send_gain = 0.0f;
-	} chorus            = {};
-	bool do_chorus_send = false;
-
-	class Sleeper {
-	public:
-		Sleeper() = delete;
-		Sleeper(MixerChannel& c, const int sleep_after_ms = DefaultWaitMs);
-		bool ConfigureFadeOut(const std::string& prefs);
-		AudioFrame MaybeFadeOrListen(const AudioFrame& frame);
-		void MaybeSleep();
-		bool WakeUp();
-
-	private:
-		void DecrementFadeLevel(const int awake_for_ms);
-
-		MixerChannel& channel;
-
-		// The wait before fading or sleeping is bound between:
-		static constexpr auto MinWaitMs     = 100;
-		static constexpr auto DefaultWaitMs = 500;
-		static constexpr auto MaxWaitMs     = 5000;
-
-		int64_t woken_at_ms            = {};
-		float fadeout_level            = {};
-		float fadeout_decrement_per_ms = {};
-		int fadeout_or_sleep_after_ms  = {};
-
-		bool wants_fadeout = false;
-		bool had_signal    = false;
-	};
-	Sleeper sleeper;
-	const bool do_sleep = false;
 };
 
 using MixerChannelPtr = std::shared_ptr<MixerChannel>;
@@ -457,6 +454,7 @@ void MIXER_DeregisterChannel(MixerChannelPtr& channel);
 void MIXER_AddConfigSection(const ConfigPtr& conf);
 int MIXER_GetSampleRate();
 int MIXER_GetPreBufferMs();
+bool MIXER_FastForwardModeEnabled();
 
 const AudioFrame MIXER_GetMasterVolume();
 void MIXER_SetMasterVolume(const AudioFrame volume);
@@ -464,8 +462,9 @@ void MIXER_SetMasterVolume(const AudioFrame volume);
 void MIXER_Mute();
 void MIXER_Unmute();
 
-void MIXER_LockAudioDevice();
-void MIXER_UnlockAudioDevice();
+void MIXER_LockMixerThread();
+void MIXER_UnlockMixerThread();
+void MIXER_CloseAudioDevice();
 
 // Return true if the mixer was explicitly muted by the user (as opposed to
 // auto-muted when `mute_when_inactive` is enabled)
@@ -479,5 +478,69 @@ void MIXER_SetReverbPreset(const ReverbPreset new_preset);
 
 ChorusPreset MIXER_GetChorusPreset();
 void MIXER_SetChorusPreset(const ChorusPreset new_preset);
+
+// Generic callback used for audio devices which generate audio on the main thread
+// These devices produce audio on the main thread and consume on the mixer thread
+// This callback is the consumer part
+template <class DeviceType, class AudioType, bool stereo, bool signeddata, bool nativeorder>
+inline void MIXER_PullFromQueueCallback(const int frames_requested, DeviceType* device)
+{
+	// Currently only handles mono sound (output_queue is a primitive type and frames == samples)
+	// Special case for AudioType == AudioFrame (stereo floating-point sound)
+	static_assert((!stereo) || std::is_same<AudioType, AudioFrame>::value);
+
+	// AudioFrame type is always stereo
+	static_assert(stereo || (!std::is_same<AudioType, AudioFrame>::value));
+
+	size_t clamped_frames = check_cast<size_t>(frames_requested);
+	if (MIXER_FastForwardModeEnabled()) {
+		// Special case, normally only hit when using the fast-forward hotkey (Alt + F12)
+		// We need a very large buffer to compensate or it results in static
+
+		// Mostly arbitrary but it works well in testing.
+		// The queue just needs to be large enough to hold the large frame requests we get in fast-forward mode.
+		// This value can be tweaked without much consequence if it ever becomes problematic.
+		constexpr float MaxExpectedFastForwardFactor = 100.0f;
+		device->output_queue.Resize(iceil(device->channel->GetFramesPerBlock() * MaxExpectedFastForwardFactor));
+
+		// We must not block in fast-forward mode or the timings used by the mixer get confused
+		// Clamp and fill with silence if needed
+		clamped_frames = std::min(clamped_frames, device->output_queue.Size());
+	} else {
+		// Normal case, resize the queue to ensure we don't have high latency
+		// Resize is a fast operation, only setting a variable for max capacity
+		// It does not drop frames or append zeros to the end of the underlying data structure
+
+		// Size to 2x blocksize. The mixer callback will request 1x blocksize.
+		// This provides a good size to avoid over-runs and stalls.
+		device->output_queue.Resize(iceil(device->channel->GetFramesPerBlock() * 2.0f));
+	}
+	int frames_recieved = 0;
+	if (clamped_frames > 0) {
+		std::vector<AudioType> to_mix = {};
+		device->output_queue.BulkDequeue(to_mix, clamped_frames);
+		frames_recieved = check_cast<int>(to_mix.size());
+		if (frames_recieved > 0) {
+			// One of the GCC CI builds throws a duplicated branch warning
+			// Clang apparently doesn't have this warning because it throws an "unknown warning" warning in the pragma
+			#ifndef __clang__
+			#pragma GCC diagnostic push
+			#pragma GCC diagnostic ignored "-Wduplicated-branches"
+			#endif
+			if (std::is_same<AudioType, AudioFrame>::value) {
+				// AudioFrame has the same memory layout as 2x floats
+				device->channel->template AddSamples<float, stereo, signeddata, nativeorder>(frames_recieved, reinterpret_cast<float*>(to_mix.data()));
+			} else {
+				device->channel->template AddSamples<AudioType, stereo, signeddata, nativeorder>(frames_recieved, to_mix.data());
+			}
+			#ifndef __clang__
+			#pragma GCC diagnostic pop
+			#endif
+		}
+	}
+	if (frames_requested > frames_recieved) {
+		device->channel->AddSilence();
+	}
+}
 
 #endif

include/rwqueue.h
@@ -99,6 +99,10 @@ public:
 	//  return false and the item will not be queued.
 	bool Enqueue(T&& item);
 
+	// Returns false and does nothing if the queue is at capacity or the queue is not running
+	// Otherwise the item gets moved into the queue and it returns true
+	bool NonblockingEnqueue(T&& item);
+
 	// The method potentially blocks until there is at least a single item
 	// in the queue to dequeue.
 
@@ -130,6 +134,13 @@ public:
 	// stopping will be available in the queue however the items that came
 	// after stopping will not be queued.
 	bool BulkEnqueue(std::vector<T>& from_source, const size_t num_requested);
+
+	// Does nothing if queue is at capacity or queue is not running.
+	// Otherwise, enqueues as many elements as possible until the queue is at capacity.
+	// Moved elements will be erased from the source vector.
+	// Elements not enqueued will be left in the source vector.
+	// Returns the number of elements enqueued.
+	size_t NonblockingBulkEnqueue(std::vector<T>& from_source, const size_t num_requested);
 
 	// The target vector will be resized to accomodate, if needed. The
 	// method potentially blocks until the requested number of items have

src/dos/cdrom.cpp
@@ -172,6 +172,7 @@ void CDROM_Interface_Physical::InitAudio()
 		return;
 	}
 
+	MIXER_LockMixerThread();
 	auto callback = std::bind(&CDROM_Interface_Physical::CdAudioCallback,
 	                          this,
 	                          std::placeholders::_1);
@@ -182,6 +183,7 @@ void CDROM_Interface_Physical::InitAudio()
 	                                  ChannelFeature::DigitalAudio});
 
 	thread = std::thread(&CDROM_Interface_Physical::CdReaderLoop, this);
+	MIXER_UnlockMixerThread();
 }
 
 void CDROM_Interface_Physical::CdAudioCallback(const int requested_frames)

src/dos/cdrom_image.cpp
@@ -488,6 +488,7 @@ CDROM_Interface_Image::CDROM_Interface_Image()
 {
 	if (refCount == 0) {
 		if (!player.channel) {
+			MIXER_LockMixerThread();
 			const auto mixer_callback = std::bind(&CDROM_Interface_Image::CDAudioCallBack,
 			                                      this, std::placeholders::_1);
 
@@ -498,6 +499,7 @@ CDROM_Interface_Image::CDROM_Interface_Image()
 			                                   ChannelFeature::DigitalAudio});
 
 			player.channel->Enable(false); // only enabled during playback periods
+			MIXER_UnlockMixerThread();
 		}
 #ifdef DEBUG
 		LOG_MSG("CDROM: Initialised the %s audio channel", ChannelName::CdAudio);
@@ -508,6 +510,7 @@ CDROM_Interface_Image::CDROM_Interface_Image()
 
 CDROM_Interface_Image::~CDROM_Interface_Image()
 {
+	MIXER_LockMixerThread();
 	refCount--;
 
 	// Stop playback before wiping out the CD Player
@@ -523,6 +526,7 @@ CDROM_Interface_Image::~CDROM_Interface_Image()
 	if (player.cd == this) {
 		player.cd = nullptr;
 	}
+	MIXER_UnlockMixerThread();
 }
 
 bool CDROM_Interface_Image::SetDevice(const char* path)

src/dos/program_mixer.cpp
@@ -757,8 +757,6 @@ void MIXER::Run()
 
 	const auto args = cmd->GetArguments();
 
-	MIXER_LockAudioDevice();
-
 	auto result = MixerCommand::ParseCommands(args,
 	                                          create_channel_infos(),
 	                                          AllChannelNames);
@@ -788,8 +786,6 @@ void MIXER::Run()
 		LOG_WARNING("MIXER: Incorrect MIXER command invocation; "
 		            "run MIXER /? for help");
 	}
-
-	MIXER_UnlockAudioDevice();
 }
 
 void MIXER::AddMessages()
@@ -929,8 +925,6 @@ void MIXER::ShowMixerStatus()
 	const auto off_value      = MSG_Get("SHELL_CMD_MIXER_CHANNEL_OFF");
 	constexpr auto none_value = "-";
 
-	MIXER_LockAudioDevice();
-
 	constexpr auto master_channel_string = "[color=light-cyan]MASTER[reset]";
 
 	show_channel(convert_ansi_markup(master_channel_string),
@@ -984,6 +978,4 @@ void MIXER::ShowMixerStatus()
 		             reverb,
 		             chorus);
 	}
-
-	MIXER_UnlockAudioDevice();
 }

commit bc779686ef6e28d841e68aa22392c5c5c13063e5

Move GUS rendering to main thread
Fixes a regression from d75e1057f5c6f7854b7ab1d9b3874cc8a6b8afce
Some games like Blood heavily use DMA for digitized sound
This causes audio distortion likely due to a timing issue on memory reads

Moving GUS to the main thread resolves this issue
Will need to look for a better asynchronous solution in the future

src/hardware/gus.cpp
@@ -42,13 +42,14 @@
 #include "setup.h"
 #include "shell.h"
 #include "string_utils.h"
+#include "timer.h"
 
 #define LOG_GUS 0 // set to 1 for detailed logging
 
 static void GUS_TimerEvent(uint32_t t);
 static void GUS_DMA_Event(uint32_t val);
 
-static std::unique_ptr<Gus> gus = nullptr;
+std::unique_ptr<Gus> gus = nullptr;
 
 Voice::Voice(uint8_t num, VoiceIrq& irq) noexcept
         : vol_ctrl{irq.vol_state},
@@ -334,6 +335,18 @@ constexpr uint8_t to_external_irq(const uint8_t irq)
 	return irq == 9 ? 2 : irq;
 }
 
+static void GUS_PicCallback()
+{
+	if (!gus || !gus->channel->is_enabled) {
+		return;
+	}
+
+	gus->frame_counter += gus->channel->GetFramesPerTick();
+	const auto requested_frames = ifloor(gus->frame_counter);
+	gus->frame_counter -= static_cast<float>(requested_frames);
+	gus->PicCallback(requested_frames);
+}
+
 Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pref,
          const char* ultradir, const std::string& filter_prefs)
         : ram(RAM_SIZE),
@@ -355,12 +368,16 @@ Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pr
 
 	RegisterIoHandlers();
 
-	// Register the Audio and DMA channels
-	const auto mixer_callback = std::bind(&Gus::AudioCallback,
-	                                      this,
-	                                      std::placeholders::_1);
+	constexpr bool Stereo = true;
+	constexpr bool SignedData = true;
+	constexpr bool NativeOrder = true;
 
-	audio_channel = MIXER_AddChannel(mixer_callback,
+	// Register the Audio and DMA channels
+	const auto mixer_callback = std::bind(MIXER_PullFromQueueCallback<Gus, AudioFrame, Stereo, SignedData, NativeOrder>,
+	                                      std::placeholders::_1,
+	                                      this);
+
+	channel = MIXER_AddChannel(mixer_callback,
 	                                 UseMixerRate,
 	                                 ChannelName::GravisUltrasound,
 	                                 {ChannelFeature::Sleep,
@@ -369,26 +386,26 @@ Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pr
 	                                  ChannelFeature::ChorusSend,
 	                                  ChannelFeature::DigitalAudio});
 
-	assert(audio_channel);
+	assert(channel);
 
 	// GUS is prone to accumulating beyond the 16-bit range so we scale back
 	// by RMS.
 	constexpr auto rms_squared = static_cast<float>(M_SQRT1_2);
-	audio_channel->Set0dbScalar(rms_squared);
+	channel->Set0dbScalar(rms_squared);
 
-	if (!audio_channel->TryParseAndSetCustomFilter(filter_prefs)) {
+	if (!channel->TryParseAndSetCustomFilter(filter_prefs)) {
 		if (filter_prefs != "off") {
 			LOG_WARNING("GUS: Invalid 'gus_filter' setting: '%s', using 'off'",
 			            filter_prefs.c_str());
 		}
 
-		audio_channel->SetHighPassFilter(FilterState::Off);
-		audio_channel->SetLowPassFilter(FilterState::Off);
+		channel->SetHighPassFilter(FilterState::Off);
+		channel->SetLowPassFilter(FilterState::Off);
 
 		set_section_property_value("gus", "gus_filter", "off");
 	}
 
-	ms_per_render = MillisInSecond / audio_channel->GetSampleRate();
+	ms_per_render = MillisInSecond / channel->GetSampleRate();
 
 	UpdatePlaybackDmaAddress(dma_pref);
 	UpdateRecordingDmaAddress(dma_pref);
@@ -397,6 +414,9 @@ Gus::Gus(const io_port_t port_pref, const uint8_t dma_pref, const uint8_t irq_pr
 	PopulateVolScalars();
 	PopulatePanScalars();
 	SetupEnvironment(port_pref, ultradir);
+
+	output_queue.Resize(iceil(channel->GetFramesPerBlock() * 2.0f));
+	TIMER_AddTickHandler(GUS_PicCallback);
 
 	LOG_MSG("GUS: Running on port %xh, IRQ %d, and DMA %d",
 	        port_pref,
@@ -421,7 +441,7 @@ void Gus::ActivateVoices(uint8_t requested_voices)
 
 		ms_per_render = MillisInSecond / sample_rate_hz;
 
-		audio_channel->SetSampleRate(sample_rate_hz);
+		channel->SetSampleRate(sample_rate_hz);
 	}
 
 	if (active_voices && prev_logged_voices != active_voices) {
@@ -465,8 +485,8 @@ void Gus::RenderUpToNow()
 	const auto now = PIC_FullIndex();
 
 	// Wake up the channel and update the last rendered time datum.
-	assert(audio_channel);
-	if (audio_channel->WakeUp()) {
+	assert(channel);
+	if (channel->WakeUp()) {
 		last_rendered_ms = now;
 		return;
 	}
@@ -487,11 +507,9 @@ void Gus::RenderUpToNow()
 	}
 }
 
-void Gus::AudioCallback(const int num_requested_frames)
+void Gus::PicCallback(const int num_requested_frames)
 {
-	assert(audio_channel);
-
-	std::lock_guard lock(mutex);
+	assert(channel);
 
 #if 0
 	if (fifo.size())
@@ -502,15 +520,15 @@ void Gus::AudioCallback(const int num_requested_frames)
 
 	// First, send any frames we've queued since the last callback
 	while (num_frames_remaining && fifo.size()) {
-		audio_channel->AddSamples_sfloat(1, &fifo.front()[0]);
+		AudioFrame frame = fifo.front();
 		fifo.pop();
+		output_queue.NonblockingEnqueue(std::move(frame));
 		--num_frames_remaining;
 	}
 	// If the queue's run dry, render the remainder and sync-up our time datum
 	if (num_frames_remaining > 0) {
-		const auto frames = RenderFrames(num_frames_remaining);
-		audio_channel->AddSamples_sfloat(num_frames_remaining,
-		                                 &frames[0][0]);
+		auto frames = RenderFrames(num_frames_remaining);
+		output_queue.NonblockingBulkEnqueue(frames, num_frames_remaining);
 	}
 	last_rendered_ms = PIC_FullIndex();
 }
@@ -542,7 +560,6 @@ void Gus::CheckIrq()
 
 bool Gus::CheckTimer(const size_t t)
 {
-	std::lock_guard lock(mutex);
 	auto& timer = t == 0 ? timer_one : timer_two;
 	if (!timer.is_masked) {
 		timer.has_expired = true;
@@ -616,7 +633,6 @@ void Gus::UpdateDmaAddr(uint32_t offset) noexcept
 template <SampleSize sample_size>
 bool Gus::SizedDmaTransfer()
 {
-	std::lock_guard lock(mutex);
 	if (dma_channel->is_masked || !dma_control_register.is_enabled) {
 		return false;
 	}
@@ -831,7 +847,6 @@ void Gus::MirrorAdLibCommandRegister(const uint8_t reg_value)
 
 void Gus::PrintStats()
 {
-	std::lock_guard lock(mutex);
 	// Aggregate stats from all voices
 	uint32_t combined_8bit_ms  = 0;
 	uint32_t combined_16bit_ms = 0;
@@ -877,7 +892,6 @@ void Gus::PrintStats()
 
 uint16_t Gus::ReadFromPort(const io_port_t port, io_width_t width)
 {
-	std::lock_guard lock(mutex);
 	RenderUpToNow();
 
 	//	LOG_MSG("GUS: Read from port %x", port);
@@ -1039,7 +1053,7 @@ void Gus::RegisterIoHandlers()
 void Gus::Reset() noexcept
 {
 	// Halt playback before altering the DSP state
-	audio_channel->Enable(false);
+	channel->Enable(false);
 
 	irq_status                 = 0;
 	irq_previously_interrupted = false;
@@ -1123,7 +1137,6 @@ void Gus::UpdatePlaybackDmaAddress(const uint8_t new_address)
 
 void Gus::WriteToPort(io_port_t port, io_val_t value, io_width_t width)
 {
-	std::lock_guard lock(mutex);
 	RenderUpToNow();
 
 	const auto val = check_cast<uint16_t>(value);
@@ -1447,8 +1460,8 @@ Gus::~Gus()
 	}
 
 	// Deregister the mixer channel, after which it's cleaned up
-	assert(audio_channel);
-	MIXER_DeregisterChannel(audio_channel);
+	assert(channel);
+	MIXER_DeregisterChannel(channel);
 
 	// Deregister the DMA source once the mixer channel is gone, which can
 	// pull samples from DMA.

src/hardware/gus.h
@@ -21,6 +21,7 @@
 
 #include "dma.h"
 #include "mixer.h"
+#include "rwqueue.h"
 
 #include <queue>
 
@@ -264,6 +265,7 @@ public:
 	bool CheckTimer(size_t t);
 	void MirrorAdLibCommandRegister(const uint8_t reg_value);
 	void PrintStats();
+	void PicCallback(const int requested_frames);
 
 	struct Timer {
 		double delay          = 0.0;
@@ -276,6 +278,10 @@ public:
 	Timer timer_one = {TIMER_1_DEFAULT_DELAY};
 	Timer timer_two = {TIMER_2_DEFAULT_DELAY};
 
+	float frame_counter     = 0.0f;
+	MixerChannelPtr channel = nullptr;
+	RWQueue<AudioFrame> output_queue {1};
+
 	std::function<bool()> PerformDmaTransfer = {};
 
 private:
@@ -284,7 +290,6 @@ private:
 	Gus& operator=(const Gus&) = delete; // prevent assignment
 
 	void ActivateVoices(uint8_t requested_voices);
-	void AudioCallback(const int requested_frames);
 	void CheckIrq();
 	void CheckVoiceIrq();
 	uint32_t GetDmaOffset() noexcept;
@@ -335,7 +340,6 @@ private:
 	VoiceIrq voice_irq            = {};
 	Voice* target_voice           = nullptr;
 	DmaChannel* dma_channel       = nullptr;
-	MixerChannelPtr audio_channel = nullptr;
 
 	// Playback related
 	double last_rendered_ms = 0.0;
@@ -381,3 +385,5 @@ private:
 	bool irq_previously_interrupted = false;
 	bool should_change_irq_dma      = false;
 };
+
+extern std::unique_ptr<Gus> gus;

src/hardware/mixer.cpp
@@ -38,6 +38,7 @@
 #include "checks.h"
 #include "control.h"
 #include "cross.h"
+#include "gus.h"
 #include "hardware.h"
 #include "lpt_dac.h"
 #include "mapper.h"
@@ -306,6 +307,9 @@ void MIXER_LockMixerThread()
 	if (lpt_dac) {
 		lpt_dac->output_queue.Stop();
 	}
+	if (gus) {
+		gus->output_queue.Stop();
+	}
 	soundblaster_mixer_queue.Stop();
 	reel_magic_audio.output_queue.Stop();
 	mixer.mutex.lock();
@@ -324,6 +328,9 @@ void MIXER_UnlockMixerThread()
 	}
 	if (lpt_dac) {
 		lpt_dac->output_queue.Start();
+	}
+	if (gus) {
+		gus->output_queue.Start();
 	}
 	soundblaster_mixer_queue.Start();
 	reel_magic_audio.output_queue.Start();