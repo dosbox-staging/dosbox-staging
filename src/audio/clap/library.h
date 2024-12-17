b506b6ba5c0e16ba9ceded6af42a68dbd186c9a7
John Novak <john@johnnovak.net>
2024-12-18 09:25:51 +1000

    Add explanatory comments to the CLAP subsystem

--- src/audio/clap/library.h
+++ src/audio/clap/library.h
@@ -40,16 +40,33 @@ struct PluginInfo {
 	std::string description = {};
 };
 
-// Encapsulates and manages the lifecycle of a CLAP library. CLAP libraries
-// are uniquely identified by their filesystem paths.
+// Encapsulates a dynamically-loaded CLAP library and manages its lifecycle.
+// CLAP libraries are uniquely identified by their filesystem paths.
+//
 class Library {
 public:
+	// Loads and initialises a CLAP plugin. `library_path` must be:
+	//
+	// - On Windows: the path of the `.clap` file (renamed DLL file)
+	//
+	// - On Linux: the path of the `.clap`  file (renamed .so file)
+	//
+	// - On macOS: the path of the application bundle. The actual
+	// dynamic-link library (.dynlib file) is in `Contents/MacOS` within the
+	// bundle. Passing in
+	//
+	// Throws `std::runtime_error` if the library cannot be loaded and
+	// initialiased.
+	//
 	Library(const std_fs::path& library_path);
+
+	// Deinits the library and unloads the dynamic-link library
 	~Library();
 
 	// Returns the path passed into the constructor
 	std_fs::path GetPath() const;
 
+	// Get a list of all plugins in the library
 	std::vector<PluginInfo> GetPluginInfos() const;
 
 	const clap_plugin_entry_t* GetPluginEntry() const;
--- src/audio/clap/plugin_manager.h
+++ src/audio/clap/plugin_manager.h
@@ -31,10 +31,9 @@
 
 namespace Clap {
 
-// Plugin manager to discover and load (instantiate) CLAP plugins.
-//
-// This is a singleton that only enumerates the list of available plugins once
-// during the lifecycle of the program.
+// CLAP plugin manager singleton to discover and load (instantiate) CLAP
+// libraries and plugins. Technically, it implements some parts of a very
+// basic CLAP host.
 //
 class PluginManager {
 public:
@@ -44,10 +43,15 @@ public:
 		return instance;
 	}
 
+	// Enumerates the list of available plugins only once during the
+	// lifecycle of the program, then it returns the cached results.
+	//
+	// Only supported plugins having a single MIDI input port and a single
+	// stereo audio output port and are enumerated.
+	//
 	std::vector<PluginInfo> GetPluginInfos();
 
-	// Use the `path` and `id` from `PluginInfo` retrieved via
-	// `GetPluginInfos()`.
+	// Loads and initialises a CLAP plugin.
 	std::unique_ptr<Plugin> LoadPlugin(const PluginInfo& plugin_info);
 
 private:
@@ -63,12 +67,14 @@ private:
 	void EnumeratePlugins();
 
 	std::vector<PluginInfo> plugin_info_cache = {};
-
-	bool plugins_enumerated = false;
+	bool plugins_enumerated                   = false;
 
 	// Library handling
 	std::shared_ptr<Library> GetOrLoadLibrary(const std_fs::path& library_path);
 
+	// Plugin instances hold shared_ptrs to the loaded library instances.
+	// We need to look up the library in the cache first whenever we
+	// instantiate a new plugin.
 	std::vector<std::weak_ptr<Library>> library_cache = {};
 };
 
