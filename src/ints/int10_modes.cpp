dosbox-staging/dosbox-staging

commit dc626678715958b83e28df2e034c55740158207e

Refactor `ATT_REGS` to `NumAttributeRegisters`

src/ints/int10_modes.cpp
@@ -39,7 +39,8 @@
 
 #define SEQ_REGS 0x05
 #define GFX_REGS 0x09
-#define ATT_REGS 0x15
+
+constexpr auto NumAttributeRegisters = 0x15;
 
 using namespace bit;
 using namespace bit::literals;
@@ -1587,9 +1588,12 @@ bool INT10_SetVideoMode(uint16_t mode)
 		IO_Write(0x3ce,ct);
 		IO_Write(0x3cf,gfx_data[ct]);
 	}
-	uint8_t att_data[ATT_REGS];
-	memset(att_data,0,ATT_REGS);
-	att_data[0x12]=0xf;				//Always have all color planes enabled
+
+	uint8_t att_data[NumAttributeRegisters];
+	memset(att_data, 0, NumAttributeRegisters);
+	// Always have all color planes enabled
+	att_data[0x12] = 0xf;
+
 	//  Program Attribute Controller
 	switch (CurMode->type) {
 	case M_EGA:
@@ -1707,7 +1711,7 @@ att_text16:
 #if 0
 		LOG_DEBUG("INT10H: Set up Palette Registers");
 #endif
-		for (auto ct = 0; ct < ATT_REGS; ++ct) {
+		for (auto ct = 0; ct < NumAttributeRegisters; ++ct) {
 			IO_Write(0x3c0, ct);
 			IO_Write(0x3c0, att_data[ct]);
 		}
@@ -1810,7 +1814,7 @@ att_text16:
 			}
 		}
 	} else {
-		for (auto ct = 0x10; ct < ATT_REGS; ++ct) {
+		for (auto ct = 0; ct < NumAttributeRegisters; ++ct) {
 			// Skip overscan register
 			if (ct == 0x11) {
 				continue;

commit 7980f7972f296db5d04b78105b8e832d23a3d8d4

Fix INT10_SetVideoMode() regression
Regression introduced in dc62667 by a typo.

This caused the grayscale static text screens in the intro of DreamWeb
to appear "quantised" to only pure black and white colours.

src/ints/int10_modes.cpp
@@ -2002,7 +2002,7 @@ bool INT10_SetVideoMode(uint16_t mode)
 			}
 		}
 	} else {
-		for (auto ct = 0; ct < NumVgaAttributeRegisters; ++ct) {
+		for (auto ct = 0x10; ct < NumVgaAttributeRegisters; ++ct) {
 			// Skip overscan register
 			if (ct == 0x11) {
 				continue;