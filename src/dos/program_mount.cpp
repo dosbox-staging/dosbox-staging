aefdd47c8e1caa9fd225b8190c28d66cd11bc16d
John Novak <john@johnovak.net>
2025-03-22 10:44:33 +1000

    Reformat

--- src/dos/program_mount.cpp
+++ src/dos/program_mount.cpp
@@ -20,6 +20,7 @@
  */
 
 #include "program_mount.h"
+#include "program_mount_common.h"
 
 #include "dosbox.h"
 
@@ -33,27 +34,31 @@
 #include "shell.h"
 #include "string_utils.h"
 
-
 void MOUNT::ListMounts()
 {
 	const std::string header_drive = MSG_Get("PROGRAM_MOUNT_STATUS_DRIVE");
 	const std::string header_type  = MSG_Get("PROGRAM_MOUNT_STATUS_TYPE");
 	const std::string header_label = MSG_Get("PROGRAM_MOUNT_STATUS_LABEL");
 
-	const int term_width = real_readw(BIOSMEM_SEG, BIOSMEM_NB_COLS);
+	const int term_width   = real_readw(BIOSMEM_SEG, BIOSMEM_NB_COLS);
 	const auto width_drive = static_cast<int>(header_drive.length());
 	const auto width_label = std::max(minimum_column_length,
 	                                  static_cast<int>(header_label.size()));
-	const auto width_type = term_width - 3 - width_drive - width_label;
+	const auto width_type  = term_width - 3 - width_drive - width_label;
 	if (width_type < 0) {
 		LOG_WARNING("Message is too long.");
 		return;
 	}
 
-	auto print_row = [&](const std::string &txt_drive, const std::string &txt_type,
-	                     const std::string &txt_label) {
-		WriteOut("%-*s %-*s %-*s\n", width_drive, txt_drive.c_str(),
-		         width_type, txt_type.c_str(), width_label,
+	auto print_row = [&](const std::string& txt_drive,
+	                     const std::string& txt_type,
+	                     const std::string& txt_label) {
+		WriteOut("%-*s %-*s %-*s\n",
+		         width_drive,
+		         txt_drive.c_str(),
+		         width_type,
+		         txt_type.c_str(),
+		         width_label,
 		         txt_label.c_str());
 	};
 
@@ -86,13 +91,14 @@ void MOUNT::ShowUsage()
 	output.Display();
 }
 
-void MOUNT::Run(void) {
+void MOUNT::Run(void)
+{
 	std::shared_ptr<DOS_Drive> newdrive = {};
 	char drive                          = '\0';
 	std::string label                   = {};
 	std::string umount                  = {};
 
-	//Hack To allow long commandlines
+	// Hack To allow long commandlines
 	ChangeToLongCmd();
 	/* Parse the command line */
 	/* if the command line is empty show current mounts */
@@ -100,8 +106,8 @@ void MOUNT::Run(void) {
 		ListMounts();
 		return;
 	}
-	// Print help if requested. Previously, help was shown as 
-	// a side effect of not being able to parse the correct 
+	// Print help if requested. Previously, help was shown as
+	// a side effect of not being able to parse the correct
 	// command line options.
 	if (HelpRequested()) {
 		ShowUsage();
@@ -109,16 +115,18 @@ void MOUNT::Run(void) {
 	}
 
 	/* In secure mode don't allow people to change mount points.
-		* Neither mount nor unmount */
+	 * Neither mount nor unmount */
 	if (control->SecureMode()) {
 		WriteOut(MSG_Get("PROGRAM_CONFIG_SECURE_DISALLOW"));
 		return;
 	}
 	bool path_relative_to_last_config = false;
-	if (cmd->FindExist("-pr",true)) path_relative_to_last_config = true;
+	if (cmd->FindExist("-pr", true)) {
+		path_relative_to_last_config = true;
+	}
 
 	/* Check for unmounting */
-	if (cmd->FindString("-u",umount,false)) {
+	if (cmd->FindString("-u", umount, false)) {
 		WriteOut(UnmountHelper(umount[0]), toupper(umount[0]));
 		return;
 	}
@@ -127,67 +135,83 @@ void MOUNT::Run(void) {
 	        control->GetSection("dosbox"));
 	assert(section);
 
-	std::string type="dir";
-	cmd->FindString("-t",type,true);
-	bool iscdrom = (type =="cdrom"); //Used for mscdex bug cdrom label name emulation
+	std::string type = "dir";
+	cmd->FindString("-t", type, true);
+	bool iscdrom = (type == "cdrom"); // Used for mscdex bug cdrom label
+	                                  // name emulation
 	const bool readonly = cmd->FindExist("-ro", true);
-	if (type=="floppy" || type=="dir" || type=="cdrom" || type =="overlay") {
-		uint16_t sizes[4] ={0};
+	if (type == "floppy" || type == "dir" || type == "cdrom" || type == "overlay") {
+		uint16_t sizes[4] = {0};
 		uint8_t mediaid;
 		std::string str_size = "";
-		if (type=="floppy") {
-			str_size="512,1,2880,2880";/* All space free */
-			mediaid=0xF0;		/* Floppy 1.44 media */
-		} else if (type=="dir" || type == "overlay") {
+		if (type == "floppy") {
+			str_size = "512,1,2880,2880"; /* All space free */
+			mediaid  = 0xF0;              /* Floppy 1.44 media */
+		} else if (type == "dir" || type == "overlay") {
 			// 512*32*32765==~500MB total size
 			// 512*32*16000==~250MB total free size
-			str_size="512,32,32765,16000";
-			mediaid=0xF8;		/* Hard Disk */
-		} else if (type=="cdrom") {
-			str_size="2048,1,65535,0";
-			mediaid=0xF8;		/* Hard Disk */
+			str_size = "512,32,32765,16000";
+			mediaid  = 0xF8; /* Hard Disk */
+		} else if (type == "cdrom") {
+			str_size = "2048,1,65535,0";
+			mediaid  = 0xF8; /* Hard Disk */
 		} else {
-			WriteOut(MSG_Get("PROGAM_MOUNT_ILL_TYPE"),type.c_str());
+			WriteOut(MSG_Get("PROGAM_MOUNT_ILL_TYPE"), type.c_str());
 			return;
 		}
 		/* Parse the free space in mb's (kb's for floppies) */
 		std::string mb_size;
-		if (cmd->FindString("-freesize",mb_size,true)) {
+		if (cmd->FindString("-freesize", mb_size, true)) {
 			char teststr[1024];
-			uint16_t freesize = static_cast<uint16_t>(atoi(mb_size.c_str()));
-			if (type=="floppy") {
+			uint16_t freesize = static_cast<uint16_t>(
+			        atoi(mb_size.c_str()));
+			if (type == "floppy") {
 				// freesize in kb
-				sprintf(teststr,"512,1,2880,%d",freesize*1024/(512*1));
+				sprintf(teststr,
+				        "512,1,2880,%d",
+				        freesize * 1024 / (512 * 1));
 			} else {
-				uint32_t total_size_cyl=32765;
-				uint32_t free_size_cyl=(uint32_t)freesize*1024*1024/(512*32);
-				if (free_size_cyl>65534) free_size_cyl=65534;
-				if (total_size_cyl<free_size_cyl) total_size_cyl=free_size_cyl+10;
-				if (total_size_cyl>65534) total_size_cyl=65534;
-				sprintf(teststr,"512,32,%d,%d",total_size_cyl,free_size_cyl);
+				uint32_t total_size_cyl = 32765;
+				uint32_t free_size_cyl  = (uint32_t)freesize *
+				                         1024 * 1024 / (512 * 32);
+				if (free_size_cyl > 65534) {
+					free_size_cyl = 65534;
+				}
+				if (total_size_cyl < free_size_cyl) {
+					total_size_cyl = free_size_cyl + 10;
+				}
+				if (total_size_cyl > 65534) {
+					total_size_cyl = 65534;
+				}
+				sprintf(teststr, "512,32,%d,%d", total_size_cyl, free_size_cyl);
 			}
-			str_size=teststr;
+			str_size = teststr;
 		}
 
-		cmd->FindString("-size",str_size,true);
-		char number[21] = { 0 };const char * scan = str_size.c_str();
-		Bitu index = 0;Bitu count = 0;
+		cmd->FindString("-size", str_size, true);
+		char number[21]  = {0};
+		const char* scan = str_size.c_str();
+		Bitu index       = 0;
+		Bitu count       = 0;
 		/* Parse the str_size string */
 		while (*scan && index < 20 && count < 4) {
-			if (*scan==',') {
-				number[index] = 0;
+			if (*scan == ',') {
+				number[index]  = 0;
 				sizes[count++] = atoi(number);
-				index = 0;
-			} else number[index++] = *scan;
+				index          = 0;
+			} else {
+				number[index++] = *scan;
+			}
 			scan++;
 		}
 		if (count < 4) {
-			number[index] = 0; //always goes correct as index is max 20 at this point.
+			number[index] = 0; // always goes correct as index is
+			                   // max 20 at this point.
 			sizes[count] = atoi(number);
 		}
 
 		// get the drive letter
-		cmd->FindCommand(1,temp_line);
+		cmd->FindCommand(1, temp_line);
 		if ((temp_line.size() > 2) ||
 		    ((temp_line.size() > 1) && (temp_line[1] != ':'))) {
 			ShowUsage();
@@ -201,7 +225,7 @@ void MOUNT::Run(void) {
 		}
 		drive = int_to_char(i_drive);
 		if (type == "overlay") {
-			//Ensure that the base drive exists:
+			// Ensure that the base drive exists:
 			if (!Drives.at(drive_index(drive))) {
 				WriteOut(MSG_Get("PROGRAM_MOUNT_OVERLAY_NO_BASE"));
 				return;
@@ -213,7 +237,7 @@ void MOUNT::Run(void) {
 			return;
 		}
 
-		if (!cmd->FindCommand(2,temp_line)) {
+		if (!cmd->FindCommand(2, temp_line)) {
 			ShowUsage();
 			return;
 		}
@@ -241,9 +265,10 @@ void MOUNT::Run(void) {
 
 #if defined(WIN32)
 		/* Removing trailing backslash if not root dir so stat
-			* will succeed */
-		if (temp_line.size() > 3 && temp_line.back() == '\\')
+		 * will succeed */
+		if (temp_line.size() > 3 && temp_line.back() == '\\') {
 			temp_line.pop_back();
+		}
 #endif
 
 		const std::string real_path = to_native_path(temp_line);
@@ -252,67 +277,87 @@ void MOUNT::Run(void) {
 		} else {
 			const auto home_resolve = resolve_home(temp_line).string();
 			if (home_resolve == real_path) {
-				LOG_MSG("MOUNT: Path '%s' found",
-						temp_line.c_str());
+				LOG_MSG("MOUNT: Path '%s' found", temp_line.c_str());
 			} else {
 				LOG_MSG("MOUNT: Path '%s' found, while looking for '%s'",
-						real_path.c_str(),
-						temp_line.c_str());
+				        real_path.c_str(),
+				        temp_line.c_str());
 			}
 			temp_line = real_path;
 		}
 
 		struct stat test;
-		if (stat(temp_line.c_str(),&test)) {
-			WriteOut(MSG_Get("PROGRAM_MOUNT_ERROR_1"),temp_line.c_str());
+		if (stat(temp_line.c_str(), &test)) {
+			WriteOut(MSG_Get("PROGRAM_MOUNT_ERROR_1"),
+			         temp_line.c_str());
 			return;
 		}
 		/* Not a switch so a normal directory/file */
 		if (!S_ISDIR(test.st_mode)) {
-			WriteOut(MSG_Get("PROGRAM_MOUNT_ERROR_2"),temp_line.c_str());
+			WriteOut(MSG_Get("PROGRAM_MOUNT_ERROR_2"),
+			         temp_line.c_str());
 			return;
 		}
 
-		if (temp_line[temp_line.size() - 1] != CROSS_FILESPLIT) temp_line += CROSS_FILESPLIT;
+		if (temp_line[temp_line.size() - 1] != CROSS_FILESPLIT) {
+			temp_line += CROSS_FILESPLIT;
+		}
 		uint8_t int8_tize = (uint8_t)sizes[1];
 
 		if (type == "cdrom") {
-			// Following options were relevant only for physical CD-ROM support:
-			for (auto opt : {"-noioctl", "-ioctl", "-ioctl_dx", "-ioctl_mci", "-ioctl_dio"}) {
-				if (cmd->FindExist(opt, false))
-					WriteOut(MSG_Get("MSCDEX_WARNING_NO_OPTION"), opt);
+			// Following options were relevant only for physical
+			// CD-ROM support:
+			for (auto opt :
+			     {"-noioctl", "-ioctl", "-ioctl_dx", "-ioctl_mci", "-ioctl_dio"}) {
+				if (cmd->FindExist(opt, false)) {
+					WriteOut(MSG_Get("MSCDEX_WARNING_NO_OPTION"),
+					         opt);
+				}
 			}
 
 			int error = 0;
 			newdrive  = std::make_shared<cdromDrive>(drive,
-			                                         temp_line.c_str(),
-			                                         sizes[0],
-			                                         int8_tize,
-			                                         sizes[2],
-			                                         0,
-			                                         mediaid,
-			                                         error);
+                                                                temp_line.c_str(),
+                                                                sizes[0],
+                                                                int8_tize,
+                                                                sizes[2],
+                                                                0,
+                                                                mediaid,
+                                                                error);
 			// Check Mscdex, if it worked out...
 			switch (error) {
-				case 0  :	WriteOut(MSG_Get("MSCDEX_SUCCESS"));				break;
-				case 1  :	WriteOut(MSG_Get("MSCDEX_ERROR_MULTIPLE_CDROMS"));	break;
-				case 2  :	WriteOut(MSG_Get("MSCDEX_ERROR_NOT_SUPPORTED"));	break;
-				case 3  :	WriteOut(MSG_Get("MSCDEX_ERROR_PATH"));				break;
-				case 4  :	WriteOut(MSG_Get("MSCDEX_TOO_MANY_DRIVES"));		break;
-				case 5  :	WriteOut(MSG_Get("MSCDEX_LIMITED_SUPPORT"));		break;
-				default :	WriteOut(MSG_Get("MSCDEX_UNKNOWN_ERROR"));			break;
+			case 0: WriteOut(MSG_Get("MSCDEX_SUCCESS")); break;
+			case 1:
+				WriteOut(MSG_Get("MSCDEX_ERROR_MULTIPLE_CDROMS"));
+				break;
+			case 2:
+				WriteOut(MSG_Get("MSCDEX_ERROR_NOT_SUPPORTED"));
+				break;
+			case 3: WriteOut(MSG_Get("MSCDEX_ERROR_PATH")); break;
+			case 4:
+				WriteOut(MSG_Get("MSCDEX_TOO_MANY_DRIVES"));
+				break;
+			case 5:
+				WriteOut(MSG_Get("MSCDEX_LIMITED_SUPPORT"));
+				break;
+			default:
+				WriteOut(MSG_Get("MSCDEX_UNKNOWN_ERROR"));
+				break;
 			};
 			if (error && error != 5) {
 				return;
 			}
 		} else {
 			/* Give a warning when mount c:\ or the / */
-#if defined (WIN32)
-			if ( (temp_line == "c:\\") || (temp_line == "C:\\") ||
-				(temp_line == "c:/") || (temp_line == "C:/")    )
+#if defined(WIN32)
+			if ((temp_line == "c:\\") || (temp_line == "C:\\") ||
+			    (temp_line == "c:/") || (temp_line == "C:/")) {
 				WriteOut(MSG_Get("PROGRAM_MOUNT_WARNING_WIN"));
+			}
 #else
-			if (temp_line == "/") WriteOut(MSG_Get("PROGRAM_MOUNT_WARNING_OTHER"));
+			if (temp_line == "/") {
+				WriteOut(MSG_Get("PROGRAM_MOUNT_WARNING_OTHER"));
+			}
 #endif
 			if (type == "overlay") {
 				const auto ldp = std::dynamic_pointer_cast<localDrive>(
@@ -320,11 +365,12 @@ void MOUNT::Run(void) {
 				const auto cdp = std::dynamic_pointer_cast<cdromDrive>(
 				        Drives[drive_index(drive)]);
 				if (!ldp || cdp) {
-					WriteOut(MSG_Get("PROGRAM_MOUNT_OVERLAY_INCOMPAT_BASE"));
+					WriteOut(MSG_Get(
+					        "PROGRAM_MOUNT_OVERLAY_INCOMPAT_BASE"));
 					return;
 				}
 				std::string base = ldp->GetBasedir();
-				uint8_t o_error = 0;
+				uint8_t o_error  = 0;
 				newdrive = std::make_shared<Overlay_Drive>(
 				        base.c_str(),
 				        temp_line.c_str(),
@@ -336,17 +382,18 @@ void MOUNT::Run(void) {
 				        o_error);
 				// Erase old drive on success
 				if (o_error) {
-					if (o_error == 1) WriteOut("No mixing of relative and absolute paths. Overlay failed.");
-					else if (o_error == 2) WriteOut("overlay directory can not be the same as underlying file system.");
-					else {
+					if (o_error == 1) {
+						WriteOut("No mixing of relative and absolute paths. Overlay failed.");
+					} else if (o_error == 2) {
+						WriteOut("overlay directory can not be the same as underlying file system.");
+					} else {
 						WriteOut("Something went wrong");
 					}
 					return;
 				}
-				//Copy current directory if not marked as deleted.
+				// Copy current directory if not marked as deleted.
 				if (newdrive->TestDir(ldp->curdir)) {
-					safe_strcpy(newdrive->curdir,
-								ldp->curdir);
+					safe_strcpy(newdrive->curdir, ldp->curdir);
 				}
 				Drives.at(drive_index(drive)) = nullptr;
 			} else {
@@ -363,7 +410,7 @@ void MOUNT::Run(void) {
 			}
 		}
 	} else {
-		WriteOut(MSG_Get("PROGRAM_MOUNT_ILL_TYPE"),type.c_str());
+		WriteOut(MSG_Get("PROGRAM_MOUNT_ILL_TYPE"), type.c_str());
 		return;
 	}
 
@@ -373,6 +420,7 @@ void MOUNT::Run(void) {
 	/* Set the correct media byte in the table */
 	mem_writeb(RealToPhysical(dos.tables.mediaid) + (drive_index(drive)) * 9,
 	           newdrive->GetMediaByte());
+
 	if (type != "overlay") {
 		WriteOut(MSG_Get("PROGRAM_MOUNT_STATUS_2"),
 		         newdrive->GetInfoString().c_str(),
@@ -380,12 +428,15 @@ void MOUNT::Run(void) {
 		if (readonly) {
 			WriteOut(MSG_Get("PROGRAM_MOUNT_READONLY"));
 		}
+		WriteOut("\n");
 	} else {
 		WriteOut(MSG_Get("PROGRAM_MOUNT_OVERLAY_STATUS"),
 		         temp_line.c_str(),
 		         drive);
 	}
-	/* check if volume label is given and don't allow it to updated in the future */
+
+	/* check if volume label is given and don't allow it to updated in the
+	 * future */
 	if (cmd->FindString("-label", label, true)) {
 		newdrive->dirCache.SetLabel(label.c_str(), iscdrom, false);
 	}
@@ -393,13 +444,17 @@ void MOUNT::Run(void) {
 	 * For floppy drives set the label to DRIVELETTER_Floppy.
 	 * This way every drive except cdroms should get a label.*/
 	else if (type == "dir" || type == "overlay") {
-		label = drive; label += "_DRIVE";
+		label = drive;
+		label += "_DRIVE";
 		newdrive->dirCache.SetLabel(label.c_str(), iscdrom, false);
 	} else if (type == "floppy") {
-		label = drive; label += "_FLOPPY";
+		label = drive;
+		label += "_FLOPPY";
 		newdrive->dirCache.SetLabel(label.c_str(), iscdrom, true);
 	}
-	if (type == "floppy") incrementFDD();
+	if (type == "floppy") {
+		incrementFDD();
+	}
 	return;
 }
 
