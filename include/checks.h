a5267b06050e514bebbf029d7807170976302b3f
John Novak <john@johnovak.net>
2025-04-23 18:41:51 +1000

    Update comment in `checks.h`

--- include/checks.h
+++ include/checks.h
@@ -1,7 +1,7 @@
 /*
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *
- *  Copyright (C) 2022-2024  The DOSBox Staging Team
+ *  Copyright (C) 2022-2025  The DOSBox Staging Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -24,18 +24,19 @@
 // This file contains macros that enable extra code checks at the file-level.
 //
 // Checks can include any compiler-toggleable features that the project team
-// feels can improve code-quality. This might include:
+// feels can improve code quality. This might include:
 //   - warnings
 //   - warnings-as-errors
 //   - diagnostics
 //   - formatting / structure / design / etc..
 //
 // These checks are those that (currently) can't be enabled project-wide
-// because they would be fatal (ie, -Werror) or too verbose (ie, -Wnarrowing
-// generating 3,400+ warnings!)
+// because they would be fatal (i.e., -Werror) or too verbose (i.e.,
+// -Wnarrowing generating too many warnings to make the warnings actually
+// useful).
 //
-// So this per file approach lets us focus down one file at a time and then
-// declare that it's free from the issue(s) we're checking.
+// This per file approach lets us focus on one file at a time and then declare
+// that it's free from the issue(s) we're checking.
 //
 // Usage:
 //     #include "checks.h"
@@ -43,8 +44,8 @@
 //
 // How to add new checks:
 //
-//   1. Assess how much work it would be to enable it at the project level. If
-//      the check is too burdensome, then carry on.
+//   1. Assess how much work it would be to enable it at the project level.
+//      If the check is too burdensome, then carry on.
 //
 //   2. The C98 and C++11 standards supports the _Pragma() function to add
 //      #pragma strings. We can use it to add compiler-specific features.
