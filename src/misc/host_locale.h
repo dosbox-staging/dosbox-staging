26193e2f98ad6aa84bfb14689fd3a320493345bd
FeralChild64 <FeralChild64@users.noreply.github.com>
2025-04-25 20:46:39 +0200

    Refactor the OS-agnostic part of the host locale detection

--- src/misc/host_locale.h
+++ src/misc/host_locale.h
@@ -1,7 +1,7 @@
 /*
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *
- *  Copyright (C) 2024-2024  The DOSBox Staging Team
+ *  Copyright (C) 2024-2025  The DOSBox Staging Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -89,6 +89,13 @@ struct StdLibLocale {
 
 	char date_separator = {};
 	char time_separator = {};
+
+private:
+	void GetNumericFormat(const std::locale& locale);
+	void GetDateFormat(const std::locale& locale);
+
+	void DetectCurrencyFormat(const std::locale& locale);
+	void DetectTimeDateFormat(const std::locale& locale);
 };
 
 struct HostLocaleElement {
