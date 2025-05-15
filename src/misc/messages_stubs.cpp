2b411bc9bd9b7714685e0ede800fc116378fa0ac
FeralChild64 <FeralChild64@users.noreply.github.com>
2025-05-15 14:49:23 +0200

    Unit tests compilation fix

--- src/misc/messages_stubs.cpp
+++ src/misc/messages_stubs.cpp
@@ -1,7 +1,7 @@
 /*
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *
- *  Copyright (C) 2022-2024  The DOSBox Staging Team
+ *  Copyright (C) 2022-2025  The DOSBox Staging Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -50,5 +50,4 @@ bool MSG_WriteToFile(const std::string&)
 	return true;
 }
 
-class Section_prop;
-void MSG_Init(Section_prop *) {}
+void MSG_LoadMessages() {}
