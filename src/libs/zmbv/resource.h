6366c1788bff0eb0fa33b78dae11a0773b5182c0
John Novak <john@johnovak.net>
2025-05-24 13:35:09 +1000

    build: Remove support for building the ZMBV dynamic library

--- src/libs/zmbv/resource.h
+++
@@ -1,8 +0,0 @@
-//{{NO_DEPENDENCIES}}
-
-#define IDD_ABOUT                       101
-#define IDD_CONFIGURE                   102
-#define IDC_HOMEPAGE                    1000
-#define IDC_EMAIL                       1001
-#define IDC_SLIDER1                     1008
-
