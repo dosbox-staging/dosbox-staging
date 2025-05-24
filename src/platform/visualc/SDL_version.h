163e21e83e84686f23d4aa35a393e631bddc0419
John Novak <john@johnovak.net>
2025-05-24 14:39:58 +1000

    build: Get rid of MSVC vcpkg workaround stubs

--- src/platform/visualc/SDL_version.h
+++
@@ -1,4 +0,0 @@
-// This file exists only for MSVC builds.
-// It's needed because vcpkg does not set SDL subdirectory in includes list
-// for projects that do not use cmake.
-#include <SDL2/SDL_version.h>
