8daeb2ee802e285adfbc87fc4663dbc205fb3e82
John Novak <john@johnnovak.net>
2025-05-30 17:40:42 +1000

    Improve messages API documentation

--- include/messages.h
+++ include/messages.h
@@ -7,14 +7,28 @@
 #include <cstdint>
 #include <string>
 
-// Load translated messages according to the configuration
+/*
+ * Load translated messages according to the configuration.
+ */
 void MSG_LoadMessages();
 
-// Add message (in UTF-8 encoding) to the translation system
+/*
+ * Add English message to the translation system in UTF-8.
+ * The message may contais ANSI markup tags (e.g., [white]CONFIG[reset]).
+ */
 void MSG_Add(const std::string& name, const std::string& message);
 
-// Get message text, adapted to the current code page,
-// with markup tags converted to ANSI escape codes
+/*
+ * Get translated message preprocessed for output to the DOS console.
+ *
+ * On success, this returns the translated message, or the English message if
+ * no translation exists for the current language, adapted to the currently
+ * active DOS code page with markup tags converted to ANSI escape codes.
+ *
+ * Returns an error message if an error occured.
+ *
+ * Use `CONSOLE_Write()` to output the message to the DOS console.
+ */
 const char* MSG_Get(const std::string& message_key);
 
 /*
@@ -24,8 +38,7 @@ const char* MSG_Get(const std::string& message_key);
  * `MSG_Add()` without any DOS code page or ANSI markup tags conversions
  * applied.
  *
- * Returns " MESSAGE NOT FOUND! " or " MESSAGE NOT VALID! " if there was an
- * error.
+ * Returns an error message if an error occured.
  */
 const char* MSG_GetEnglishRaw(const std::string& message_key);
 
@@ -37,11 +50,13 @@ const char* MSG_GetEnglishRaw(const std::string& message_key);
  * ANSI markup tags conversions applied. It effectively returns the translated
  * message as-is from the UTF-8 language file.
  *
- * Returns " MESSAGE NOT FOUND! " or " MESSAGE NOT VALID! " if there was an
- * error.
+ * Returns an error message if an error occured.
  */
 const char* MSG_GetTranslatedRaw(const std::string& message_key);
 
+/*
+ * Return true if a message with this key exists.
+ */
 bool MSG_Exists(const std::string& message_key);
 
 bool MSG_WriteToFile(const std::string& file_name);
