9e125a582236f5142f786a56a712a9b756042424
shermp <14854761+shermp@users.noreply.github.com>
2025-06-03 20:02:38 +1200

    Add icons cmake script

---
+++ extras/icons/Icons.cmake
@@ -0,0 +1,15 @@
+set(DOSBOX_PNG_ICONS 
+    icon_16.png 16
+    icon_22.png 22
+    icon_24.png 24
+    icon_32.png 32
+    icon_48.png 48
+    icon_96.png 96
+    icon_128.png 128
+    icon_256.png 256
+    icon_512.png 512
+    icon_1024.png 1024
+)
+set(DOSBOX_PNG_ICON_DIR extras/icons/png)
+
+set(DOSBOX_SVG_ICON extras/icons/svg/dosbox-staging.svg)
