f8b237db37b4020f6019ee40473dcb9363ae1213
John Novak <john@johnnovak.net>
2025-06-09 19:19:43 +1000

    Introduce `DOS_GetFirstShell()`

--- src/dos/program_choice.cpp
+++ src/dos/program_choice.cpp
@@ -14,8 +14,13 @@ void CHOICE::Run()
 {
 	std::string tmp = "";
 	cmd->GetStringRemain(tmp);
+
 	char args[CMD_MAXLINE];
 	safe_strcpy(args, tmp.c_str());
-	first_shell->CMD_CHOICE(args);
+
+	auto shell = DOS_GetFirstShell();
+	assert(shell);
+
+	shell->CMD_CHOICE(args);
 	result_errorcode = dos.return_code;
 }
