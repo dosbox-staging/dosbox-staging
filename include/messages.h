58400e3198396f492c23bb088c76eaec1fdf1118
John Novak <john@johnnovak.net>
2025-06-15 18:31:03 +1000

    Rename `name` args/vars to `message_key` for clarity

--- include/messages.h
+++ include/messages.h
@@ -15,17 +15,17 @@ void MSG_Add(const std::string& name, const std::string& message);
 
 // Get message text, adapted to the current code page,
 // with markup tags converted to ANSI escape codes
-const char* MSG_Get(const std::string& name);
+const char* MSG_Get(const std::string& message_key);
 
 // Get message text in UTF-8, without ANSI markup tags conversions
-const char* MSG_GetRaw(const std::string& name);
+const char* MSG_GetRaw(const std::string& message_key);
 
 // Like 'MSG_GetRaw', but does not check if code page is compatible with current
 // translation.
 // Use this one if the string is going to be logged or sent to the host OS.
-const char* MSG_GetForHost(const std::string& name);
+const char* MSG_GetForHost(const std::string& message_key);
 
-bool MSG_Exists(const std::string& name);
+bool MSG_Exists(const std::string& message_key);
 
 bool MSG_WriteToFile(const std::string& file_name);
 
