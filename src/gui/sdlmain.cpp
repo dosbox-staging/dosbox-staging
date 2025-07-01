bcf7055adfbfe1a38cd77c3a05f430b8499057dd
John Novak <john@johnnovak.net>
2025-07-02 08:41:51 +1000

    style: Reformat comments

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -1693,9 +1693,9 @@ static void present_frame_texture()
 
 	if (CAPTURE_IsCapturingPostRenderImage()) {
 		// glReadPixels() implicitly blocks until all pipelined
-		// rendering commands have finished, so we're guaranteed
-		// to read the contents of the up-to-date backbuffer
-		// here right before the buffer swap.
+		// rendering commands have finished, so we're guaranteed to read
+		// the contents of the up-to-date backbuffer here right before
+		// the buffer swap.
 		//
 		const auto image = get_rendered_output_from_backbuffer();
 		if (image) {
@@ -1755,9 +1755,9 @@ static void present_frame_gl()
 
 	if (CAPTURE_IsCapturingPostRenderImage()) {
 		// glReadPixels() implicitly blocks until all pipelined
-		// rendering commands have finished, so we're
-		// guaranateed to read the contents of the up-to-date
-		// backbuffer here right before the buffer swap.
+		// rendering commands have finished, so we're guaranateed to
+		// read the contents of the up-to-date backbuffer here right
+		// before the buffer swap.
 		//
 		const auto image = get_rendered_output_from_backbuffer();
 		if (image) {
