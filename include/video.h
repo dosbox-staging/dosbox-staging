7c75811ea61c1a35af93f1987a6e650c7592a71f
John Novak <john@johnnovak.net>
2025-07-20 17:48:17 +1000

    WIP presentation modes

--- include/video.h
+++ include/video.h
@@ -63,6 +63,39 @@ typedef enum {
 	GFX_CallbackRedraw
 } GFX_CallbackFunctions_t;
 
+enum class PresentationMode {
+	// In DOS rate presentation video frames are presented at the emulated DOS
+	// refresh rate, irrespective of the host rate (e.g., ~70 Hz for the
+	// common 320x200 VGA mode). In other words, the DOS rate and only that
+	// determines the presentation rate.
+	//
+	// Presenting at DOS rate is the most useful with VRR monitors when our
+	// present rate dictates the refresh rate of the monitor, so to speak, so
+	// we can handle any weird DOS refresh rate without tearing. Another
+	// common use case is presenting on a fixed refresh rate monitor without
+	// vsync.
+	DosRate,
+
+	// In host rate presentation video frames are presented at the refresh
+	// rate of the host monitor (the refresh rate set at the host operating
+	// system level), irrespective of the emulated DOS refresh rate. This
+	// effectively means we present the most recently rendered frame at
+	// regularly spaced intervals determined by the host refresh rate.
+	//
+	// Host rate only really makes sense with vsync enabled on fixed refresh
+	// rate monitors. Without vsync, we wouldn't be better off than simply
+	// presenting at the DOS (there would be a lot of tearing in both cases).
+	// But with vsync enabled, we're effectively "sampling" the stream of
+	// emulated frames at the host refresh rate and display them without
+	// tearing. This means that some frames might be presented twice and some
+	// might be skipped due to the mismatch between the DOS and host refresh
+	// rates.
+	//
+	// The most typical use case for host rate presentation is displaying ~70
+	// Hz 320x200 VGA games on a fixed 60 Hz monitor.
+	HostRate
+};
+
 // Graphics standards ordered by time of introduction (and roughly by
 // their capabilities)
 enum class GraphicsStandard { Hercules, Cga, Pcjr, Tga, Ega, Vga, Svga, Vesa };
@@ -349,8 +382,15 @@ void GFX_ResetScreen(void);
 void GFX_RequestExit(const bool requested);
 void GFX_Start(void);
 void GFX_Stop(void);
+
+// Called at the start of every unique frame (when there have been changes to
+// the framebuffer).
 bool GFX_StartUpdate(uint8_t * &pixels, int &pitch);
-void GFX_EndUpdate( const uint16_t *changedLines );
+
+// Called at the end of every frame, regardless of whether there have been
+// changes to the framebuffer or not.
+void GFX_EndUpdate(const uint16_t *changed_lines);
+
 void GFX_LosingFocus();
 void GFX_RegenerateWindow(Section *sec);
 
@@ -399,5 +439,8 @@ float GFX_GetDpiScaleFactor();
 RenderingBackend GFX_GetRenderingBackend();
 
 double GFX_GetHostRefreshRate();
+
+PresentationMode GFX_GetPresentationMode();
+void GFX_MaybePresentFrame();
 
 #endif
