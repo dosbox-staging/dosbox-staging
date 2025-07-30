8a2bb520fffa07581c57ab54568c11b1d1947bf1
John Novak <john@johnnovak.net>
2025-07-30 17:39:19 +1000

    style: Reformat

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -616,12 +616,12 @@ double DOSBOX_GetUptime()
 void DOSBOX_InitAllModuleConfigsAndMessages()
 {
 	// Note the [sdl] section is initialised in sdlmain.cpp
-	
-	SectionProp* secprop             = nullptr;
-	PropBool* pbool                  = nullptr;
-	PropInt* pint                    = nullptr;
-	PropHex* phex                    = nullptr;
-	PropString* pstring              = nullptr;
+
+	SectionProp* secprop              = nullptr;
+	PropBool* pbool                   = nullptr;
+	PropInt* pint                     = nullptr;
+	PropHex* phex                     = nullptr;
+	PropString* pstring               = nullptr;
 	PropMultiValRemain* pmulti_remain = nullptr;
 
 	// Specifies if and when a setting can be changed
@@ -635,8 +635,8 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	/* Setup all the different modules making up DOSBox */
 
 	secprop = control->AddSectionProp("dosbox",
-	                                   &DOSBOX_ConfigChanged,
-	                                   changeable_at_runtime);
+	                                  &DOSBOX_ConfigChanged,
+	                                  changeable_at_runtime);
 	pstring = secprop->AddString("language", always, "auto");
 	pstring->SetHelp(
 	        "Select the DOS messages language:\n"
@@ -650,17 +650,17 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 
 	pstring = secprop->AddString("machine", only_at_start, "svga_s3");
 	pstring->SetValues({"hercules",
-	                     "cga_mono",
-	                     "cga",
-	                     "pcjr",
-	                     "tandy",
-	                     "ega",
-	                     "svga_s3",
-	                     "svga_et3000",
-	                     "svga_et4000",
-	                     "svga_paradise",
-	                     "vesa_nolfb",
-	                     "vesa_oldvbe"});
+	                    "cga_mono",
+	                    "cga",
+	                    "pcjr",
+	                    "tandy",
+	                    "ega",
+	                    "svga_s3",
+	                    "svga_et3000",
+	                    "svga_et4000",
+	                    "svga_paradise",
+	                    "vesa_nolfb",
+	                    "vesa_oldvbe"});
 
 	pstring->SetDeprecatedWithAlternateValue("vgaonly", "svga_paradise");
 	pstring->SetHelp(
@@ -721,18 +721,18 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	pstring = secprop->AddString("vmemsize", only_at_start, "auto");
 
 	pstring->SetValues({"auto",
-	                     // values in MB
-	                     "1",
-	                     "2",
-	                     "4",
-	                     "8",
-	                     // values in KB
-	                     "256",
-	                     "512",
-	                     "1024",
-	                     "2048",
-	                     "4096",
-	                     "8192"});
+	                    // values in MB
+	                    "1",
+	                    "2",
+	                    "4",
+	                    "8",
+	                    // values in KB
+	                    "256",
+	                    "512",
+	                    "1024",
+	                    "2048",
+	                    "4096",
+	                    "8192"});
 	pstring->SetHelp(
 	        "Video memory in MB (1-8) or KB (256 to 8192). 'auto' uses the default for\n"
 	        "the selected video adapter ('auto' by default). See the 'machine' setting for\n"
@@ -764,13 +764,13 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	        "            specified in Hz (valid range is from 24.000 to 1000.000). This is a\n"
 	        "            niche option for a select few fast-paced mid to late 1990s 3D games\n"
 	        "            for high refresh rate gaming.\n"
-			"\n"
+	        "\n"
 	        "Note: Many games will misbehave when overriding the DOS video mode's refresh\n"
 	        "      rate with non-standard values. This can manifest in glitchy video,\n"
 	        "      sped-up or slowed-down audio, jerky mouse movement, mouse button presses\n"
 	        "      not being registered, and even gameplay bugs. Overriding the DOS refresh\n"
 	        "      rate is a hack that only works acceptably with a small subset of all DOS\n"
-			"      games (typically mid to late 1990s games).");
+	        "      games (typically mid to late 1990s games).");
 
 	pstring = secprop->AddString("vesa_modes", only_at_start, "compatible");
 	pstring->SetValues({"compatible", "all", "halfline"});
@@ -920,8 +920,8 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 
 	// The MIDI section must be added *after* the FluidSynth, MT-32 and
 	// SoundCanvas MIDI device sections. If the MIDI section is intialised
-	// before these, these devices would be double-initialised if selected at
-	// startup time (e.g., by having `mididevice = mt32` in the config).
+	// before these, these devices would be double-initialised if selected
+	// at startup time (e.g., by having `mididevice = mt32` in the config).
 	MIDI_AddConfigSection(control);
 
 #if C_DEBUGGER
@@ -949,8 +949,8 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 
 	// PC speaker emulation
 	secprop = control->AddSectionProp("speaker",
-	                                   &PCSPEAKER_Init,
-	                                   changeable_at_runtime);
+	                                  &PCSPEAKER_Init,
+	                                  changeable_at_runtime);
 
 	pstring = secprop->AddString("pcspeaker", when_idle, "impulse");
 	pstring->SetHelp(
@@ -1054,8 +1054,8 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 
 	// ReelMagic Emulator
 	secprop = control->AddSectionProp("reelmagic",
-	                                   &ReelMagic_Init,
-	                                   changeable_at_runtime);
+	                                  &ReelMagic_Init,
+	                                  changeable_at_runtime);
 
 	pstring = secprop->AddString("reelmagic", when_idle, "off");
 	pstring->SetHelp(
@@ -1291,8 +1291,8 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	        "FreeDOS and MS-DOS 7/8 COMMAND.COM supports this behavior.");
 
 	pstring = secprop->AddPath("shell_history_file",
-	                            only_at_start,
-	                            "shell_history.txt");
+	                           only_at_start,
+	                           "shell_history.txt");
 
 	pstring->SetHelp(
 	        "File containing persistent command line history ('shell_history.txt'\n"
