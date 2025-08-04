ebda9bce3aa0546aa3df9b0e4188af4dd8987a22
Alexandre Bique <bique.alexandre@gmail.com>
2025-08-04 19:20:59 +1000

    Upgrade CLAP audio plugin headers to v1.2.6
    This is a copy of the CLAP 1.2.6 include files from
    https://github.com/free-audio/clap
    
    CLAP stands for CLever Audio Plugin. It is an interface that provides a
    stable ABI to define a standard for Digital Audio Workstations and audio
    plugins (synthesizers, audio effects, etc.) to work together.
    
    CLAP is an open standard released under the MIT license.
    
    Signed-off-by: John Novak <john@johnnovak.net>

--- src/libs/clap/all.h
+++ src/libs/clap/all.h
@@ -6,7 +6,11 @@
 #include "factory/draft/plugin-state-converter.h"
 
 #include "ext/draft/extensible-audio-ports.h"
+#include "ext/draft/gain-adjustment-metering.h"
+#include "ext/draft/mini-curve-display.h"
+#include "ext/draft/project-location.h"
 #include "ext/draft/resource-directory.h"
+#include "ext/draft/scratch-memory.h"
 #include "ext/draft/transport-control.h"
 #include "ext/draft/triggers.h"
 #include "ext/draft/tuning.h"
