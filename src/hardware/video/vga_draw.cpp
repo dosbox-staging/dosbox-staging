f74a60d8ff58793d31a500c53917fcbbc673657f
John Novak <john@johnnovak.net>
2025-08-09 11:34:27 +1000

    Move `include/rgb565.h` to `src/util`

--- src/hardware/video/vga_draw.cpp
+++ src/hardware/video/vga_draw.cpp
@@ -12,13 +12,13 @@
 
 #include "bitops.h"
 #include "gui/render_scalers.h"
-#include "ints/int10.h"
-#include "util/math_utils.h"
-#include "mem_unaligned.h"
 #include "hardware/pic.h"
+#include "ints/int10.h"
+#include "mem_unaligned.h"
 #include "reelmagic.h"
 #include "render.h"
-#include "rgb565.h"
+#include "util/math_utils.h"
+#include "util/rgb565.h"
 #include "vga.h"
 #include "video.h"
 
