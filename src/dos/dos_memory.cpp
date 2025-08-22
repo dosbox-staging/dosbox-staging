f66dfb72ac5a28d29cf8f37fdd8ac643a9930357
ripsaw8080 <ripsaw8080@users.sourceforge.net>
2025-08-22 16:01:00 +0000

    Sets MCB ownership in all cases where resizing the block does not return an error
    Cherry-picked from SVN r4490. Improves DOS compatibility.
    
    Co-authored-by: Marco Buzzanca <marco.bzn@gmail.com>
    Signed-off-by: Marco Buzzanca <marco.bzn@gmail.com>

--- src/dos/dos_memory.cpp
+++ src/dos/dos_memory.cpp
@@ -298,7 +298,8 @@ bool DOS_ResizeMemory(uint16_t segment,uint16_t * blocks) {
 	DOS_MCB	mcb_next(segment+total);
 	if (*blocks<=total) {
 		if (*blocks == total) {
-			/* Nothing to do */
+			/* Size unchanged */
+			mcb.SetPSPSeg(dos.psp());
 			return true;
 		}
 		/* Shrinking MCB */
@@ -347,7 +348,11 @@ bool DOS_ResizeMemory(uint16_t segment,uint16_t * blocks) {
 	}
 	mcb.SetSize(total);
 	mcb.SetPSPSeg(dos.psp());
-	if (*blocks==total) return true;	/* block fit exactly */
+	if (*blocks == total) {
+		/* block fit exactly */
+		mcb.SetPSPSeg(dos.psp());
+		return true;
+	}
 
 	*blocks=total;	/* return maximum */
 	DOS_SetError(DOSERR_INSUFFICIENT_MEMORY);
