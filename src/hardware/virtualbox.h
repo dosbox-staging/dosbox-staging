b600b5e7eb0bbbe8f895e0dced7ddf88769f9708
John Novak <john@johnnovak.net>
2025-08-23 00:49:53 +1000

    Use setting update handler in `[ethernet]` section

--- src/hardware/virtualbox.h
+++ src/hardware/virtualbox.h
@@ -4,9 +4,13 @@
 #ifndef DOSBOX_VIRTUALBOX_H
 #define DOSBOX_VIRTUALBOX_H
 
+#include <string>
+
 #include "config/setup.h"
 
 void VIRTUALBOX_Init(Section* sec);
+
+void VIRTUALBOX_NotifySettingUpdated(Section* sec, const std::string& prop_name);
 
 void VIRTUALBOX_NotifyBooting();
 
