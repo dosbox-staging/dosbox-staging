5d8c309fee10270e7b9bbbb4e36cae02db744f29
John Novak <john@johnnovak.net>
2025-09-20 12:10:35 +1000

    Move PS/1 Audio definitions into `ps1audio.h`

--- src/audio/mixer.cpp
+++ src/audio/mixer.cpp
@@ -27,6 +27,7 @@
 #include "gui/mapper.h"
 #include "hardware/audio/gus.h"
 #include "hardware/audio/pcspeaker.h"
+#include "hardware/audio/ps1audio.h"
 #include "hardware/audio/sblaster.h"
 #include "hardware/audio/tandy_sound.h"
 #include "hardware/hardware.h"
