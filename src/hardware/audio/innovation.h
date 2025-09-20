e8c9a1bf6db437bd5c7d1e581cb491301fbdacc3
John Novak <john@johnnovak.net>
2025-09-20 12:15:43 +1000

    Move Innovation defintions to `innovation.h`

---
+++ src/hardware/audio/innovation.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText:  2025-2025 The DOSBox Staging Team
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef DOSBOX_INNOVATION_H
+#define DOSBOX_INNOVATION_H
+
+#include "config/setup.h"
+
+void INNOVATION_AddConfigSection(const ConfigPtr &conf);
+
+#endif // DOSBOX_INNOVATION_H
