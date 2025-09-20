2524399b0907e4db0b0849be4526384409b16d34
John Novak <john@johnnovak.net>
2025-09-20 12:06:36 +1000

    Move PC speaker definitions to `pcspeaker.h`

---
+++ src/hardware/audio/pcspeaker.h
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText:  2022-2025 The DOSBox Staging Team
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef DOSBOX_PCSPEAKER_H
+#define DOSBOX_PCSPEAKER_H
+
+#include "config/setup.h"
+
+void PCSPEAKER_AddConfigSection(Section* sec);
+
+void PCSPEAKER_NotifyLockMixer();
+void PCSPEAKER_NotifyUnlockMixer();
+
+#endif // DOSBOX_PCSPEAKER_H
