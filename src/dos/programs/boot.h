f0a758830f34ddda315e099232c562b8ce8385c0
John Novak <john@johnnovak.net>
2025-09-27 13:07:23 +1000

    Clean up BOOT command UMB/XMS/EMS disable mechanism

--- src/dos/programs/boot.h
+++ src/dos/programs/boot.h
@@ -5,29 +5,33 @@
 #ifndef DOSBOX_PROGRAM_BOOT_H
 #define DOSBOX_PROGRAM_BOOT_H
 
-#include "dosbox.h"
 #include "dos/programs.h"
+#include "dosbox.h"
 
 class BOOT final : public Program {
-    public:
-	    BOOT()
-	    {
-		    AddMessages();
-		    help_detail = {HELP_Filter::All,
-		                   HELP_Category::Dosbox,
-		                   HELP_CmdType::Program,
-		                   "BOOT"};
-	    }
-	    void Run(void) override;
+public:
+	BOOT()
+	{
+		AddMessages();
+		help_detail = {HELP_Filter::All,
+		               HELP_Category::Dosbox,
+		               HELP_CmdType::Program,
+		               "BOOT"};
+	}
+	void Run(void) override;
 
-    private:
-        static void AddMessages();
+private:
+	static void AddMessages();
+
 	FILE* getFSFile_mounted(const char* filename, uint32_t* ksize,
 	                        uint32_t* bsize, uint8_t* error);
+
 	FILE* getFSFile(const char* filename, uint32_t* ksize, uint32_t* bsize,
 	                bool tryload = false);
+
 	void printError();
-	void disable_umb_ems_xms();
+
+	void DisableUmbXmsEms();
 	void NotifyBooting();
 };
 
