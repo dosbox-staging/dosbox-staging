6a3953ae40c8e1ca0051fa617690bc767833bb26
John Novak <john@johnnovak.net>
2025-10-03 00:56:14 +1000

    Fix toggling fullscreen/windowed via the `fullscreen` setting

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -3010,7 +3010,16 @@ static void regenerate_window(Section* sec)
 static void notify_sdl_setting_updated(SectionProp& section,
                                        const std::string& prop_name)
 {
-	if (prop_name == "mapperfile") {
+	if (prop_name == "fullscreen") {
+		auto fullscreen_requested = section.GetBool("fullscreen");
+
+		if (sdl.desktop.is_fullscreen && !fullscreen_requested) {
+			exit_fullscreen();
+		} else if (!sdl.desktop.is_fullscreen && fullscreen_requested) {
+			enter_fullscreen();
+		}
+
+	} else if (prop_name == "mapperfile") {
 		MAPPER_BindKeys(&section);
 
 	} else if (prop_name == "window_position") {
@@ -3034,7 +3043,6 @@ static void notify_sdl_setting_updated(SectionProp& section,
 	} else {
 		// TODO add more granular support for these settings:
 		//   texture_renderer
-		//   fullscreen
 		//   fullscreen_mode
 		//   window_size
 		//   window_decorations
