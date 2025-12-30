b3a6a2ea000494a39a759168e1643a78d0bc347c
dividebysandwich <josef.jahn@gmail.com>
2025-12-30 11:27:11 +0100

    Implement IMGMAKE command

--- src/dosbox.h
+++ src/dosbox.h
@@ -180,6 +180,8 @@ constexpr auto ShadersDir                = "shaders";
 
 constexpr auto MicrosInMillisecond = 1000;
 constexpr auto BytesPerKilobyte    = 1024;
+constexpr auto BytesPerMegabyte = BytesPerKilobyte * 1024;
+constexpr int64_t BytesPerGigabyte = BytesPerMegabyte * 1024;
 
 enum class DiskSpeed { Maximum, Fast, Medium, Slow };
 
