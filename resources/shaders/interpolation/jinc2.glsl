755cc1bc9f7356f51b97f32b3f67e903c87e1ede
Marco Buzzanca <marco.bzn@gmail.com>
2026-01-08 16:43:12 +0100

    Remove unused constant

--- resources/shaders/interpolation/jinc2.glsl
+++ resources/shaders/interpolation/jinc2.glsl
@@ -13,7 +13,6 @@
 #define JINC2_SINC 0.82
 #define JINC2_AR_STRENGTH 0.8
 
-const float halfpi  = 1.5707963267948966192313216916398;
 const float pi      = 3.1415926535897932384626433832795;
 const float wa      = JINC2_WINDOW_SINC * pi;
 const float wb      = JINC2_SINC * pi;
