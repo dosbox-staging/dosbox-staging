dd8e580b8bf12eea27054f8bd311d478395a8a86
dividebysandwich <josef.jahn@gmail.com>
2026-01-11 15:33:37 +0100

    Fix double-minus

--- src/dos/programs/imgmake.cpp
+++ src/dos/programs/imgmake.cpp
@@ -1458,7 +1458,7 @@ void IMGMAKE::AddMessages()
 	        "  [color=white]-label name[reset]  Volume label.\n"
 	        "  [color=white]-writetodos[reset]  Write image to the emulated DOS filesystem\n"
 	        "               instead of the Host filesystem.\n"
-	        "  [color=white]-d[reset]           Same as --writetodos\n"
+	        "  [color=white]-d[reset]           Same as -writetodos\n"
 
 	        "\n"
 	        "Examples:\n"
