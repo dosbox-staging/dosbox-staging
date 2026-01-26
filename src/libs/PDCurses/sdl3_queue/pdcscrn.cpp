acc132c7ccf9a4cc5a569657a8dd4407505e6988
Kirk Klobe <kklobe@gmail.com>
2026-01-25 22:29:16 -0600

    Use better default debugger window height

--- src/libs/PDCurses/sdl3_queue/pdcscrn.cpp
+++ src/libs/PDCurses/sdl3_queue/pdcscrn.cpp
@@ -275,7 +275,7 @@ int PDC_scr_open(void)
     if (pdc_own_window)
     {
         const char *env = getenv("PDC_LINES");
-        pdc_sheight = (env ? atoi(env) : 25) * pdc_fheight;
+        pdc_sheight = (env ? atoi(env) : 50) * pdc_fheight;
 
         env = getenv("PDC_COLS");
         pdc_swidth = (env ? atoi(env) : 80) * pdc_fwidth;
