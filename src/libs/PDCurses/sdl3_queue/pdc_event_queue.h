9d00e07cfa2ea11fdf13a67e43e5f64dc58f0a2f
Kirk Klobe <kklobe@gmail.com>
2026-01-25 22:29:16 -0600

    Use better default debugger window height

--- src/libs/PDCurses/sdl3_queue/pdc_event_queue.h
+++ src/libs/PDCurses/sdl3_queue/pdc_event_queue.h
@@ -6,8 +6,8 @@
 #include <SDL3/SDL.h>
 
 struct QueuedEvent {
-    SDL_Event ev;
-    std::string text; /* copied text for SDL_EVENT_TEXT_INPUT */
+    SDL_Event ev = {};
+    std::string text = {}; /* copied text for SDL_EVENT_TEXT_INPUT */
 };
 
 extern std::queue<QueuedEvent> debugger_event_queue;
