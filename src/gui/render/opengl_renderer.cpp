c4cc088e455aee0e02a3ca9fa02597ddd6beeacf
John Novak <john@johnnovak.net>
2026-02-11 14:51:21 +1000

    Request depth channel when creating the OpenGL context

--- src/gui/render/opengl_renderer.cpp
+++ src/gui/render/opengl_renderer.cpp
@@ -69,7 +69,7 @@ SDL_Window* OpenGlRenderer::CreateSdlWindow(const int x, const int y,
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 #ifdef USE_DEBUG_CONTEXT
