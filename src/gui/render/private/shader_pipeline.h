679c8e2a59d307cef39063baa9bf2038078a6775
John Novak <john@johnnovak.net>
2026-03-04 18:43:08 +1000

    Make shader pipeline more flexible & shaders more self-describing
    - Shaders now can specify their name, inputs, and output size via
      pragmas.
    
    - Shader pass resolution is now performed by shader name strings instead
      of hardcoded enums in the code.

--- src/gui/render/private/shader_pipeline.h
+++ src/gui/render/private/shader_pipeline.h
@@ -1,24 +1,43 @@
-// SPDX-FileCopyrightText:  2026-2026 The DOSBox Staging Team
+// SPDX-FileCopyrightText:  2025-2026 The DOSBox Staging Team
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DOSBOX_SHADER_PIPELINE_H
 #define DOSBOX_SHADER_PIPELINE_H
 
 #include <iterator>
-#include <vector>
+#include <optional>
 #include <string>
+#include <vector>
 
 #include "gui/render/render.h"
 #include "misc/std_filesystem.h"
+#include "misc/video.h"
 #include "shader.h"
 #include "shader_common.h"
-#include "shader_pass.h"
 #include "utils/rect.h"
 
 // Glad must be included before SDL
 #include "glad/gl.h"
 #include <SDL.h>
 #include <SDL_opengl.h>
+
+struct ShaderPass {
+	Shader shader = {};
+
+	// Input texture; contains at least one element
+	std::vector<GLuint> in_textures = {};
+
+	// Output texture size for intermediate shader passes (width & height
+	// only), or the position and size of the viewport for the final pass.
+	DosBox::Rect out_texture_size = {};
+
+	// Textures and FBOs for intermediate shader passes. Both are 0 for the
+	// final pass that's rendered directly to the window's framebuffer.
+	GLuint out_fbo     = 0;
+	GLuint out_texture = 0;
+
+	std::string ToString() const;
+};
 
 class ShaderPipeline {
 
@@ -31,6 +50,7 @@ public:
 	                             const GLuint _input_texture);
 
 	void NotifyViewportSizeChanged(const DosBox::Rect draw_rect_px);
+	void NotifyVideoModeChanged(const VideoMode& video_mode);
 
 	void SetMainShader(const Shader& shader);
 	void SetMainShaderPreset(const ShaderPreset& preset);
@@ -47,10 +67,15 @@ public:
 	ShaderPipeline& operator=(const ShaderPipeline&) = delete;
 
 private:
-	void LoadInternalShaderPassOrExit(const std::string shader_name,
-	                                  const ShaderPass pass_params);
+	void LoadInternalShaderPassOrExit(const std::string shader_name);
 
-	ShaderPass& GetShaderPass(const ShaderPassId id);
+	void CreatePipeline();
+	void DestroyPipeline();
+
+	ShaderPass& GetShaderPass(const std::string& name);
+
+	std::pair<GLuint, DosBox::Rect> GetPreviousPassOutputTexture(
+	        const std::vector<ShaderPass>::iterator pass) const;
 
 	GLuint CreateTexture(const int width, const int height,
 	                     const TextureFilterMode filter_mode) const;
@@ -63,10 +88,12 @@ private:
 	void RenderPass(const ShaderPass& pass, const GLuint vertex_array_object) const;
 
 	struct {
-		int width      = 0;
-		int height     = 0;
-		GLuint texture = 0;
+		DosBox::Rect size = {};
+		GLuint texture    = 0;
 	} input_texture = {};
+
+	VideoMode video_mode  = {};
+	DosBox::Rect viewport = {};
 
 	// ---------------------------------------------------------------------
 	// Shader passes
