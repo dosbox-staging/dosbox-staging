464d5e341c120c99c08799fafb301e671b406671
John Novak <john@johnnovak.net>
2026-04-16 21:01:15 +1000

    Add extra notes to the `GUIDE` & `MANUAL` command help

--- src/dos/programs/manual.cpp
+++ src/dos/programs/manual.cpp
@@ -46,7 +46,13 @@ void MANUAL::AddMessages()
 	        "  [color=light-green]manual[reset]\n"
 	        "\n"
 	        "Notes:\n"
-	        "  - This will open an offline copy of the user manual bundled with your DOSBox\n"
-	        "    Staging installation; you don't need an internet connection to read the\n"
-	        "    manual.\n");
+	        "  - This will open a local offline copy of the user manual guide bundled with\n"
+	        "    your DOSBox Staging installation; you don't need an internet connection to\n"
+	        "    read the bundled manual.\n"
+	        "\n"
+	        "  - The offline documentation is located in the 'docs' subfolder of your\n"
+	        "    DOSBox Staging 'resources' folder.\n"
+	        "\n"
+	        "  - If the offline documentation cannot be found, the command will open the\n"
+	        "    guide on the project website (requires internet connection).");
 }
