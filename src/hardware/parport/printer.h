cb5e7e10e6a9c175afe20bcae824d5edd0ae555a
John Novak <john@johnnovak.net>
2026-06-15 19:18:49 +1000

    De-Bitu printer code
    Mechanical de-Bitu sweep across the parport directory. Note DOSBox
    Staging only targets 64-bit platforms.
    
      Bit8u  -> uint8_t
      Bit16u -> uint16_t
      Bit32u -> uint32_t
      Bit8s  -> int8_t
      Bit16s -> int16_t
      Bit32s -> int32_t
      Bitu   -> uint64_t   (Bitu was uintptr_t; identical on 64-bit)
      Bits   -> int64_t
    
    The bit_types_compat.h shim that was added during the initial port to
    keep the dropped-in code touchless is no longer needed and is
    removed.

--- src/hardware/parport/printer.h
+++ src/hardware/parport/printer.h
@@ -17,7 +17,6 @@
 #if !defined __PRINTER_H
 #define __PRINTER_H
 
-#include "bit_types_compat.h"
 #include "misc/types.h"
 
 #include <png.h>
@@ -75,12 +74,12 @@ enum Typeface
 
 class CPrinter {
 public:
-
-	CPrinter (Bit16u dpi, Bit16u width, Bit16u height, char* output, bool multipageOutput);
+	CPrinter(uint16_t dpi, uint16_t width, uint16_t height, char* output,
+	         bool multipageOutput);
 	virtual ~CPrinter();
 
 	// Process one character sent to virtual printer
-	void printChar(Bit8u ch);
+	void printChar(uint8_t ch);
 
 	// Hard Reset (like switching printer off and on)
 	void resetPrinterHard();
@@ -106,12 +105,12 @@ public:
 private:
 
 	// used to fill the color "sub-pallettes"
-	void FillPalette(Bit8u redmax, Bit8u greenmax, Bit8u bluemax, Bit8u colorID,
-							SDL_Palette* pal);
+	void FillPalette(uint8_t redmax, uint8_t greenmax, uint8_t bluemax,
+	                 uint8_t colorID, SDL_Palette* pal);
 
-    // Checks if given char belongs to a command and process it. If false, the character
-	// should be printed
-	bool processCommandChar(Bit8u ch);
+	// Checks if given char belongs to a command and process it. If false,
+	// the character should be printed
+	bool processCommandChar(uint8_t ch);
 
 	// Resets the printer to the factory settings
 	void resetPrinter();
@@ -124,55 +123,56 @@ private:
 
 	// Blits the given glyph on the page surface. If add is true, the values of bitmap are
 	// added to the values of the pixels in the page
-	void blitGlyph(FT_Bitmap bitmap, Bit16u destx, Bit16u desty, bool add);
+	void blitGlyph(FT_Bitmap bitmap, uint16_t destx, uint16_t desty, bool add);
 
 	// Draws an anti-aliased line from (fromx, y) to (tox, y). If broken is true, gaps are included
-	void drawLine(Bitu fromx, Bitu tox, Bitu y, bool broken);
+	void drawLine(uint64_t fromx, uint64_t tox, uint64_t y, bool broken);
 
 	// Setup the bitGraph structure
-	void setupBitImage(Bit8u dens, Bit16u numCols);
+	void setupBitImage(uint8_t dens, uint16_t numCols);
 
 	// Process a character that is part of bit image. Must be called iff bitGraph.remBytes > 0.
-	void printBitGraph(Bit8u ch);
+	void printBitGraph(uint8_t ch);
 
 	// Copies the codepage mapping from the constant array to CurMap
-	void selectCodepage(Bit16u cp);
+	void selectCodepage(uint16_t cp);
 
 	// Output current page
 	void outputPage();
 
 	// Prints out a byte using ASCII85 encoding (only outputs something every four bytes). When b>255, closes the ASCII85 string
-	void fprintASCII85(FILE* f, Bit16u b);
+	void fprintASCII85(FILE* f, uint16_t b);
 
 	// Closes a multipage document
 	void finishMultipage();
 
 	// Returns value of the num-th pixel (couting left-right, top-down) in a safe way
-	Bit8u getPixel(Bit32u num);
+	uint8_t getPixel(uint32_t num);
 
 	FT_Library FTlib = nullptr; // FreeType2 library used to render the
 	                            // characters
 
 	SDL_Surface* page = nullptr; // Surface representing the current page
 	FT_Face curFont = nullptr; // The font currently used to render characters
-	Bit8u color = 0;
+	uint8_t color = 0;
 
 	Real64 curX = 0.0, curY = 0.0; // Position of the print head (in inch)
 
-	Bit16u dpi    = 0;     // dpi of the page
-	Bit16u ESCCmd = 0;     // ESC-command that is currently processed
-	bool ESCSeen  = false; // True if last read character was an ESC (0x1B)
-	bool FSSeen   = false; // True if last read character was an FS (0x1C)
-	                       // (IBM commands)
+	uint16_t dpi    = 0;  // dpi of the page
+	uint16_t ESCCmd = 0;  // ESC-command that is currently processed
+	bool ESCSeen = false; // True if last read character was an ESC (0x1B)
+	bool FSSeen  = false; // True if last read character was an FS (0x1C)
+	                      // (IBM commands)
 
-	Bit8u numParam = 0, neededParam = 0; // Numbers of parameters already
-	                                     // read/needed to process command
+	uint8_t numParam = 0, neededParam = 0; // Numbers of parameters already
+	                                       // read/needed to process command
 
-	Bit8u params[20] = {};          // Buffer for the read params
-	Bit16u style     = 0;           // Style of font (see STYLE_* constants)
-	Real64 cpi = 0.0, actcpi = 0.0; // CPI value set by program and the actual
-	                                // one (taking in account font types)
-	Bit8u score = 0; // Score for lines (see SCORE_* constants)
+	uint8_t params[20] = {};        // Buffer for the read params
+	uint16_t style     = 0;         // Style of font (see STYLE_* constants)
+	Real64 cpi = 0.0, actcpi = 0.0; // CPI value set by program and the
+	                                // actual one (taking in account font
+	                                // types)
+	uint8_t score = 0; // Score for lines (see SCORE_* constants)
 
 	Real64 topMargin = 0.0, bottomMargin = 0.0, rightMargin = 0.0,
 	       leftMargin = 0.0; // Margins of the page (in inch)
@@ -183,13 +183,13 @@ private:
 	Real64 lineSpacing = 0.0; // Size of one line (in inch)
 
 	Real64 horiztabs[32] = {}; // Stores the set horizontal tabs (in inch)
-	Bit8u numHorizTabs   = 0;  // Number of configured tabs
+	uint8_t numHorizTabs = 0;  // Number of configured tabs
 
 	Real64 verttabs[16] = {}; // Stores the set vertical tabs (in inch)
-	Bit8u numVertTabs   = 0;  // Number of configured tabs
+	uint8_t numVertTabs = 0;  // Number of configured tabs
 
-	Bit8u curCharTable = 0; // Currently used char table und charset
-	Bit8u printQuality = 0; // Print quality (see QUALITY_* constants)
+	uint8_t curCharTable = 0; // Currently used char table und charset
+	uint8_t printQuality = 0; // Print quality (see QUALITY_* constants)
 
 	Typeface LQtypeFace = roman; // Typeface used in LQ printing mode
 
@@ -205,22 +205,22 @@ private:
 
 	struct bitGraphicParams				// Holds information about printing bit images
 	{
-		Bit16u horizDens = 0, vertDens = 0; // Density of image to print
-		                                    // (in dpi)
-		bool adjacent     = false; // Print adjacent pixels? (ignored)
-		Bit8u bytesColumn = 0;     // Bytes per column
-		Bit16u remBytes = 0;  // Bytes left to read before image is done
-		Bit8u column[6] = {}; // Bytes of the current and last column
-		Bit8u readBytesColumn = 0; // Bytes read so far for the current
-		                           // column
+		uint16_t horizDens = 0, vertDens = 0; // Density of image to
+		                                      // print (in dpi)
+		bool adjacent       = false; // Print adjacent pixels? (ignored)
+		uint8_t bytesColumn = 0;     // Bytes per column
+		uint16_t remBytes = 0; // Bytes left to read before image is done
+		uint8_t column[6] = {}; // Bytes of the current and last column
+		uint8_t readBytesColumn = 0; // Bytes read so far for the
+		                             // current column
 	} bitGraph;
 
-	Bit8u densk = 0, densl = 0, densy = 0, densz = 0; // Image density modes
-	                                                  // used in ESC K/L/Y/Z
-	                                                  // commands
+	uint8_t densk = 0, densl = 0, densy = 0, densz = 0; // Image density
+	                                                    // modes used in ESC
+	                                                    // K/L/Y/Z commands
 
-	Bit16u curMap[256]   = {}; // Currently used ASCII => Unicode mapping
-	Bit16u charTables[4] = {}; // Charactertables
+	uint16_t curMap[256]   = {}; // Currently used ASCII => Unicode mapping
+	uint16_t charTables[4] = {}; // Charactertables
 
 	Real64 definedUnit = -1.0; // Unit used by some ESC/P2 commands
 	                           // (negative => use default)
@@ -232,21 +232,21 @@ private:
 	Real64 hmi = -1.0; // Horizontal motion index (in inch; overrides CPI
 	                   // settings)
 
-	Bit8u msb             = 0; // MSB mode
-	Bit16u numPrintAsChar = 0; // Number of bytes to print as characters
-	                           // (even when normally control codes)
+	uint8_t msb             = 0; // MSB mode
+	uint16_t numPrintAsChar = 0; // Number of bytes to print as characters
+	                             // (even when normally control codes)
 
-	char* output       = nullptr; // Output method selected by user
-	void* outputHandle = nullptr; // If not null, additional pages will be
-	                              // appended to the given handle
-	bool multipageOutput = false; // If true, all pages are combined to one
-	                              // file/print job etc. until the "eject
-	                              // page" button is pressed
-	Bit16u multiPageCounter = 0;  // Current page (when printing multipages)
+	char* output       = nullptr;  // Output method selected by user
+	void* outputHandle = nullptr;  // If not null, additional pages will be
+	                               // appended to the given handle
+	bool multipageOutput = false;  // If true, all pages are combined to one
+	                               // file/print job etc. until the "eject
+	                               // page" button is pressed
+	uint16_t multiPageCounter = 0; // Current page (when printing multipages)
 
-	Bit8u ASCII85Buffer[4] = {}; // Buffer used in ASCII85 encoding
-	Bit8u ASCII85BufferPos = 0;  // Position in ASCII85 encode buffer
-	Bit8u ASCII85CurCol = 0; // Columns printed so far in the current lines
+	uint8_t ASCII85Buffer[4] = {}; // Buffer used in ASCII85 encoding
+	uint8_t ASCII85BufferPos = 0;  // Position in ASCII85 encode buffer
+	uint8_t ASCII85CurCol = 0; // Columns printed so far in the current lines
 };
 
 #endif
