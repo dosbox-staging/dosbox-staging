c88b1c15454f18a679ee8161fa22368de324200d
Daniel Bomar <dbdaniel42@gmail.com>
2026-07-05 19:03:53 -0500

    Do not fill audio with silence in SDL callback
    In contrast to SDL 3, the SDL 3 API is designed to allow
    sending as much or as little audio as you have available.
    
    It will internally fill the device buffer with silence on underrun.
    Doing this ourselves will only make the situation worse.

--- src/audio/mixer.cpp
+++ src/audio/mixer.cpp
@@ -2591,19 +2591,13 @@ static void SDLCALL mixer_callback([[maybe_unused]] void* userdata,
 
 	// Mac OSX has been observed to be problematic if we ever block inside
 	// SDL's callback. This ensures that we do not block waiting for more
-	// audio. If the queue has run dry, we write what we have available and
-	// the rest of the request is silence.
+	// audio. If the queue has run dry, we write what we have available.
 	const auto frames_to_dequeue = std::min(mixer.final_output.Size(),
 	                                        frames_requested);
 
-	output.resize(frames_requested);
+	const auto frames_received = mixer.final_output.BulkDequeue(output, frames_to_dequeue);
 
-	const auto frames_received = mixer.final_output.BulkDequeue(output.data(),
-	                                                             frames_to_dequeue);
-	// Satisfy any shortfall with silence
-	std::fill(output.begin() + frames_received, output.end(), AudioFrame{});
-
-	SDL_PutAudioStreamData(stream, output.data(), bytes_requested);
+	SDL_PutAudioStreamData(stream, output.data(), check_cast<int>(frames_received) * BytesPerAudioFrame);
 }
 
 static void mixer_thread_loop()
