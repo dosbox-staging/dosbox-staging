6587a7dae8477c4e17cfc2f3a10f699ff0b3c333
John Novak <john@johnnovak.net>
2026-09-20 10:22:43 +1000

    style: Reformat

--- src/hardware/audio/adlib_gold.cpp
+++ src/hardware/audio/adlib_gold.cpp
@@ -316,7 +316,7 @@ AdlibGold::AdlibGold(const int sample_rate_hz)
           stereo_processor(nullptr)
 {
 	surround_processor = std::make_unique<SurroundProcessor>(sample_rate_hz);
-	stereo_processor   = std::make_unique<StereoProcessor>(sample_rate_hz);
+	stereo_processor = std::make_unique<StereoProcessor>(sample_rate_hz);
 }
 
 AdlibGold::~AdlibGold() = default;
